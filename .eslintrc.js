6479608d363f5353c06035e46f8eafc9d027ad41
Massimiliano Mirra <bard@hyperstruct.net>
2021-05-15 11:34:13 +0100

    dev,enh: add lint script and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier', '@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+  ],
   env: { node: true },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
+    'import/no-default-export': 'error',
+  },
 }
