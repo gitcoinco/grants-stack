06badbb7e2da0da667af098eb87a0b28f0fb2597
Andrea Franz <andrea@gravityblast.com>
2022-04-19 15:29:41 +0200

    fix process and buffer for browser

---
+++ client/src/browserPatches.ts
@@ -0,0 +1,4 @@
+import { Buffer } from 'buffer'
+import process from 'process'
+window.Buffer = Buffer
+window.process = process;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+import './browserPatches';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import thunkMiddleware from 'redux-thunk';
