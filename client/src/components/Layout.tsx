b826dd6e3d50f9ef4dd78e4152031e97a568b69a
Andrea Franz <andrea@gravityblast.com>
2022-05-11 13:41:06 +0200

    move web3 logic to Layout and show children components only if connected

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,17 +1,64 @@
-import React from "react";
-import { Header } from './Header'
+import React, { useEffect } from 'react';
+import { RootState } from "../reducers";
+import { Header } from "./Header";
+import {
+  shallowEqual,
+  useSelector,
+  useDispatch,
+} from "react-redux";
+import { initializeWeb3 } from "../actions/web3";
+import { startIPFS } from "../actions/ipfs";
 
 interface Props {
   children: JSX.Element
 }
 
 function Layout(ownProps: Props) {
+  const dispatch = useDispatch();
+
+  const props = useSelector((state: RootState) => ({
+    web3Initializing: state.web3.initializing,
+    web3Initialized: state.web3.initialized,
+    web3Error: state.web3.error,
+    chainID: state.web3.chainID,
+    account: state.web3.account,
+    ipfsInitializing: state.ipfs.initializing,
+    ipfsInitializationError: state.ipfs.initializationError,
+    ipfsInitialized: state.ipfs.initialized,
+    ipfsLastFileSavedURL: state.ipfs.lastFileSavedURL,
+  }), shallowEqual);
+
+  useEffect(() => {
+    dispatch(initializeWeb3());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(startIPFS());
+  }, [dispatch]);
+
+  const connectHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    dispatch(initializeWeb3());
+  }
+
   return <div>
     <Header />
     <hr />
 
     <main>
-      {ownProps.children}
+      {!props.web3Error && <>
+        {props.web3Initialized && ownProps.children}
+      </>}
+
+      {props.web3Error !== undefined && <div>
+        <div>
+          {props.web3Error}
+        </div>
+      </div>}
+
+      {!props.web3Initialized && <>
+      <button onClick={connectHandler}>CONNECT</button>
+      </>}
     </main>
 
     <hr />
