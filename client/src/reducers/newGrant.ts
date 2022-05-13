df5752dc807a17753eaac83b5ed86e4519b4cd07
Tim Schultz <timschultz@Computer.local>
2022-05-12 20:51:58 -0600

    updating reducer to match updated contract

--- client/src/reducers/newGrant.ts
+++ client/src/reducers/newGrant.ts
@@ -1,44 +1,50 @@
 import {
   NewGrantActions,
   NEW_GRANT_CREATED,
-  NEW_GRANT_TX_STATUS
+  NEW_GRANT_TX_STATUS,
 } from "../actions/newGrant";
 
 export interface NewGrant {
-  id: number,
-  ipfsHash: string,
-  owner?: string
+  id: number;
+  metaData: string;
+  owner?: string;
 }
 export interface NewGrantState {
-  grants: NewGrant[]
-  txStatus: string | null
+  grants: NewGrant[];
+  txStatus: string | null;
 }
 
 const initialState: NewGrantState = {
   grants: [],
-  txStatus: null
-}
+  txStatus: null,
+};
 
-export const newGrantReducer = (state: NewGrantState = initialState, action: NewGrantActions): NewGrantState => {
+export const newGrantReducer = (
+  state: NewGrantState = initialState,
+  action: NewGrantActions
+): NewGrantState => {
   switch (action.type) {
     case NEW_GRANT_CREATED: {
       return {
         ...state,
-        grants: [...state.grants, {
-          id: action.id,
-          ipfsHash: action.ipfsHash,
-          owner: action.owner
-        }]
-      }
+        grants: [
+          ...state.grants,
+          {
+            id: action.id,
+            metaData: action.metaData,
+            owner: action.owner,
+          },
+        ],
+      };
     }
 
     case NEW_GRANT_TX_STATUS: {
       return {
         ...state,
-        txStatus: action.status
-      }
+        txStatus: action.status,
+      };
     }
   }
 
   return initialState;
-}
+};
