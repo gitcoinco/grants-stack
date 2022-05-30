049fb87d9314c05183f59f48f85ae52c8bb5220e
Chibuotu Amadi <chibuotu@gmail.com>
2022-05-30 03:04:01 +0100

    write protected route component

---
+++ packages/round-manager/src/features/auth/ProtectedRoute.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { Outlet, useOutletContext } from 'react-router-dom'
+
+import { useGetWeb3Query, Web3Instance } from "../auth/web3Service"
+
+
+export default function ProtectedRoute() {
+  const [skip, setSkip] = useState(true)
+
+  const { data, error, refetch, isSuccess } = useGetWeb3Query(undefined, { skip })
+
+  const connectHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setSkip(false)
+    refetch()
+  };
+
+  return !isSuccess ? (
+    <div className="flex h-screen">
+      <main className="m-auto">
+        <button className="font-mono border-black border-4 hover:bg-gray-200 font-bold py-4 px-8 rounded" type="button" onClick={connectHandler}>
+          Connect Wallet
+        </button>
+        <p className="text-red-600 mt-4">{error?.toString()}</p>
+      </main>
+    </div>
+  ) : <Outlet context={data} />
+}
+
+export function useWeb3() {
+  return useOutletContext<Web3Instance>()
+}
---
+++ packages/round-manager/src/features/auth/web3Service.ts
@@ -0,0 +1,57 @@
+import { ethers } from "ethers"
+import { global } from "../../global"
+import { api } from "../../api"
+
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+export type Web3Instance = {
+  account: string;
+  chainId: number;
+}
+
+const VALID_NETWORK_NAME = "Goerli";
+const VALID_CHAIN_ID = 5;
+const LOCAL_CHAIN_ID = 1337;
+
+export const web3Api = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getWeb3: builder.query<Web3Instance, void>({
+      queryFn: async () => {
+        try {
+          if (!window.ethereum) {
+            return { error: "not a web3 browser" }
+          }
+
+          global.web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+
+          const accounts: Array<string> = await window.ethereum.request({ method: "eth_requestAccounts" })
+
+          const { chainId } = await global.web3Provider!.getNetwork()
+
+          if (chainId !== VALID_CHAIN_ID && chainId !== LOCAL_CHAIN_ID) {
+            return { error: `wrong network, please connect to ${VALID_NETWORK_NAME}` };
+          }
+
+          // reload page on wallet events
+          window.ethereum.on("chainChanged", () => window.location.reload())
+          window.ethereum.on("accountsChanged", () => window.location.reload())
+          window.ethereum.on("disconnect", () => window.location.reload())
+
+          return { data: { account: accounts[0], chainId } }
+
+        } catch (err) {
+          console.log("error", err);
+          return { error: "Unable to connect web3 account" }
+        }
+      },
+    }),
+  }),
+  overrideExisting: false
+})
+
+export const { useGetWeb3Query } = web3Api
