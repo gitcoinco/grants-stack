d8a0d49549711b7b89eb18cac309ced8c8b442ac
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-05-31 14:08:48 +0530

    chore: update address

--- packages/contracts/scripts/config/round.config.ts
+++ packages/contracts/scripts/config/round.config.ts
@@ -10,7 +10,7 @@ type DeployParams = Record<string, NetworkParams>;
 // Any timr
 export const params: DeployParams = {
   goerli: {
-    grantRoundImplementationContract: '0xf96Ed3cc3de33002Ac1A65C6ED902C2D9a724c25',
+    grantRoundImplementationContract: '0xc2B040cdd5fba17779ca2d81c4214d590Db885A9',
     grantRoundFactoryContract: '0x191DE462AFfcD7c45db63A80756d6eEdD1a66709',
     bulkVoteContract: '0xc76Ea06e2BC6476178e40E2B40bf5C6Bf3c40EF6'
   },
