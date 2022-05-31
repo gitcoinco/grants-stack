da4837766a2a657cf4744b182caa5afd7910f2b5
Chibuotu Amadi <chibuotu@gmail.com>
2022-05-31 04:27:28 +0100

    add loading state to protectedroute

--- packages/round-manager/src/features/auth/ProtectedRoute.tsx
+++ packages/round-manager/src/features/auth/ProtectedRoute.tsx
@@ -4,7 +4,7 @@ import { useGetWeb3Query, Web3Instance } from "../auth/web3Service"
 
 
 export default function ProtectedRoute() {
-  const { data, error, refetch, isSuccess } = useGetWeb3Query()
+  const { data, error, refetch, isSuccess, isFetching, isLoading } = useGetWeb3Query()
 
   const connectHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
@@ -14,10 +14,15 @@ export default function ProtectedRoute() {
   return !isSuccess ? (
     <div className="flex h-screen">
       <main className="m-auto">
-        <button className="font-mono border-black border-4 hover:bg-gray-200 font-bold py-4 px-8 rounded" type="button" onClick={connectHandler}>
-          Connect Wallet
-        </button>
-        <p className="text-red-600 mt-4">{error?.toString()}</p>
+        {isFetching || isLoading
+          ? <p>Loading...</p>
+          : <div>
+              <button className="font-mono border-black border-4 hover:bg-gray-200 font-bold py-4 px-8 rounded" type="button" onClick={connectHandler}>
+                Connect Wallet
+              </button>
+              <p className="text-red-600 mt-4">{error?.toString()}</p>
+            </div>
+        }
       </main>
     </div>
   ) : <Outlet context={data} />
