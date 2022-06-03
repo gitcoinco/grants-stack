d9fe9dce0b5fbb59457d7ca93043c2ee4948fab8
Tim Schultz <schultz.timothy52@gmail.com>
2022-06-03 10:43:14 -0600

    added link to header logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { shallowEqual, useSelector, useDispatch } from "react-redux";
 import { RootState } from "../reducers";
 import { initializeWeb3 } from "../actions/web3";
-import { newGrantPath } from "../routes";
+import { grantsPath, newGrantPath } from "../routes";
 import Button, { ButtonVariants } from "./base/Button";
 import Plus from "./icons/Plus";
 import colors from "../styles/colors";
@@ -28,14 +28,16 @@ export default function Header() {
   };
   return (
     <header className="justify-between container mx-auto border-b mb-4 sm:flex xs:flex-col sm:h-20 xs:h-40">
-      <div className="flex items-center">
-        <img
-          className="py-4"
-          alt="Gitcoin Logo"
-          src="./assets/gitcoin-logo.svg"
-        />
-        <h3 className="ml-6">Grant Hub</h3>
-      </div>
+      <Link to={grantsPath()}>
+        <div className="flex items-center">
+          <img
+            className="py-4"
+            alt="Gitcoin Logo"
+            src="./assets/gitcoin-logo.svg"
+          />
+          <h3 className="ml-6 mt-1">Grant Hub</h3>
+        </div>
+      </Link>
       {!props.web3Initialized && (
         <Button variant={ButtonVariants.outline} onClick={() => connectHandler}>
           Connect Wallet
