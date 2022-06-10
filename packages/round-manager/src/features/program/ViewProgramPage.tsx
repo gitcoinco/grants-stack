3bdbebdb678a59955820ed1a4f7f7623b7f01942
Chibuotu Amadi <chibuotu@gmail.com>
2022-06-10 15:16:16 +0100

    add rounds to program detail page

--- packages/round-manager/src/features/program/ViewProgramPage.tsx
+++ packages/round-manager/src/features/program/ViewProgramPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams, Link } from "react-router-dom"
 import { Button } from "../common/styles"
 import { useWeb3 } from "../common/ProtectedRoute"
 import { useListProgramsQuery } from "../api/services/program"
+import { useListRoundsQuery } from "../api/services/round"
 
 
 export default function ViewProgram() {
@@ -13,6 +14,20 @@ export default function ViewProgram() {
   const { program } = useListProgramsQuery(account, {
     selectFromResult: ({ data }) => ({ program: data?.find((program) => program.id === id) }),
   })
+
+  const {
+    data: rounds,
+    isLoading: isRoundsLoading,
+    isSuccess: isRoundsFetched
+  } = useListRoundsQuery({ account, programId: id })
+
+  const roundItems = rounds?.map((round, index) =>
+    <Link to={`/round/${round.id}`} key={index}>
+      <button className="w-60 h-60 border-4 border-black bg-gray-300 hover:bg-gray-200 text-2xl">
+        {round.metadata!.name}
+      </button>
+    </Link>
+  )
 
   const goBack = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
@@ -37,16 +52,14 @@ export default function ViewProgram() {
           </div><br />
           <h2 className="text-3xl my-8">My Rounds</h2>
           <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-4 mb-8">
-            <Link to="/round/12">
-              <button className="w-60 h-60 border-4 border-black bg-gray-300 hover:bg-gray-200 text-2xl">
-                Round Name
-              </button>
-            </Link>
-            <Link to={`/round/create?programId=${program?.id}`}>
-              <button className="w-60 h-60 rounded-full border-4 border-black hover:bg-gray-200 text-2xl">
-                Create Round
-              </button>
-            </Link>
+            {roundItems}
+            {isRoundsFetched &&
+              <Link to={`/round/create?programId=${program?.id}`}>
+                <button className="w-60 h-60 rounded-full border-4 border-black hover:bg-gray-200 text-2xl">
+                  Create Round
+                </button>
+              </Link>}
+            {isRoundsLoading && <p>Fetching your grant rounds...</p>}
           </div>
           <Button type="button" onClick={goBack}>Back</Button>
         </div>
