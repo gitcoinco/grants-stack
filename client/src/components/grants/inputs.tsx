80c6035106cca528b16f78ad4ae4ad7d87cb5df0
tim-james <107402524+tim-james@users.noreply.github.com>
2022-06-15 15:33:41 -0600

    moved img input into its own file

--- client/src/components/grants/inputs.tsx
+++ client/src/components/grants/inputs.tsx
@@ -97,25 +97,3 @@ export function TextArea({
     </div>
   );
 }
-
-export function ImageInput({
-  label,
-  imgHandler,
-}: {
-  label: string;
-  imgHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}) {
-  return (
-    <div className="mt-6">
-      <label className="block text-xs mb-2" htmlFor={label}>
-        {label}
-      </label>
-      <input
-        type="file"
-        name="file"
-        onChange={(e) => imgHandler(e)}
-        accept=".png,.jpg"
-      />
-    </div>
-  );
-}
