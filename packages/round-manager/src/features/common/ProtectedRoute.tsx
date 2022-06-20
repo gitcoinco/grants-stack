b1c32c7265b6f8fd82e5f1303c66174402106147
Chibuotu Amadi <chibuotu@gmail.com>
2022-06-20 22:50:24 +0100

    add ui styling to wallet connection page

--- packages/round-manager/src/features/common/ProtectedRoute.tsx
+++ packages/round-manager/src/features/common/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import { Outlet, useOutletContext } from 'react-router-dom'
 
 import { useGetWeb3Query } from "../api/services/web3"
 import { Web3Instance } from "../api/types"
+import { ReactComponent as GitcoinLogo } from "../../assets/gitcoinlogo.svg"
 
 
 /**
@@ -9,7 +10,7 @@ import { Web3Instance } from "../api/types"
  * It prompts a user to connect wallet if no web3 instance is found.
  */
 export default function ProtectedRoute() {
-  const { data, error, refetch, isSuccess, isFetching, isLoading } = useGetWeb3Query()
+  const { data, refetch, isSuccess, isFetching, isLoading } = useGetWeb3Query()
 
   const connectHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
@@ -19,26 +20,24 @@ export default function ProtectedRoute() {
   return !isSuccess ? (
     <div>
       <main className="bg-[#0E0333] h-screen">
-        {!isFetching && !isLoading &&
-          <>
-          <div className="flex left-0 h-full fixed bg-white z-10">
-            <div className="m-auto pl-20">
-              <h1 className="text-6xl my-2.5">Round Manager</h1>
-              <p className="text-base my-4">
-                As a round operator you can manage high-impact<br/>
-                grant programs and distribute funds across different<br/>
-                rounds and voting mechanisms.
-              </p>
-              <button className="bg-[#0E0333] font-bold mt-10 py-4 px-8 rounded text-white" type="button" onClick={connectHandler}>
-                Connect Wallet
-              </button>
-              <p className="text-red-600 mt-4">{error?.toString()}</p>
-            </div>
+        <div className="flex left-0 h-full fixed bg-white z-10">
+          <div>
+            <GitcoinLogo className="ml-20 mt-3.5 absolute" />
           </div>
-          <div className="left-0 w-[55%] h-full fixed overflow-x-hidden skew-x-[-10deg] bg-white"></div>
-          <div className="right-0 w-1/2 h-full fixed overflow-x-hidden"></div>
-          </>
-        }
+          <div className="m-auto ml-20">
+            <h1 className="text-[64px] font-display">Round Manager</h1>
+            <p className="text-2xl my-2 text-[#757087]">
+              As a round operator you can manage high-impact<br />
+              grant programs and distribute funds across different<br />
+              rounds and voting mechanisms.
+            </p>
+            <button type="button" className="bg-[#0E0333] font-bold mt-8 py-4 px-8 rounded text-white" onClick={connectHandler}>
+              {(!isFetching || !isLoading) ? 'Connect Wallet' : 'Connecting..'}
+            </button>
+          </div>
+        </div>
+        <div className="left-0 w-[55%] h-full fixed overflow-x-hidden skew-x-[-10deg] bg-white"></div>
+        <div className="right-0 w-1/2 h-full fixed overflow-x-hidden"></div>
       </main>
     </div>
   ) : <Outlet context={data} />
