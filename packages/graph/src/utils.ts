e7fc64fd81edf13ceeb8cb9bf742ed253f9623a2
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-06-20 17:15:02 +0530

    feat: indexing NewProjectApplication Event

--- packages/graph/src/utils.ts
+++ packages/graph/src/utils.ts
@@ -1,6 +1,11 @@
 import { crypto } from '@graphprotocol/graph-ts'
 import { ByteArray } from '@graphprotocol/graph-ts';
 
+/**
+ * Returns keccak256 of array after elements are joined by '-'
+ * @param Array<String>
+ * @returns keccak256
+ */
 export function generateID(array: Array<string>): string {
   return crypto.keccak256(
     ByteArray.fromHexString(array.join('-'))
