052af6f2fcfce00c35ad280fa764b76da53d6904
schultztimothy <schultz.timothy52@gmail.com>
2022-06-23 10:44:50 -0600

    added project img utility function and updated reference from grant to project

--- client/src/components/grants/Card.tsx
+++ client/src/components/grants/Card.tsx
@@ -7,7 +7,7 @@ import { grantPath } from "../../routes";
 import Lightning from "../icons/Lightning";
 import colors from "../../styles/colors";
 import TextLoading from "../base/TextLoading";
-import { Metadata } from "../../types";
+import { getProjectImage } from "../../utils/components";
 
 function Card({ projectId }: { projectId: number }) {
   const dispatch = useDispatch();
@@ -30,19 +30,12 @@ function Card({ projectId }: { projectId: number }) {
     }
   }, [dispatch, props.ipfsInitialized, projectId, props.currentProject]);
 
-  const checkProject = (loading: boolean, project?: Metadata) => {
-    if (loading || (project && !project.projectImg)) {
-      return "./assets/card-img.png";
-    }
-    return `https://ipfs.io/ipfs/${project?.projectImg}`;
-  };
-
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg my-6">
       <Link to={grantPath(projectId)}>
         <img
           className="w-full"
-          src={checkProject(props.loading, props.currentProject)}
+          src={getProjectImage(props.loading, props.currentProject)}
           alt="project banner"
         />
         <div className="py-4 relative text-center">
