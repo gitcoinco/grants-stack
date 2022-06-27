d25a90e78039d2d6b83425c966b5eaa8bfd92c9b
Andrea Franz <andrea@gravityblast.com>
2022-06-27 15:02:39 +0200

    add upgrades deps

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -6,6 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "@openzeppelin/hardhat-upgrades";
 
 dotenv.config();
 
