f0fe4c56cf0ec02caa002ec8fc1e52168e5fd50e
Andrea Franz <andrea@gravityblast.com>
2022-06-29 13:12:37 +0200

    new contract address on goerli

--- client/src/contracts/deployments.ts
+++ client/src/contracts/deployments.ts
@@ -5,7 +5,7 @@ export const chains: { [key: number]: string } = {
 
 export const addresses: { [key: string]: any } = {
   goerli: {
-    projectRegistry: "0xc755eFc2168E959B50F1B2F0918DBEcC709956F4",
+    projectRegistry: "0xf47669845D97f66922058430f01F24AdA28Aaf36",
   },
   optimisticKovan: {
     projectRegistry: "0x46Da9bbe66d0FA187b46371D850245838D3c6850",
