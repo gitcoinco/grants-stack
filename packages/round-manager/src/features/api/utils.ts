ea3aa416c9f1058bc2cb45836c4babfed733083f
Chibuotu Amadi <chibuotu@gmail.com>
2022-07-07 04:33:56 +0100

    feat: add function to pin data to ipfs

--- packages/round-manager/src/features/api/utils.ts
+++ packages/round-manager/src/features/api/utils.ts
@@ -1,6 +1,8 @@
 import { create as IPFSCreate } from "ipfs-core"
 
 import { global } from "../../global"
+import { IPFSObject } from "./types"
+
 
 /**
  * Fetch data from IPFS
@@ -20,6 +22,63 @@ export const fetchFromIPFS = async (cid: string) => {
   }
 
   return content
+}
+
+
+/**
+ * Pin data to IPFS
+ * The data could either be a file or a JSON object
+ * 
+ * @param obj - the data to be pinned on IPFS
+ * @returns the unique content identifier that points to the data
+ */
+export const pinToIPFS = (obj: IPFSObject) => {
+
+  let params: any = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${process.env.REACT_APP_PINATA_JWT}`
+    },
+    body: {
+      pinataMetadata: obj.metadata,
+      pinataOptions: {
+        cidVersion: 1
+      }
+    }
+  }
+
+  if (typeof obj.content === 'object') {
+    // content is a JSON object
+    return fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
+      ...params,
+      headers: {
+        ...params.headers,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ ...params.body, pinataContent: obj.content })
+    }).then(resp => {
+      if (resp.ok) {
+        return resp.json()
+      }
+
+      return Promise.reject(resp)
+    });
+  } else {
+    // content is a blob
+    const fd = new FormData();
+    fd.append("file", obj.content)
+    fd.append("pinataOptions", JSON.stringify(params.body.pinataOptions))
+    fd.append("pinataMetadata", JSON.stringify(params.body.pinataMetadata))
+
+    return fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", { ...params, body: fd }
+    ).then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      }
+
+      return Promise.reject(resp)
+    })
+  }
 }
 
 
