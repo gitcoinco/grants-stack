79962b5f7d83495fea4229f2f386b8c5d817ddd1
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-07-14 01:09:49 +0530

    feat: index program contract public variables

--- packages/graph/src/program/factory.ts
+++ packages/graph/src/program/factory.ts
@@ -1,9 +1,12 @@
-import { 
+import {
   ProgramCreated as ProgramCreatedEvent
 } from "../../generated/Program/ProgramFactory";
 
 import { ProgramImplementation } from  "../../generated/templates";
-import { Program } from "../../generated/schema";
+import { MetaPtr, Program } from "../../generated/schema";
+import {
+  ProgramImplementation  as ProgramImplementationContract
+} from "../../generated/templates/ProgramImplementation/ProgramImplementation";
 
 
 /**
@@ -18,6 +21,22 @@ export function handleProgramCreated(event: ProgramCreatedEvent): void {
   if (!program) {
     // create if program does not exist
     program = new Program(programContractAddress.toHex());
+
+    // load program contract
+    const programContract = ProgramImplementationContract.bind(programContractAddress);
+
+    // set metaPtr
+    const metaPtrId = ['metaPtr', programContractAddress.toHex()].join('-');
+
+    let programMetaPtr = programContract.metaPtr();
+
+    let metaPtr = new MetaPtr(metaPtrId);
+
+    metaPtr.protocol = programMetaPtr.getProtocol().toI32();
+    metaPtr.pointer = programMetaPtr.getPointer().toString();
+    metaPtr.save();
+
+    program.metaPtr = metaPtr.id;
   }
 
   program.save();
