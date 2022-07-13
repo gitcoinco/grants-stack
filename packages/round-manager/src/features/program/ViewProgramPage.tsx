362404fc3f9071395aaab058d379a0221eec49c5
Chibuotu Amadi <chibuotu@gmail.com>
2022-07-13 18:29:25 +0100

    chore: rename startTime & endTime

--- packages/round-manager/src/features/program/ViewProgramPage.tsx
+++ packages/round-manager/src/features/program/ViewProgramPage.tsx
@@ -49,7 +49,7 @@ export default function ViewProgram() {
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
             </svg>
             <span className="text-grey-400 my-auto">Round: </span>
-            <span className="my-auto">{`${round.startTime.toLocaleDateString()} - ${round.endTime.toLocaleDateString()}`}</span>
+            <span className="my-auto">{`${round.roundStartTime.toLocaleDateString()} - ${round.roundEndTime.toLocaleDateString()}`}</span>
           </p>
         </div>
       </div>
