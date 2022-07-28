844e32877b1a75ba2634beef7c75e32ed58add87
Chibuotu Amadi <chibuotu@gmail.com>
2022-07-28 21:39:33 +0100

    feat: hi-fi design application detail

--- packages/round-manager/src/features/common/Navbar.tsx
+++ packages/round-manager/src/features/common/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { PlusSmIcon } from "@heroicons/react/solid"
 
 import { ReactComponent as GitcoinLogo } from "../../assets/gitcoinlogo-white.svg"
+import { ReactComponent as RoundManagerLogo } from "../../assets/round-manager-logo.svg"
 import { Button } from "../common/styles"
 
 
@@ -10,7 +11,7 @@ export interface NavbarProps {
 }
 
 
-export default function Navbar({ programCta = false }: NavbarProps) {
+export default function Navbar({ programCta = true }: NavbarProps) {
 
   return (
     <nav className="bg-grey-500">
@@ -19,17 +20,20 @@ export default function Navbar({ programCta = false }: NavbarProps) {
           <div className="flex">
             <Link to="/" className="flex-shrink-0 flex items-center">
               <GitcoinLogo className="block h-8 w-auto" />
-              <h5 className="pl-2.5 md:pl-6 text-white hidden lg:block md:block">Round Manager</h5>
+              <div className="hidden lg:block md:block">
+                <span className="mx-6 text-grey-400">|</span>
+                <RoundManagerLogo className="lg:inline-block md:inline-block" />
+              </div>
             </Link>
           </div>
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              { programCta &&
+              {programCta &&
                 <Link to="/program/create">
                   <Button
-                    $variant="outline"
+                    $variant="solid"
                     type="button"
-                    className="inline-flex items-center px-4 py-1.5 border border-white shadow-sm text-xs font-medium rounded text-grey-500 bg-white"
+                    className="inline-flex items-center px-4 py-1.5 shadow-sm text-sm rounded"
                   >
                     <PlusSmIcon className="h-5 w-5 mr-1" aria-hidden="true" />
                     Create Program
