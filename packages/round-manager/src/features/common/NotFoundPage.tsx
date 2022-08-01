60280e12894501262a3cb14f4ff8bcd30ad8e7d2
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-08-01 09:54:49 +0100

    chore: update 404 as per design

--- packages/round-manager/src/features/common/NotFoundPage.tsx
+++ packages/round-manager/src/features/common/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import { Button } from "../common/styles"
-import { HomeIcon } from "@heroicons/react/solid"
+import { ReactComponent as NotFoundBanner } from "../../assets/404.svg"
 
 
 export default function NotFoundPage() {
@@ -10,20 +10,29 @@ export default function NotFoundPage() {
     <>
       <Navbar />
       <main>
-        <div className="flex h-screen">
-          <div className="m-auto">
-            <h1 className="my-5">Error 404</h1>
-            <h2 className="my-5">Page Not Found</h2>
+        <div className="flex h-screen pt-8">
+          <div className="m-auto text-center mt-5">
+            <h1 className="my-5 text-sm text-red-100 font-bold">404 ERROR</h1>
+            <h2 className="my-5 text-4xl">Uh oh! You might be a little lost</h2>
+
+            <p className='text-grey-400 mb-0'>
+              It looks like the page you’re looking for doesn’t exist.
+            </p>
+            <p className='text-grey-400 mt-1 mb-5'>
+              For support, contact us on <a href="https://discord.com/invite/gitcoin">Discord.</a>
+            </p>
+            
             <Link to="/">
               <Button
-                $variant="solid"
+                $variant="outline"
                 type="button"
-                className="inline-flex items-center px-20 py-2.5 my-3 shadow-sm text-sm rounded"
+                className="px-3 bg-violet-100 text-violet-400 border-0 text-xs"
               >
-                <HomeIcon className="h-5 w-5 mr-2.5" aria-hidden="true" />
-                Go to Home
+                Go back home
               </Button>
             </Link>
+
+            <NotFoundBanner className='max-w-full	' />
           </div>
         </div>
       </main>
