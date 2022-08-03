b2710d303b57390c6f40915e74c9cd69bc79144c
Daniele Salatti <me@danielesalatti.com>
2022-08-02 17:12:50 -0700

    Add basic Datadog integration

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router";
 import thunkMiddleware from "redux-thunk";
 import { Provider } from "react-redux";
 import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import Datadog from "react-datadog";
 import {
   createStore,
   applyMiddleware,
@@ -71,23 +72,38 @@ const theme = extendTheme({ colors });
 root.render(
   // <React.StrictMode>
   <ErrorBoundary>
-    <ChakraProvider theme={theme} resetCSS={false}>
-      <Provider store={store}>
-        <ReduxRouter history={history} store={store}>
-          <Layout>
-            <Routes>
-              <Route path={slugs.root} element={<Landing />} />
-              <Route path={slugs.grants} element={<ProjectsList />} />
-              <Route path={slugs.grant} element={<Project />} />
-              <Route path={slugs.newGrant} element={<NewProject />} />
-              <Route path={slugs.edit} element={<EditProject />} />
-              <Route path={slugs.round} element={<RoundShow />} />
-              <Route path={slugs.roundApplication} element={<RoundApply />} />
-            </Routes>
-          </Layout>
-        </ReduxRouter>
-      </Provider>
-    </ChakraProvider>
+    <Datadog
+      applicationId="5c45f4d1-3258-4206-bbdb-b73c9af5f340"
+      clientToken="pubf505ad0eca99217895614fb3000dea1f"
+      site="datadoghq.eu"
+      service="grant-hub"
+      // Specify a version number to identify the deployed version of your application in Datadog
+      // version="1.0.0"
+      sampleRate={100}
+      premiumSampleRate={100}
+      // trackInteractions
+      sessionReplayRecording={false}
+      // Uncomment if session replay is enabled
+      // defaultPrivacyLevel="mask-user-input"
+    >
+      <ChakraProvider theme={theme} resetCSS={false}>
+        <Provider store={store}>
+          <ReduxRouter history={history} store={store}>
+            <Layout>
+              <Routes>
+                <Route path={slugs.root} element={<Landing />} />
+                <Route path={slugs.grants} element={<ProjectsList />} />
+                <Route path={slugs.grant} element={<Project />} />
+                <Route path={slugs.newGrant} element={<NewProject />} />
+                <Route path={slugs.edit} element={<EditProject />} />
+                <Route path={slugs.round} element={<RoundShow />} />
+                <Route path={slugs.roundApplication} element={<RoundApply />} />
+              </Routes>
+            </Layout>
+          </ReduxRouter>
+        </Provider>
+      </ChakraProvider>
+    </Datadog>
   </ErrorBoundary>
   // </React.StrictMode>
 );
