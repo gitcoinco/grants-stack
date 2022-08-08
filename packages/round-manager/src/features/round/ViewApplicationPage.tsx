c7d3ead7315384aff5f635b2ae68b151a730e4bc
Chibuotu Amadi <chibuotu@gmail.com>
2022-08-08 23:45:29 +0100

    fix: load application images with ipfs hash

--- packages/round-manager/src/features/round/ViewApplicationPage.tsx
+++ packages/round-manager/src/features/round/ViewApplicationPage.tsx
@@ -94,16 +94,20 @@ export default function ViewApplicationPage() {
           </div>
           <div>
             <div>
-              {/* TODO: after applyToRound script is updated,
-              fetch image via ipfs e.g {`https://gitcoin.mypinata.cloud/ipfs/${application?.project.bannerImg}`} */}
-              <img className="h-32 w-full object-cover lg:h-80 rounded" src={application?.project.bannerImg} alt="" />
+              <img
+                className="h-32 w-full object-cover lg:h-80 rounded"
+                src={`https://gitcoin.mypinata.cloud/ipfs/${application?.project.bannerImg}`}
+                alt=""
+              />
             </div>
             <div className="pl-4 sm:pl-6 lg:pl-8">
               <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
                 <div className="flex">
-                  {/* TODO: after applyToRound script is updated,
-                      fetch image via ipfs  e.g {`https://gitcoin.mypinata.cloud/ipfs/${application?.project.logoImg}`} */}
-                  <img className="h-24 w-24 rounded-full ring-4 ring-white bg-white sm:h-32 sm:w-32" src={application?.project.logoImg} alt="" />
+                  <img
+                    className="h-24 w-24 rounded-full ring-4 ring-white bg-white sm:h-32 sm:w-32"
+                    src={`https://gitcoin.mypinata.cloud/ipfs/${application?.project.logoImg}`}
+                    alt=""
+                  />
                 </div>
                 <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
                   <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
