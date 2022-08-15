8e4ec46326f9a33a202a6f897499ed5e28d080c0
Andrea Franz <andrea@gravityblast.com>
2022-08-15 10:42:52 +0200

    fix deploy script

--- contracts/scripts/migrations/001-deploy-project-registry.ts
+++ contracts/scripts/migrations/001-deploy-project-registry.ts
@@ -1,6 +1,7 @@
 import hre, { ethers, upgrades } from "hardhat";
 import { prompt, prettyNum } from "../../lib/utils";
-import { HardwareSigner } from "../../lib/HardwareSigner";
+// import HardwareSigner from "../../lib/HardwareSigner";
+const { HardwareSigner } = require("../../lib/HardwareSigner");
 
 async function main() {
   const network = await ethers.provider.getNetwork();
@@ -14,7 +15,7 @@ async function main() {
   const account = new HardwareSigner(ethers.provider, null, "m/44'/60'/0'/0/0");
   const accountAddress = await account.getAddress();
 
-  const balance = await ethers.provider.getBalance(account.address);
+  const balance = await ethers.provider.getBalance(accountAddress);
 
   console.log(`chainId: ${network.chainId}`);
   console.log(`network: ${networkName} (from ethers: ${network.name})`);
@@ -24,10 +25,8 @@ async function main() {
   await prompt("do you want to deploy the ProjectRegistry contract?");
   console.log("deploying...");
 
-  const ProjectRegistry = await ethers.getContractFactory("ProjectRegistry");
-  const instance = await upgrades.deployProxy(ProjectRegistry, [], {
-    type: 1,
-  });
+  const ProjectRegistry = await ethers.getContractFactory("ProjectRegistry", account);
+  const instance = await upgrades.deployProxy(ProjectRegistry, []);
   console.log("tx hash", instance.deployTransaction.hash);
   await instance.deployed();
 
