5ec32560afdf6828e8055d3a9cc46ddb0a32a391
David Hayes <david@focusedlabs.io>
2022-08-29 16:15:16 -0400

    refactor: use context API for new ViewProgramPage code
    [#298]

--- packages/round-manager/src/features/program/ViewProgramPage.tsx
+++ packages/round-manager/src/features/program/ViewProgramPage.tsx
@@ -1,30 +1,30 @@
-import { Link, useParams } from "react-router-dom"
-import { ChevronRightIcon, PlusIcon, PlusSmIcon, UserIcon } from "@heroicons/react/solid"
-import { RefreshIcon } from "@heroicons/react/outline"
+import {Link, useParams} from "react-router-dom";
+import {ChevronRightIcon, PlusIcon, PlusSmIcon, UserIcon} from "@heroicons/react/solid";
+import {RefreshIcon} from "@heroicons/react/outline";
 
-import { Button } from "../common/styles"
-import { useWallet } from "../common/Auth"
-import { useListRoundsQuery } from "../api/services/round"
-import Navbar from "../common/Navbar"
-import Footer from "../common/Footer"
-import { abbreviateAddress } from "../api/utils"
-import { datadogLogs } from "@datadog/browser-logs"
-import { useEffect, useState } from "react"
-import NotFoundPage from "../common/NotFoundPage"
-import AccessDenied from "../common/AccessDenied"
+import {Button} from "../common/styles";
+import {useWallet} from "../common/Auth";
+import {useListRoundsQuery} from "../api/services/round";
+import Navbar from "../common/Navbar";
+import Footer from "../common/Footer";
+import {abbreviateAddress} from "../api/utils";
+import {datadogLogs} from "@datadog/browser-logs";
+import {useEffect, useState} from "react";
+import NotFoundPage from "../common/NotFoundPage";
+import AccessDenied from "../common/AccessDenied";
 
-import { usePrograms } from "../../context/ProgramContext"
+import {usePrograms} from "../../context/ProgramContext";
 
 export default function ViewProgram() {
 
-  datadogLogs.logger.info('====> Route: /program/:id')
-  datadogLogs.logger.info(`====> URL: ${window.location.href}`)
+  datadogLogs.logger.info("====> Route: /program/:id");
+  datadogLogs.logger.info(`====> URL: ${window.location.href}`);
 
-  const { id } = useParams()
+  const {id} = useParams();
 
-  const { address, provider } = useWallet()
+  const {address, provider} = useWallet();
 
-  const { programs } = usePrograms();
+  const {programs} = usePrograms();
 
   const programToRender = programs.find(program => program.id === id);
 
@@ -32,24 +32,24 @@ export default function ViewProgram() {
     data: rounds,
     isLoading: isRoundsLoading,
     isSuccess: isRoundsFetched
-  } = useListRoundsQuery({ address, signerOrProvider: provider, programId: id })
+  } = useListRoundsQuery({address, signerOrProvider: provider, programId: id});
 
-  let [programExists, setProgramExists] = useState(true)
-  let [hasAccess, setHasAccess] = useState(true)
+  let [programExists, setProgramExists] = useState(true);
+  let [hasAccess, setHasAccess] = useState(true);
 
   useEffect(() => {
     if (isRoundsFetched) {
-      setProgramExists(!!program)
+      setProgramExists(!!programToRender);
 
-      if(program) {
-        program.operatorWallets.includes(
+      if (programToRender) {
+        programToRender.operatorWallets.includes(
           address?.toLowerCase()
-        ) ? setHasAccess(true) : setHasAccess(false)
+        ) ? setHasAccess(true) : setHasAccess(false);
       } else {
-        setHasAccess(true)
+        setHasAccess(true);
       }
     }
-  }, [isRoundsFetched, program, address])
+  }, [isRoundsFetched, programToRender, address]);
 
 
   const roundItems = rounds ? rounds.map((round, index) =>
@@ -68,10 +68,10 @@ export default function ViewProgram() {
           <div className="grid sm:grid-cols-3">
             <p className="text-xs flex gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-grey-500 my-auto" viewBox="0 0 20 20"
-                  fill="currentColor">
+                   fill="currentColor">
                 <path fillRule="evenodd"
                       d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                      clipRule="evenodd" />
+                      clipRule="evenodd"/>
               </svg>
               <span className="text-grey-400 my-auto">Applications: </span>
               <span className="my-auto" data-testid="application-time-period">
@@ -79,10 +79,11 @@ export default function ViewProgram() {
               </span>
             </p>
             <p className="text-xs flex gap-1 md:ml-8">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 my-auto" viewBox="0 0 20 20" fill="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 my-auto" viewBox="0 0 20 20"
+                   fill="currentColor">
                 <path fillRule="evenodd"
                       d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                      clipRule="evenodd" />
+                      clipRule="evenodd"/>
               </svg>
               <span className="text-grey-400 my-auto">Round: </span>
               <span className="my-auto" data-testid="round-time-period">
@@ -92,7 +93,7 @@ export default function ViewProgram() {
           </div>
         </div>
 
-          <ChevronRightIcon className="h-5 w-5" />
+        <ChevronRightIcon className="h-5 w-5"/>
 
       </div>
     </Link>
@@ -102,7 +103,7 @@ export default function ViewProgram() {
     {
       programToRender?.operatorWallets.map((operatorWallet, index) =>
         <div className="bg-white text-grey-400 pb-2 pr-5" data-testid="program-operator-wallet" key={index}>
-          <UserIcon className="inline h-4 w-4 text-grey-400 mr-1" />
+          <UserIcon className="inline h-4 w-4 text-grey-400 mr-1"/>
           <span className="text-sm text-grey-400" key={index}>
             {abbreviateAddress(operatorWallet)}
           </span>
@@ -124,7 +125,7 @@ export default function ViewProgram() {
       </p>
       <Link to={`/round/create?programId=${programToRender?.id}`}>
         <Button className="my-4 px-4 mt-10">
-          <PlusIcon className="h-4 w-4 inline-flex -translate-y-0.5" aria-hidden="true" /> Create round
+          <PlusIcon className="h-4 w-4 inline-flex -translate-y-0.5" aria-hidden="true"/> Create round
         </Button>
       </Link>
     </div>
@@ -132,18 +133,18 @@ export default function ViewProgram() {
 
   return (
     <>
-      {!programExists && <NotFoundPage />}
-      {!hasAccess && <AccessDenied />}
+      {!programExists && <NotFoundPage/>}
+      {!hasAccess && <AccessDenied/>}
       {programExists && hasAccess &&
         <>
-          <Navbar programCta={true} />
+          <Navbar programCta={true}/>
           <div className="container mx-auto flex flex-col w-screen">
             <header className="flex flex-col justify-center border-b pl-2 md:pl-20 py-6">
               <div className="flex flex-row items-center">
                 <Link to={`/`}>
                   <p className="text-sm text-grey-400 font-semibold">My Programs</p>
                 </Link>
-                <ChevronRightIcon className="h-6 w-6 mx-3 text-sm text-grey-400" aria-hidden="true" />
+                <ChevronRightIcon className="h-6 w-6 mx-3 text-sm text-grey-400" aria-hidden="true"/>
                 <p className="text-sm text-grey-400 font-semibold">Program Details</p>
               </div>
               <h1 className="text-3xl sm:text-[32px] my-2">
@@ -160,9 +161,10 @@ export default function ViewProgram() {
                       <div className="md:mb-8">
                         <div className="flex flex-row justify-between">
                           <p className="font-bold">My Rounds</p>
-                          <Link to={`/round/create?programId=${programToRender?.id}`} className="text-violet-400 font-thin"
+                          <Link to={`/round/create?programId=${programToRender?.id}`}
+                                className="text-violet-400 font-thin"
                                 data-testid="create-round-small-link">
-                            <PlusSmIcon className="h-5 w-5 inline -translate-y-0.5" aria-hidden="true" /> Create round
+                            <PlusSmIcon className="h-5 w-5 inline -translate-y-0.5" aria-hidden="true"/> Create round
                           </Link>
                         </div>
                         {roundItems}
@@ -176,9 +178,9 @@ export default function ViewProgram() {
               {isRoundsFetched && roundItems.length === 0 && noRoundsGroup}
             </main>
           </div>
-          <Footer />
+          <Footer/>
         </>
       }
     </>
-  )
+  );
 }
--- packages/round-manager/src/features/program/__tests__/ViewProgramPage.test.tsx
+++ packages/round-manager/src/features/program/__tests__/ViewProgramPage.test.tsx
@@ -1,61 +1,55 @@
-import ViewProgram from "../ViewProgramPage"
-import { screen } from "@testing-library/react"
-import { useListRoundsQuery } from "../../api/services/round"
-import { useWallet } from "../../common/Auth"
-import { makeProgramData, makeRoundData, renderWithContext } from "../../../test-utils"
-import { faker } from "@faker-js/faker"
-import { Round } from "../../api/types"
+import ViewProgram from "../ViewProgramPage";
+import {screen} from "@testing-library/react";
+import {useListRoundsQuery} from "../../api/services/round";
+import {useWallet} from "../../common/Auth";
+import {makeProgramData, makeRoundData, renderWithContext} from "../../../test-utils";
+import {faker} from "@faker-js/faker";
+import {Program, Round} from "../../api/types";
 
-const programId = faker.datatype.number().toString()
+const programId = faker.datatype.number().toString();
 const useParamsFn = () => ({id: programId});
 
-jest.mock("../../common/Navbar")
-jest.mock("../../common/Auth")
-jest.mock("../../api/services/program")
-jest.mock("../../api/services/round")
+jest.mock("../../common/Navbar");
+jest.mock("../../common/Auth");
+jest.mock("../../api/services/program");
+jest.mock("../../api/services/round");
 jest.mock("@rainbow-me/rainbowkit", () => ({
   ConnectButton: jest.fn(),
-}))
+}));
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useParams: useParamsFn
-}))
-
+}));
 
 describe("<ViewProgram />", () => {
-  let stubProgram = makeProgramData();
+  let stubProgram: Program;
 
   beforeEach(() => {
     jest.clearAllMocks();
 
-    stubProgram = makeProgramData();
+    stubProgram = makeProgramData({id: programId});
 
-    (useWallet as jest.Mock).mockReturnValue({ chain: {}, address: stubProgram.operatorWallets[0] });
-    (useListProgramsQuery as jest.Mock).mockReturnValue({ program: stubProgram });
-    (useListRoundsQuery as jest.Mock).mockReturnValue({ data: [], isLoading: false, isSuccess: true });
+    (useWallet as jest.Mock).mockReturnValue({chain: {}, address: stubProgram.operatorWallets[0]});
+    (useListRoundsQuery as jest.Mock).mockReturnValue({data: [], isLoading: false, isSuccess: true});
   });
 
   it("should display 404 when there no program is found", () => {
-    (useListProgramsQuery as jest.Mock).mockReturnValue({ });
-
-    renderWrapped(<ViewProgram />);
+    renderWithContext(<ViewProgram/>, {programs: []});
     expect(screen.getByText("404 ERROR")).toBeInTheDocument();
-  })
+  });
 
   it("should display access denied when wallet accessing is not program operator", () => {
-    (useWallet as jest.Mock).mockReturnValue({ chain: {} });
+    (useWallet as jest.Mock).mockReturnValue({chain: {}});
 
-    renderWrapped(<ViewProgram />);
+    renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
     expect(screen.getByText("Access Denied!")).toBeInTheDocument();
-  })
+  });
 
   it("displays the program name", async () => {
-    const stubProgram = makeProgramData({id: programId});
+    renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
-    renderWithContext(<ViewProgram />, {programs: [stubProgram]})
-
-    await screen.findByText(stubProgram.metadata!.name)
-  })
+    await screen.findByText(stubProgram.metadata!.name);
+  });
 
   it("displays a list of operator wallets", async () => {
     let operatorWallets = [
@@ -64,93 +58,85 @@ describe("<ViewProgram />", () => {
       faker.finance.ethereumAddress(),
     ];
 
-    const stubProgram = makeProgramData({ id: programId, operatorWallets });
-    (useWallet as jest.Mock).mockReturnValue({ chain: {}, address: stubProgram.operatorWallets[0] });
+    const stubProgram = makeProgramData({id: programId, operatorWallets});
+    (useWallet as jest.Mock).mockReturnValue({chain: {}, address: stubProgram.operatorWallets[0]});
 
-    renderWithContext(<ViewProgram />, {programs: [stubProgram]});
+    renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
-    const wallets = await screen.findAllByTestId("program-operator-wallet")
+    const wallets = await screen.findAllByTestId("program-operator-wallet");
     expect(wallets.length).toEqual(operatorWallets.length);
-  })
+  });
 
   describe("when there are no rounds in the program", () => {
     beforeEach(() => {
-      (useListRoundsQuery as jest.Mock).mockReturnValue({ data: [], isLoading: false, isSuccess: true });
+      (useListRoundsQuery as jest.Mock).mockReturnValue({data: [], isLoading: false, isSuccess: true});
     });
 
     it("displays introductory text on the page", async () => {
-      const stubProgram = makeProgramData({id: programId});
-
-      renderWithContext(<ViewProgram />, {programs: [stubProgram]});
+      renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
       await screen.findAllByTestId("program-details-intro");
-    })
+    });
   });
 
   describe("when there is a round in the program", () => {
-    let stubRound = makeRoundData({ ownedBy: stubProgram.id });
+    let stubRound: Round;
 
     beforeEach(() => {
-      stubRound = makeRoundData({ ownedBy: stubProgram.id });
+      stubRound = makeRoundData({ownedBy: stubProgram.id});
 
-      (useWallet as jest.Mock).mockReturnValue({ chain: {}, address: stubProgram.operatorWallets[0] });
-      (useListProgramsQuery as jest.Mock).mockReturnValue({ program: stubProgram });
-      (useListRoundsQuery as jest.Mock).mockReturnValue({ data: [stubRound], isLoading: false, isSuccess: true });
+      (useWallet as jest.Mock).mockReturnValue({chain: {}, address: stubProgram.operatorWallets[0]});
+      (useListRoundsQuery as jest.Mock).mockReturnValue({data: [stubRound], isLoading: false, isSuccess: true});
     });
 
-
     it("displays round name", async () => {
-      const stubProgram = makeProgramData({id: programId});
-      const stubRound = makeRoundData({ ownedBy: stubProgram.id });
+      const stubRound = makeRoundData({ownedBy: stubProgram.id});
       (useListRoundsQuery as jest.Mock).mockReturnValue({
         data: [stubRound],
         isLoading: false, isSuccess: true
       });
 
-      renderWithContext(<ViewProgram />, {programs: [stubProgram]});
+      renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
       expect(screen.getByText(stubRound.roundMetadata?.name!!)).toBeInTheDocument();
     });
 
     it("displays grant application start and end dates", async () => {
-      const stubProgram = makeProgramData({id: programId});
-      const stubRound = makeRoundData({ ownedBy: stubProgram.id });
+      const stubRound = makeRoundData({ownedBy: stubProgram.id});
       (useListRoundsQuery as jest.Mock).mockReturnValue({
         data: [stubRound],
         isLoading: false, isSuccess: true
       });
 
-      renderWithContext(<ViewProgram />, {programs: [stubProgram]});
+      renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
       const applicationTimePeriod = await screen.findByTestId("application-time-period");
       expect(applicationTimePeriod.textContent).toEqual(`${stubRound?.applicationsStartTime.toLocaleDateString()} - ${stubRound.applicationsEndTime.toLocaleDateString()}`);
     });
 
     it("displays round start and end dates", async () => {
-      const stubProgram = makeProgramData({id: programId});
-      const stubRound = makeRoundData({ ownedBy: stubProgram.id });
+      const stubRound = makeRoundData({ownedBy: stubProgram.id});
       (useListRoundsQuery as jest.Mock).mockReturnValue({
         data: [stubRound],
         isLoading: false, isSuccess: true
       });
 
-      renderWithContext(<ViewProgram />, {programs: [stubProgram]});
+      renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
       const roundTimePeriodElement = await screen.findByTestId("round-time-period");
       expect(roundTimePeriodElement.textContent).toEqual(`${stubRound.roundStartTime.toLocaleDateString()} - ${stubRound.roundEndTime.toLocaleDateString()}`);
     });
 
     it("displays create round link", async () => {
-      const stubProgram = makeProgramData({id: programId});
-      const stubRound = makeRoundData({ ownedBy: stubProgram.id });
+      const stubRound = makeRoundData({ownedBy: stubProgram.id});
       (useListRoundsQuery as jest.Mock).mockReturnValue({
         data: [stubRound],
         isLoading: false, isSuccess: true
       });
 
-      renderWithContext(<ViewProgram />, {programs: [stubProgram]});
+      renderWithContext(<ViewProgram/>, {programs: [stubProgram]});
 
       await screen.findByTestId("create-round-small-link");
-    })
-  })
-})
+    });
+  });
+});
