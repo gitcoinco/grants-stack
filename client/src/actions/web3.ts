3a59f3201e327862a9442d9eb79ef119635d5393
Andrea Franz <andrea@gravityblast.com>
2022-09-01 00:04:43 +0200

    fix lint warnings

--- client/src/actions/web3.ts
+++ client/src/actions/web3.ts
@@ -109,10 +109,7 @@ export const initializeWeb3 =
 
     let t: Web3Type;
     if (window.ethereum) {
-      t = window.ethereum.isStatus
-        ? Web3Type.Status
-        : Web3Type.Generic;
-
+      t = window.ethereum.isStatus ? Web3Type.Status : Web3Type.Generic;
       window.ethereum.on("chainChanged", () => window.location.reload());
       window.ethereum.on("accountsChanged", () => {
         window.location.reload();
@@ -124,5 +121,4 @@ export const initializeWeb3 =
     dispatch(web3Initialized(t));
     dispatch(web3AccountLoaded(address));
     dispatch(web3ChainIDLoaded(chain?.id));
-
   };
