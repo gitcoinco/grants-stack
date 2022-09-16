04aca2bf362523fb2833c6b3a015133f84302caf
Tim <tim.proctor75@gmail.com>
2022-09-16 12:37:09 +0300

    adding coverage (wip)

--- packages/contracts/test/QuadraticFundingVotingStrategy.test.ts
+++ packages/contracts/test/QuadraticFundingVotingStrategy.test.ts
@@ -1,10 +1,11 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { deployContract } from "ethereum-waffle";
-import { isAddress } from "ethers/lib/utils";
+import { BytesLike, isAddress } from "ethers/lib/utils";
 import { artifacts, ethers } from "hardhat";
 import { Artifact } from "hardhat/types";
 import { QuadraticFundingVotingStrategy } from "../typechain";
+import { Wallet } from "ethers";
 
 describe("QuadraticFundingVotingStrategy", function () {
 
@@ -36,6 +37,49 @@ describe("QuadraticFundingVotingStrategy", function () {
       // Deploy QuadraticFundingVotingStrategy contract
       quadraticFundingVotingStrategyArtifact = await artifacts.readArtifact('QuadraticFundingVotingStrategy');
       quadraticFundingVotingStrategy = <QuadraticFundingVotingStrategy>await deployContract(user, quadraticFundingVotingStrategyArtifact, []);
+    });
+
+    describe.only("test: vote", () => {
+      const votes = [
+        [Wallet.createRandom().address, 1, Wallet.createRandom().address],
+        [Wallet.createRandom().address, 2, Wallet.createRandom().address],
+      ];
+
+      const encodedVotes: BytesLike[] = [];
+
+      for (let i = 0; i < votes.length; i++) {
+        encodedVotes.push(
+          ethers.utils.defaultAbiCoder.encode(
+            ["address", "uint256", "address"],
+            votes[i]
+          )
+        );
+      }
+
+      it("invoking Vote with encoded votes SHOULD NOT revert", async () => {
+        expect(quadraticFundingVotingStrategy.vote(encodedVotes, user.address)).to.not.be
+          .reverted;
+      });
+
+      it("invoking vote with encoded votes should emit the Voted event", async () => {
+        const txn = await quadraticFundingVotingStrategy.vote(encodedVotes, user.address);
+        const receipt = await txn.wait();
+
+        if (receipt.events) {
+          for (let i = 0; i < receipt.events.length; i++) {
+            const event = receipt.events[i];
+            if (event.event === 'Voted') {
+              expect(event.args).to.deep.equal({
+                token: votes[i][0],
+                amount: votes[i][1],
+                voter: user.address,
+                grantAddress: votes[i][2],
+                roundAddress: quadraticFundingVotingStrategy.address,
+              });
+            }
+          }
+        }
+      });
     });
 
   })
