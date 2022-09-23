c4f0ac133e56158903ba44064ae72245699e3427
Jaxcoder <codenamejason@outlook.com>
2022-09-23 12:44:10 -0400

    add test file - todo

---
+++ client/src/components/base/__tests__/Apply.test.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+
+describe("<Apply />", () => {
+    describe("with a valid round", () => {
+        let store: any;
+
+        describe("application status", () => {
+            test("should be true", async () => {
+                expect(true).toBe(true);
+            });
+        });
+    });
+});
