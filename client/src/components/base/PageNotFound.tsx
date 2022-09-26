52e8e39adfdb2f06251b35b6a1886fab91ffbf85
Daniele Salatti <me@danielesalatti.com>
2022-09-26 14:11:42 +0200

    404 UI fixes

--- client/src/components/base/PageNotFound.tsx
+++ client/src/components/base/PageNotFound.tsx
@@ -3,22 +3,35 @@ import Button, { ButtonVariants } from "./Button";
 
 function PageNotFound() {
   return (
-    <div className="h-full w-full absolute flex flex-col justify-center items-center">
+    <div className="w-full flex flex-col justify-center items-center">
       <div className="w-full lg:w-3/5 sm:w-2/3">
-        <img alt="404 Page not Found" src="./assets/404.png" />
         <div className="p-8 flex flex-col">
-          <p className="mt-4 mb-12 w-full text-center">
-            The page you are looking for does not exist.
+          <p className="font-semibold text-red-700 uppercase text-center">
+            404 Error
           </p>
-          <Link to="/">
-            <Button
-              styles={["w-full justify-center"]}
-              variant={ButtonVariants.primary}
+          <p className="mt-2 w-full text-center">
+            It looks like the page you are looking for doesn&apos;t exist.
+            <br />
+            For support, contact us on{" "}
+            <a
+              className="text-primary-background font-medium"
+              href="https://discord.gg/gitcoin"
             >
-              Home
-            </Button>
-          </Link>
+              Discord
+            </a>
+          </p>
+          <div className="w-full flex justify-center">
+            <Link to="/">
+              <Button
+                styles={["justify-center"]}
+                variant={ButtonVariants.secondary}
+              >
+                Go back Home
+              </Button>
+            </Link>
+          </div>
         </div>
+        <img alt="404 Page not Found" src="./assets/404.png" />
       </div>
     </div>
   );
