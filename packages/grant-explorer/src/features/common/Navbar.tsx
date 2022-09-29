6960f897705aeaa1d927df322dac253bd7bb50f6
Josef Vacek <vacekj@outlook.com>
2022-09-29 18:55:06 +0200

    feat(ge:navbar): change to correct version
    replace cart icon with heart-holding hand icon, switch to white theme

--- packages/grant-explorer/src/features/common/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar.tsx
@@ -1,9 +1,9 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import {ReactComponent as RoundManagerLogo} from "../../assets/topbar-logos.svg";
-import {ConnectButton} from "@rainbow-me/rainbowkit";
-import {ReactComponent as Search} from '../../assets/search.svg';
-import {ReactComponent as Cart} from '../../assets/cart.svg';
+import { ReactComponent as RoundManagerLogo } from "../../assets/topbar-logos-black.svg";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ReactComponent as Search } from "../../assets/search-black.svg";
+import { ReactComponent as HeartHand } from "../../assets/hearthand.svg";
 
 export interface NavbarProps {
   programCta?: boolean;
@@ -11,11 +11,11 @@ export interface NavbarProps {
 
 export default function Navbar() {
   return (
-    <nav className="bg-grey-500">
+    <nav className="bg-white">
       <div className="mx-auto px-4 sm:px-6 lg:px-20">
         <div className="flex justify-between h-16">
           <div className="flex">
-            <Link to="/" className="flex-shrink-0 flex items-center">
+            <Link to="#" className="flex-shrink-0 flex items-center">
               <div className="hidden lg:block md:block">
                 <RoundManagerLogo className="lg:inline-block md:inline-block" />
               </div>
@@ -23,7 +23,7 @@ export default function Navbar() {
           </div>
           <div className="flex items-center gap-4">
             <Search />
-            <Cart />
+            <HeartHand />
             <ConnectButton />
           </div>
         </div>
