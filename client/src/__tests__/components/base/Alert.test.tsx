961c7580b2bf32f20b2f5df6abe977be4ccfeb6d
Andrea Franz <andrea@gravityblast.com>
2022-09-29 14:13:52 +0200

    fix lint errors

--- client/src/__tests__/components/base/Alert.test.tsx
+++ client/src/__tests__/components/base/Alert.test.tsx
@@ -1,18 +1,15 @@
 import "@testing-library/jest-dom";
 import { screen } from "@testing-library/react";
 import { AlertContainer } from "../../../components/base/Alert";
-import { buildAlert } from "../../../utils/test_utils";
+import { buildAlert, renderWrapped } from "../../../utils/test_utils";
 import setupStore from "../../../store";
-import {
-  renderWrapped,
-} from "../../../utils/test_utils";
 
 describe("<AlertContainer />", () => {
   test("it should show multiple alerts", async () => {
     const store = setupStore();
 
-    const alert1 = buildAlert({id: 1, message: "First Alert"});
-    const alert2 = buildAlert({id: 2, message: "Second Alert"});
+    const alert1 = buildAlert({ id: 1, message: "First Alert" });
+    const alert2 = buildAlert({ id: 2, message: "Second Alert" });
 
     renderWrapped(<AlertContainer alerts={[alert1, alert2]} />, store);
 
--- client/src/__tests__/reducers/ui.test.ts
+++ client/src/__tests__/reducers/ui.test.ts
@@ -6,18 +6,24 @@ describe("uiReducer", () => {
     test("it should add an alert", async () => {
       const state = { alerts: [] };
       const alert = buildAlert();
-      const newState = uiReducer(state, { type: "UI_ALERT_ADDED", payload: alert});
-      expect(newState).toEqual({alerts: [alert]});
+      const newState = uiReducer(state, {
+        type: "UI_ALERT_ADDED",
+        payload: alert,
+      });
+      expect(newState).toEqual({ alerts: [alert] });
     });
 
     test("it should remove an alert", async () => {
-      const alert1 = buildAlert({id: 1});
-      const alert2 = buildAlert({id: 2});
+      const alert1 = buildAlert({ id: 1 });
+      const alert2 = buildAlert({ id: 2 });
 
       const state = { alerts: [alert1, alert2] };
-      const newState = uiReducer(state, { type: "UI_ALERT_REMOVED", payload: 1});
+      const newState = uiReducer(state, {
+        type: "UI_ALERT_REMOVED",
+        payload: 1,
+      });
 
-      expect(newState).toEqual({alerts: [alert2]});
+      expect(newState).toEqual({ alerts: [alert2] });
     });
   });
 });
