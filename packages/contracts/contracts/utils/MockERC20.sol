7bd00f981d8d2ed774681e0e701ebdf232cf3ef8
Tim <tim.proctor75@gmail.com>
2022-09-30 17:49:52 +0300

    added erc20 mocktoken (still wip)

---
+++ packages/contracts/contracts/utils/MockERC20.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+  constructor(uint256 initialSupply) ERC20("Mock", "MCK") {
+    _mint(msg.sender, initialSupply);
+  }
+
+  function mint(uint256 amount) external {
+    _mint(msg.sender, amount);
+  }
+}
--- packages/contracts/test/QuadraticFundingVotingStrategy.test.ts
+++ packages/contracts/test/QuadraticFundingVotingStrategy.test.ts
@@ -4,7 +4,7 @@ import { deployContract } from "ethereum-waffle";
 import { BytesLike, isAddress } from "ethers/lib/utils";
 import { artifacts, ethers } from "hardhat";
 import { Artifact } from "hardhat/types";
-import { QuadraticFundingVotingStrategy } from "../typechain";
+import { MockERC20, QuadraticFundingVotingStrategy } from "../typechain";
 import { Wallet } from "ethers";
 
 describe("QuadraticFundingVotingStrategy", function () {
@@ -13,7 +13,8 @@ describe("QuadraticFundingVotingStrategy", function () {
   let quadraticFundingVotingStrategy: QuadraticFundingVotingStrategy;
   let quadraticFundingVotingStrategyArtifact: Artifact;
 
-
+  let mockERC20: MockERC20;
+  let mockERC20Artifact: Artifact;
   describe('constructor', () => {
 
     it('deploys properly', async () => {
@@ -23,26 +24,36 @@ describe("QuadraticFundingVotingStrategy", function () {
       quadraticFundingVotingStrategyArtifact = await artifacts.readArtifact('QuadraticFundingVotingStrategy');
       quadraticFundingVotingStrategy = <QuadraticFundingVotingStrategy>await deployContract(user, quadraticFundingVotingStrategyArtifact, []);
 
+      mockERC20Artifact = await artifacts.readArtifact('MockERC20');
+      mockERC20 = <MockERC20>await deployContract(user, mockERC20Artifact, ['18']);
+      
       // Verify deploy
       expect(isAddress(quadraticFundingVotingStrategy.address), 'Failed to deploy QuadraticFundingVotingStrategy').to.be.true;
+      expect(isAddress(mockERC20.address), 'Failed to deploy MockERC20').to.be.true;
+
     });
   })
 
 
-  describe('core functions', () => {
+  describe.only('core functions', () => {
 
     before(async () => {
       [user] = await ethers.getSigners();
-
       // Deploy QuadraticFundingVotingStrategy contract
       quadraticFundingVotingStrategyArtifact = await artifacts.readArtifact('QuadraticFundingVotingStrategy');
       quadraticFundingVotingStrategy = <QuadraticFundingVotingStrategy>await deployContract(user, quadraticFundingVotingStrategyArtifact, []);
+      mockERC20Artifact = await artifacts.readArtifact('MockERC20');
+      mockERC20 = <MockERC20>await deployContract(user, mockERC20Artifact, ['18']);
     });
 
-    describe.only("test: vote", () => {
+    it("votes properly", async () => {
+      // console.log('mockERC20', mockERC20);
       const votes = [
-        [Wallet.createRandom().address, 1, Wallet.createRandom().address],
-        [Wallet.createRandom().address, 2, Wallet.createRandom().address],
+        // [Wallet.createRandom().address, 1, Wallet.createRandom().address],
+        // [Wallet.createRandom().address, 2, Wallet.createRandom().address],
+        [mockERC20.address, 100, Wallet.createRandom().address],
+        [mockERC20.address, 100, Wallet.createRandom().address],
+    
       ];
 
       const encodedVotes: BytesLike[] = [];
@@ -55,31 +66,26 @@ describe("QuadraticFundingVotingStrategy", function () {
           )
         );
       }
+      console.log('ok')
+      expect(quadraticFundingVotingStrategy.vote(encodedVotes, user.address)).to.not.be.reverted;
+      const txn = await quadraticFundingVotingStrategy.vote(encodedVotes, user.address);
+      const receipt = await txn.wait();
 
-      it("invoking Vote with encoded votes SHOULD NOT revert", async () => {
-        expect(quadraticFundingVotingStrategy.vote(encodedVotes, user.address)).to.not.be
-          .reverted;
-      });
-
-      it("invoking vote with encoded votes should emit the Voted event", async () => {
-        const txn = await quadraticFundingVotingStrategy.vote(encodedVotes, user.address);
-        const receipt = await txn.wait();
-
-        if (receipt.events) {
-          for (let i = 0; i < receipt.events.length; i++) {
-            const event = receipt.events[i];
-            if (event.event === 'Voted') {
-              expect(event.args).to.deep.equal({
-                token: votes[i][0],
-                amount: votes[i][1],
-                voter: user.address,
-                grantAddress: votes[i][2],
-                roundAddress: quadraticFundingVotingStrategy.address,
-              });
-            }
+      if (receipt.events) {
+        for (let i = 0; i < receipt.events.length; i++) {
+          console.log('ok')
+          const event = receipt.events[i];
+          if (event.event === 'Voted') {
+            expect(event.args).to.deep.equal({
+              token: mockERC20.address,
+              amount: 100,
+              voter: user.address,
+              grantAddress: votes[i][2],
+              roundAddress: quadraticFundingVotingStrategy.address,
+            });
           }
         }
-      });
+      }
     });
 
   })
