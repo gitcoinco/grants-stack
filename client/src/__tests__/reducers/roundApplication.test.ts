4aff5dcb12e9cd15878d637f86671da3dc774d2b
Jaxcoder <codenamejason@outlook.com>
2022-10-03 18:08:22 -0400

    linting

--- client/src/__tests__/reducers/roundApplication.test.ts
+++ client/src/__tests__/reducers/roundApplication.test.ts
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import {
   roundApplicationReducer,
   RoundApplicationState,
-  Status
+  Status,
 } from "../../reducers/roundApplication";
 
 describe("roundApplication reducer", () => {
@@ -14,11 +14,7 @@ describe("roundApplication reducer", () => {
       roundAddress: "0x1234",
       projectId: 1,
     });
-    expect(newState["0x1234"].status).toBe(
-      Status.Sent
-    );
-    expect(newState["0x1234"].projectsIDs[0]).toBe(
-      1
-    );
+    expect(newState["0x1234"].status).toBe(Status.Sent);
+    expect(newState["0x1234"].projectsIDs[0]).toBe(1);
   });
 });
