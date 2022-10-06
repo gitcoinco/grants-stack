8b7329542abc7f12493e10d411f4ef49fc88e30f
Jaxcoder <codenamejason@outlook.com>
2022-10-06 16:51:19 -0400

    Layout the card for app status per figma

--- client/src/components/grants/ApplicationCard.tsx
+++ client/src/components/grants/ApplicationCard.tsx
@@ -1,17 +1,34 @@
-import { RoundApplicationState } from "../../reducers/roundApplication";
+import { Badge, Box, Button, SimpleGrid } from "@chakra-ui/react";
 
-interface ApplicationCardProps {
-  application?: RoundApplicationState | undefined;
-  roundId?: string | undefined;
-}
+// interface ApplicationCardProps {}
 
-export default function ApplicationCard({
-  application,
-  roundId,
-}: ApplicationCardProps) {
+export default function ApplicationCard() {
   return (
-    <div className="rounded border-2 border-gray-400 p-2">
-      Status: {application && application[roundId!]?.status}
-    </div>
+    <Box className="border-gray-300 p-2" borderWidth="1px" borderRadius="md">
+      <Box p={2}>
+        <span className="text-[20px]">My Applications</span>
+      </Box>
+      <Box p={2}>
+        <span className="text-[16px]">Fantom</span>
+      </Box>
+      <SimpleGrid columns={2} spacing={2}>
+        <Box className="pl-2">
+          <span>Round 1</span>
+        </Box>
+        <Box className="pl-2 text-right">
+          <Badge borderRadius="full" px={2}>
+            Pending
+          </Badge>
+        </Box>
+      </SimpleGrid>
+      <Box p={2}>
+        <span>Jan 1st, 2022 - Sep 2nd, 2022</span>
+      </Box>
+      <Box p={1} pr={1} className="text-center pt-4 pb-6">
+        <Button colorScheme="purple" width="100%">
+          View Application
+        </Button>
+      </Box>
+    </Box>
   );
 }
