6489429b0b39d53c792b421cd12eedb6b79d521c
ghostffcode <meremezeukah@gmail.com>
2022-10-08 08:38:50 -0500

    add more design colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,26 +1,28 @@
 module.exports = {
   // if this is not set it will default to user's operating system preferences
-  darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         // Very open to better naming here :)
-        "primary-background": "#6F3FF5",
-        "secondary-background": "#0E0333",
-        "danger-background": "#D03E63",
-        "primary-text": "#0E0333",
-        "secondary-text": "#757087",
-        "tertiary-text": "#E2E0E7",
-        "quaternary-text": "#FFFFFF",
-        "danger-text": "#D03E63",
-        "green-text": "#11BC92",
-        "gitcoin-gold": "#E1B815",
-        "gitcoin-yellow": "#FFF8DB",
-        "gitcoin-pink-500": "#D44D6E",
-        "gitcoin-pink-100": "#FDDEE4",
-        "gitcoin-teal-100": "#E6FFF9",
-        "gitcoin-teal-500": "#11BC92",
+        'primary-background': '#6F3FF5',
+        'secondary-background': '#0E0333',
+        'danger-background': '#D03E63',
+        'primary-text': '#0E0333',
+        'secondary-text': '#757087',
+        'tertiary-text': '#E2E0E7',
+        'quaternary-text': '#FFFFFF',
+        'danger-text': '#D03E63',
+        'green-text': '#11BC92',
+        'gitcoin-gold': '#E1B815',
+        'gitcoin-yellow': '#FFF8DB',
+        'gitcoin-pink-500': '#D44D6E',
+        'gitcoin-pink-100': '#FDDEE4',
+        'gitcoin-teal-100': '#E6FFF9',
+        'gitcoin-teal-500': '#11BC92',
+        'gitcoin-violet-100': '#F0EBFF',
+        'gitcoin-violet-500': '#5932C4',
       },
     },
     fontFamily: {
@@ -28,4 +30,4 @@ module.exports = {
     },
   },
   important: true,
-};
+}
