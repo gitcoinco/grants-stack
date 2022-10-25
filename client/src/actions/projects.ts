f53af7e016f764cc971c5a93dd1e151b773dc522
Jaxcoder <codenamejason@outlook.com>
2022-10-25 10:11:31 -0400

    working query

--- client/src/actions/projects.ts
+++ client/src/actions/projects.ts
@@ -156,7 +156,7 @@ export const getRoundProjectsApplied =
         }
         `,
         chainId,
-        { id: projectID }
+        { projectID }
       );
       console.log("applicationsFound", applicationsFound);
 
