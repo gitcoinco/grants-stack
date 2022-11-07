05fa9cb56ecb69aaf6c98fba551cba10e649229f
hans <hmrtn@pm.me>
2022-11-07 13:00:28 -0800

    refactor: shortlist icon component to with count param
    fixes#612

--- packages/grant-explorer/src/features/common/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as GitcoinLogo } from "../../assets/gitcoinlogo-black.sv
 import { ReactComponent as GrantsExplorerLogo } from "../../assets/topbar-logos-black.svg";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { ReactComponent as Search } from "../../assets/search-black.svg";
-import { ReactComponent as HeartHand } from "../../assets/hearthand.svg";
+import { ReactComponent as ShortlistIcon } from "../../assets/shortlist-inactive.svg";
 
 export interface NavbarProps {
   roundUrlPath: string;
@@ -13,26 +13,37 @@ export interface NavbarProps {
 export default function Navbar(props: NavbarProps) {
   return (
     <nav className="bg-white">
-      <div className="mx-auto px-4 sm:px-6 lg:px-20">
-          <div className="flex justify-between h-16">
-            <div className="flex">
-              <Link to="#" className="flex-shrink-0 flex items-center">
-                  <GitcoinLogo className="block h-8 w-auto" />
-                <div className="hidden lg:block md:block">
-                    <span className="mx-6 text-grey-400">|</span>
-                    <GrantsExplorerLogo className="lg:inline-block md:inline-block" />
+        <div className="mx-auto px-4 sm:px-6 lg:px-20">
+            <div className="flex justify-between h-16">
+                <div className="flex">
+                    <Link to="#" className="flex-shrink-0 flex items-center">
+                        <GitcoinLogo className="block h-8 w-auto"/>
+                        <div className="hidden lg:block md:block">
+                            <span className="mx-6 text-grey-400">|</span>
+                            <GrantsExplorerLogo className="lg:inline-block md:inline-block"/>
+                        </div>
+                    </Link>
                 </div>
-              </Link>
+                <div className="flex items-center gap-6">
+                    <Search/>
+                    <Shortlist count={0} roundUrlPath={props.roundUrlPath}/>
+                    <ConnectButton/>
+                </div>
             </div>
-            <div className="flex items-center gap-6">
-              <Search />
-              <Link to={`${props.roundUrlPath}/ballot`}>
-                  <HeartHand />
-                </Link>
-              <ConnectButton />
-            </div>
-          </div>
-      </div>
+        </div>
     </nav>
   );
+}
+
+function Shortlist(props: { count: number, roundUrlPath: string }) {
+    return (
+        <span className="relative inline-block">
+            <Link to={`${props.roundUrlPath}/ballot`}>
+                <ShortlistIcon />
+                <div className="inline-flex absolute top-2 left-1 justify-center items-center w-4 h-3 text-xs font-bold text-black">
+                    {props.count === 0 ? "" : props.count}
+                </div>
+            </Link>
+        </span>
+    );
 }
