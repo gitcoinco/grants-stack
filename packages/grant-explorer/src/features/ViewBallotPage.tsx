8bcfd05c4fb2570e645cf085ecbf1c71bf7d1497
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-11-08 13:17:57 +0530

    chore: setup empty state for shortlist + final ballot
    fixes #484

--- packages/grant-explorer/src/features/ViewBallotPage.tsx
+++ packages/grant-explorer/src/features/ViewBallotPage.tsx
@@ -1,8 +1,10 @@
 import { useBallot } from "../context/BallotContext";
 import { Project } from "./api/types";
 import { useRoundById } from "../context/RoundContext";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Navbar from "./common/Navbar";
+import { ChevronLeftIcon } from "@heroicons/react/solid";
+import { Button } from "./common/styles";
 
 export default function ViewBallot() {
   const { chainId, roundId } = useParams();
@@ -11,70 +13,166 @@ export default function ViewBallot() {
   useRoundById(chainId!, roundId!);
 
   const [shortlist, , ,finalBallot] = useBallot();
-  const shortlistNotEmpty = shortlist.length > 0;
-  const finalBallotNotEmpty = finalBallot.length > 0;
 
 
   return (
     <>
       <Navbar roundUrlPath={`/round/${chainId}/${roundId}`} />
-      {/* Shortlist */}
-      <div>
-        {shortlistNotEmpty && ProjectShortList(shortlist)}
-        {!shortlistNotEmpty && EmptyProjectShortList() }
-      </div>
 
-      {/* Final Ballot */}
-      <div>
-        {finalBallotNotEmpty && ProjectFinalBallot(finalBallot)}
-        {!finalBallotNotEmpty && EmptyProjecFinalBallot() }
+      <div className="mx-20 h-screen px-4 py-7">
+
+        { Header(chainId, roundId) }
+
+        <div className="grid grid-cols-2 gap-4">
+          { ProjectShortList(shortlist, chainId, roundId) }
+
+          { ProjectFinalBallot(finalBallot) }
+        </div>
+        <div className="grid grid-cols-2  gap-4">
+          <div></div>
+          <div>
+            { Summary() }
+
+            <Button
+              $variant="solid"
+              type="button"
+              className=" items-center shadow-sm text-sm rounded w-full opacity-50"
+            >
+              Cast your ballot!
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-function ProjectShortList(shortlist: Project[]) {
-  return (
-    <div>
-      {shortlist.map((project: Project, key: number) => {
-        return (
-          <div key={key} data-testid="project">
-            {project.projectMetadata.title}
-          </div>
-        );
-      })}
+function Header(chainId?: string, roundId?: string) {
+ return(
+  <div>
+    <div className="flex flex-row items-center gap-3 text-sm">
+      <ChevronLeftIcon className="h-5 w-5 mt-6 mb-6" />
+      <Link to={`/round/${chainId}/${roundId}`}>
+        <span className="font-normal">Back</span>
+      </Link>
     </div>
+
+    <h1 className="text-3xl mt-6 font-thin border-b-2 pb-2">
+      Ballot Builder
+    </h1>
+
+    <p className="my-4">
+      Select your favorite projects from the Shortlist to build your Final Ballot.
+    </p>
+  </div>
+ );
+}
+
+function ProjectShortList(shortlist: Project[], chainId?: string, roundId?: string) {
+  const shortlistNotEmpty = shortlist.length > 0;
+  return (
+    <>
+      { shortlistNotEmpty ?
+        <div>
+          {shortlist.map((project: Project, key: number) => {
+            return (
+              <div key={key} data-testid="project">
+                {project.projectMetadata.title}
+              </div>
+            );
+          })}
+        </div>
+        :
+        EmptyProjectShortList(chainId, roundId)
+      }
+    </>
   );
 }
 
 
-function EmptyProjectShortList() {
+function EmptyProjectShortList(chainId?: string, roundId?: string) {
   return (
     <>
-      <p>You do not have anything on your shortlist</p>
+      <div className="block p-6 rounded-lg shadow-lg bg-white border">
+        <h2 className="text-xl border-b-2 pb-2">
+          Shortlist
+        </h2>
+
+        <div className="my-4">
+          <p className="text-grey-500 font-light">
+            Projects that you add to the shortlist will appear here.
+          </p>
+        </div>
+
+        <div className="flex justify-center">
+          <Link to={"/round/" + chainId + "/" + roundId}>
+            <Button
+              $variant="solid"
+              type="button"
+              className="inline-flex items-center shadow-sm text-sm rounded"
+            >
+              Browse Projects
+            </Button>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
 
-
 function ProjectFinalBallot(finalBallot: Project[]) {
+  const finalBallotNotEmpty = finalBallot.length > 0;
+
   return (
-    <div>
-      {finalBallot.map((project: Project, key: number) => {
-        return (
-          <div key={key} data-testid="project">
-            {project.projectMetadata.title}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      { finalBallotNotEmpty ?
+        <div>
+          {finalBallot.map((project: Project, key: number) => {
+            return (
+              <div key={key} data-testid="project">
+                {project.projectMetadata.title}
+              </div>
+            );
+          })}
+        </div>
+      :
+        EmptyProjecFinalBallot()
+      }
+    </>
   );
 }
 
 function EmptyProjecFinalBallot() {
   return (
     <>
-      <p>You do not have anything on your ballot</p>
+      <div className="block p-6 rounded-lg shadow-lg bg-white border border-violet-400">
+        <h2 className="text-xl border-b-2 pb-2">
+          Final Ballot
+        </h2>
+
+        <div className="mt-4">
+          <p className="text-grey-500">
+            Add the projects you want to fund here!
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function Summary() {
+  return (
+    <>
+      <div className="my-5 block p-6 rounded-lg shadow-lg bg-white border border-violet-400 font-semibold">
+        <h2 className="text-xl border-b-2 pb-2">
+          Summary
+        </h2>
+
+        <div className="flex justify-between mt-4">
+          <p>Your Contribution</p>
+          <p>000.00 DAI</p>
+        </div>
+      </div>
     </>
   )
 }
--- packages/grant-explorer/src/features/round/__tests__/ViewBallotPage.test.tsx
+++ packages/grant-explorer/src/features/round/__tests__/ViewBallotPage.test.tsx
@@ -5,6 +5,7 @@ import { Project } from "../../api/types";
 import { makeApprovedProjectData } from "../../../test-utils";
 import { RoundProvider } from "../../../context/RoundContext";
 import { faker } from "@faker-js/faker";
+import { MemoryRouter } from "react-router-dom";
 
 const chainId = faker.datatype.number();
 const roundId = faker.finance.ethereumAddress();
@@ -12,6 +13,8 @@ const useParamsFn = () => ({
   chainId,
   roundId,
 });
+
+
 jest.mock("../../common/Navbar");
 jest.mock("../../common/Auth");
 jest.mock("@rainbow-me/rainbowkit", () => ({
@@ -23,45 +26,133 @@ jest.mock("react-router-dom", () => ({
 }));
 
 describe("View Ballot Page", () => {
-  it("shows list of projects with project name", () => {
-    const shortlist: Project[] = [
-      makeApprovedProjectData(),
-      makeApprovedProjectData(),
-    ];
 
-    render(
-      <RoundProvider>
-        <BallotContext.Provider
-          value={{
-            shortlist: shortlist, setShortlist: () => {},
-            finalBallot: [], setFinalBallot: () => {}
-          }}
-        >
-          <ViewBallot />
-        </BallotContext.Provider>
-      </RoundProvider>
-    );
+  describe("Shortlist", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
-    const projects = screen.getAllByTestId("project");
-    expect(projects.length).toEqual(shortlist.length);
-    projects.forEach((project, i) => {
-      expect(project.textContent).toContain(shortlist[i].projectMetadata.title);
+    it("shows list of projects with project name", () => {
+      const shortlist: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: shortlist, setShortlist: () => {},
+                finalBallot: [], setFinalBallot: () => {}
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      const projects = screen.getAllByTestId("project");
+      expect(projects.length).toEqual(shortlist.length);
+      projects.forEach((project, i) => {
+        expect(project.textContent).toContain(shortlist[i].projectMetadata.title);
+      });
+    });
+
+    it("shows message that you have no projects", () => {
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: [], setShortlist: () => {},
+                finalBallot: [], setFinalBallot: () => {}
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+      screen.getByText(/Projects that you add to the Shortlist will appear here./i);
     });
   });
 
-  it("shows message that you have no projects", () => {
-    render(
-      <RoundProvider>
-        <BallotContext.Provider
-          value={{
-            shortlist: [], setShortlist: () => {},
-            finalBallot: [], setFinalBallot: () => {}
-          }}
-        >
-          <ViewBallot />
-        </BallotContext.Provider>
-      </RoundProvider>
-    );
-    screen.getByText(/you do not have anything on your ballot/i);
+  describe("Final Ballot", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("shows list of projects with project name", () => {
+      const finalBallot: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: [], setShortlist: () => {},
+                finalBallot: finalBallot, setFinalBallot: () => {}
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      const projects = screen.getAllByTestId("project");
+      expect(projects.length).toEqual(finalBallot.length);
+      projects.forEach((project, i) => {
+        expect(project.textContent).toContain(finalBallot[i].projectMetadata.title);
+      });
+    });
+
+    it("shows message that you have no projects", () => {
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: [], setShortlist: () => {},
+                finalBallot: [], setFinalBallot: () => {}
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+      screen.getByText(/Add the projects you want to fund here!/i);
+    });
+  });
+
+  describe("Summary", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("shows default amount that you have no projects in final ballot", () => {
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: [], setShortlist: () => {},
+                finalBallot: [], setFinalBallot: () => {}
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+      screen.getByText(/Your contribution/i);
+      screen.getByText(/000.00 DAI/i);
+    });
   });
 });
