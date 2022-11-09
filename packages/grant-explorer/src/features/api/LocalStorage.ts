5db9ad196afe136c7e41a098ce7f06c8a9289572
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-11-09 12:39:39 +0530

    feat: update BallotContext with finalBallot implementation
    - implement addToFinalBallot to remove from shortlist
      before adding to finalBallot
    - implement removeFromFinalBallot to add to shortlist
      after removing from shortlist
    
    refs #586
    refs #487

--- packages/grant-explorer/src/features/api/LocalStorage.ts
+++ packages/grant-explorer/src/features/api/LocalStorage.ts
@@ -16,3 +16,20 @@ export function loadShortlist(roundId: string): Project[] {
   }
   return JSON.parse(serializedShortlist);
 }
+
+export function saveFinalBallot(finalBallot: Project[], roundId: string): void {
+  window.localStorage.setItem(
+    `finalBallot-round-${roundId}`,
+    JSON.stringify(finalBallot)
+  );
+}
+
+export function loadFinalBallot(roundId: string): Project[] {
+  const serializedFinalBallot = window.localStorage.getItem(
+    `finalBallot-round-${roundId}`
+  );
+  if (!serializedFinalBallot) {
+    return [];
+  }
+  return JSON.parse(serializedFinalBallot);
+}
--- packages/grant-explorer/src/features/api/__tests__/localstorage.test.ts
+++ packages/grant-explorer/src/features/api/__tests__/localstorage.test.ts
@@ -1,58 +1,116 @@
 import { Project } from "../types";
 import { makeApprovedProjectData } from "../../../test-utils";
-import { loadShortlist, saveShortlist } from "../LocalStorage";
+import { loadFinalBallot, loadShortlist, saveFinalBallot, saveShortlist } from "../LocalStorage";
 
 describe("Local Storage", () => {
-  beforeEach(() => {
-    localStorage.clear();
-    jest.clearAllMocks();
+
+  describe("shortlist local storage", () => {
+    beforeEach(() => {
+      localStorage.clear();
+      jest.clearAllMocks();
+    });
+
+    it("stores shortlist", () => {
+      const roundId1 = "1";
+      const shortlist: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+
+      saveShortlist(shortlist, roundId1);
+
+      expect(localStorage.setItem).toHaveBeenLastCalledWith(
+        `shortlist-round-${roundId1}`,
+        JSON.stringify(shortlist)
+      );
+      expect(localStorage.__STORE__[`shortlist-round-${roundId1}`]).toBe(
+        JSON.stringify(shortlist)
+      );
+      expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    });
+
+    it("retrieves a shortlist for different rounds", function () {
+      const roundId1 = "1";
+      const roundId2 = "2";
+      const shortlist1: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+      const shortlist2: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+      localStorage.__STORE__[`shortlist-round-${roundId1}`] =
+        JSON.stringify(shortlist1);
+      localStorage.__STORE__[`shortlist-round-${roundId2}`] =
+        JSON.stringify(shortlist2);
+
+      const list1 = loadShortlist(roundId1);
+      const list2 = loadShortlist(roundId2);
+
+      expect(list1).toEqual(shortlist1);
+      expect(list2).toEqual(shortlist2);
+    });
+
+    it("retrieves empty project list when no shortlist exists", function () {
+      const list = loadShortlist("1");
+
+      expect(list).toEqual([]);
+    });
   });
 
-  it("stores shortlist", () => {
-    const roundId1 = "1";
-    const shortlist: Project[] = [
-      makeApprovedProjectData(),
-      makeApprovedProjectData(),
-    ];
+  describe("finalBallot local storage", () => {
+    beforeEach(() => {
+      localStorage.clear();
+      jest.clearAllMocks();
+    });
 
-    saveShortlist(shortlist, roundId1);
+    it("stores finalBallot", () => {
+      const roundId1 = "1";
+      const finalBallot: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
 
-    expect(localStorage.setItem).toHaveBeenLastCalledWith(
-      `shortlist-round-${roundId1}`,
-      JSON.stringify(shortlist)
-    );
-    expect(localStorage.__STORE__[`shortlist-round-${roundId1}`]).toBe(
-      JSON.stringify(shortlist)
-    );
-    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
-  });
+      saveFinalBallot(finalBallot, roundId1);
 
-  it("retrieves a shortlist for different rounds", function () {
-    const roundId1 = "1";
-    const roundId2 = "2";
-    const shortlist1: Project[] = [
-      makeApprovedProjectData(),
-      makeApprovedProjectData(),
-    ];
-    const shortlist2: Project[] = [
-      makeApprovedProjectData(),
-      makeApprovedProjectData(),
-    ];
-    localStorage.__STORE__[`shortlist-round-${roundId1}`] =
-      JSON.stringify(shortlist1);
-    localStorage.__STORE__[`shortlist-round-${roundId2}`] =
-      JSON.stringify(shortlist2);
+      expect(localStorage.setItem).toHaveBeenLastCalledWith(
+        `finalBallot-round-${roundId1}`,
+        JSON.stringify(finalBallot)
+      );
+      expect(localStorage.__STORE__[`finalBallot-round-${roundId1}`]).toBe(
+        JSON.stringify(finalBallot)
+      );
+      expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    });
 
-    const list1 = loadShortlist(roundId1);
-    const list2 = loadShortlist(roundId2);
+    it("retrieves a finalBallot for different rounds", function () {
+      const roundId1 = "1";
+      const roundId2 = "2";
+      const finalBallot1: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+      const finalBallot2: Project[] = [
+        makeApprovedProjectData(),
+        makeApprovedProjectData(),
+      ];
+      localStorage.__STORE__[`finalBallot-round-${roundId1}`] =
+        JSON.stringify(finalBallot1);
+      localStorage.__STORE__[`finalBallot-round-${roundId2}`] =
+        JSON.stringify(finalBallot2);
 
-    expect(list1).toEqual(shortlist1);
-    expect(list2).toEqual(shortlist2);
-  });
+      const list1 = loadFinalBallot(roundId1);
+      const list2 = loadFinalBallot(roundId2);
 
-  it("retrieves empty project list when no shortlist exists", function () {
-    const list = loadShortlist("1");
+      expect(list1).toEqual(finalBallot1);
+      expect(list2).toEqual(finalBallot2);
+    });
 
-    expect(list).toEqual([]);
+    it("retrieves empty project list when no finalBallot exists", function () {
+      const list = loadFinalBallot("1");
+
+      expect(list).toEqual([]);
+    });
   });
 });
