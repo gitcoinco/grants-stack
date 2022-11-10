d348a77d45cc05c6f8832121853bf1257911e76a
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-11-10 11:17:27 +0530

    feat: select project from shortlist
    fixes #486
    
    Co-authored-by: Josef Vacek <vacekj@outlook.com>

--- packages/grant-explorer/src/features/round/__tests__/ViewBallotPage.test.tsx
+++ packages/grant-explorer/src/features/round/__tests__/ViewBallotPage.test.tsx
@@ -141,14 +141,272 @@ describe("View Ballot Page", () => {
       expect(setShortlist).toHaveBeenCalled();
     });
 
-    // it("shows a remove-from-ballot button replacing add-to-ballot when add-to-ballot is clicked", () => {
-    //   renderWithContext(<ViewProjectDetails />, { rounds: [roundWithProjects] });
-    //   const addToBallot = screen.getByTestId("add-to-ballot");
-    //   fireEvent.click(addToBallot);
+    it("shows default select button in grey state", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
 
-    //   expect(screen.getByTestId("remove-from-ballot")).toBeInTheDocument();
-    //   expect(screen.queryByTestId("add-to-ballot")).not.toBeInTheDocument();
-    // });
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      const SelectButton = screen.getByText("Select");
+      expect(SelectButton.classList).toContain("bg-grey-150");
+    });
+
+    it("select button turns violet when clicked", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+      const SelectButtonAfterClick = screen.getByTestId("select");
+      expect(SelectButtonAfterClick.classList).toContain("bg-violet-400");
+    });
+
+    it("active selected button turns grey when clicked", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist: shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+      fireEvent.click(SelectButton);
+      expect(SelectButton.classList).toContain("bg-grey-150");
+    });
+
+    it("selecting project from shortlist adds to selected state", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      /* Enable selection mode */
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+
+      /* Click the first project */
+      const FirstProject = screen.getAllByTestId("project")[0];
+      fireEvent.click(FirstProject);
+
+      expect(
+        screen.getAllByTestId("project")[0].firstElementChild!.classList
+      ).toContain("bg-violet-100");
+    });
+
+    it("unselecting project from shortlist removes from selected count", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      /* Enable selection mode */
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+
+      /* Click the first project */
+      const FirstProject = screen.getAllByTestId("project")[0];
+      fireEvent.click(FirstProject);
+
+      expect(screen.getByTestId("select")).toHaveTextContent(
+        "Add selected (1) to Final Donation"
+      );
+
+      /* Click the first project again to deselect it */
+      fireEvent.click(screen.getAllByTestId("project")[0]);
+
+      expect(screen.getByText("Select")).toBeInTheDocument();
+    });
+
+    it("selecting project turns background to violet", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      /* Enable selection mode */
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+
+      /* Click the first project */
+      const FirstProject = screen.getAllByTestId("project")[0];
+      fireEvent.click(FirstProject);
+
+      expect(
+        screen.getAllByTestId("project")[0].firstElementChild!.classList
+      ).toContain("bg-violet-100");
+    });
+    it("unselecting project turns background to white", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      /* Enable selection mode */
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+
+      /* Click the first project */
+      const FirstProject = screen.getAllByTestId("project")[0];
+      fireEvent.click(FirstProject);
+
+      expect(
+        screen.getAllByTestId("project")[0].firstElementChild!.classList
+      ).toContain("bg-violet-100");
+
+      fireEvent.click(screen.getAllByTestId("project")[0]);
+      expect(
+        screen.getAllByTestId("project")[0].firstElementChild!.classList
+      ).not.toContain("bg-violet-100");
+    });
+    it("unselecting all projects from selected state turns select button grey", () => {
+      const shortlist: Project[] = [makeApprovedProjectData()];
+
+      const setShortlist = jest.fn();
+
+      render(
+        <MemoryRouter>
+          <RoundProvider>
+            <BallotContext.Provider
+              value={{
+                shortlist,
+                setShortlist: setShortlist,
+                finalBallot: [],
+                setFinalBallot: () => {},
+              }}
+            >
+              <ViewBallot />
+            </BallotContext.Provider>
+          </RoundProvider>
+        </MemoryRouter>
+      );
+
+      /* Enable selection mode */
+      const SelectButton = screen.getByText("Select");
+      fireEvent.click(SelectButton);
+
+      /* Click the first project */
+      const FirstProject = screen.getAllByTestId("project")[0];
+      fireEvent.click(FirstProject);
+
+      /* Click the first project again */
+      fireEvent.click(screen.getAllByTestId("project")[0]);
+
+      /* Select button should turn gray */
+      expect(screen.getByText("Select").classList).toContain("bg-grey-150");
+    });
   });
 
   describe("Final Ballot", () => {
