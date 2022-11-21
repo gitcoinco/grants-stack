7dfa074eab3d3898d12529a9ac993d786331d606
Jaxcoder <codenamejason@outlook.com>
2022-11-21 13:43:51 -0500

    add loading spinner on round apply page

---
+++ client/src/components/base/LoadingSpinner.tsx
@@ -0,0 +1,34 @@
+import { Spinner, Stack } from "@chakra-ui/react";
+
+// Other properties can be added as needed for reuse
+function LoadingSpinner({
+  size,
+  thickness,
+  showText,
+}: {
+  size: string;
+  thickness: string;
+  showText: boolean;
+}) {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Stack spacing={4} direction="column">
+        <Spinner
+          className="flex items-center justify-center"
+          thickness={thickness}
+          boxSize={size}
+          speed="0.80s"
+          emptyColor="gray.200"
+          color="purple.500"
+        />
+        {showText ? (
+          <span className="flex items-center justify-center text-gitcoin-grey-400 text-[18px]">
+            Loading...
+          </span>
+        ) : null}
+      </Stack>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
