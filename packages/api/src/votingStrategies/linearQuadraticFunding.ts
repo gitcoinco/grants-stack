dcf966933c11cc257be76844d798638069c6ad4a
hans <hansmrtn@pm.me>
2022-11-28 08:58:25 -0800

    feat: build skeleton and implement /calculate endpoint (#713)
    - create base skeleton for voting calculators
    - added readme to define how to use package
    - add utils 
    
    closes #381
    closes #382

---
+++ packages/api/src/votingStrategies/linearQuadraticFunding.ts
@@ -0,0 +1,140 @@
+import {
+  QFContribution,
+  QFContributionsByProjectId,
+  ProjectMatch,
+  RoundMetadata,
+  ChainId,
+} from "../../types";
+import { fetchFromGraphQL } from "../utils";
+
+/**
+ * Fetch data from a GraphQL endpoint
+ *
+ * @param chainId - The chain ID of the blockchain indexed by the subgraph
+ * @param votingStrategyId - The voting strategy address
+ * @returns The result of the query
+ */
+export const fetchVotesHandler = async (
+  chainId: ChainId,
+  votingStrategyId: string
+): Promise<QFContribution[]> => {
+  const variables = { votingStrategyId };
+
+  const query = `
+    query GetVotes($votingStrategyId: String) {
+      votingStrategies(where:{
+        id: $votingStrategyId
+      }) {
+        votes {
+          amount
+          token
+          from
+          to
+        }
+      }
+    }
+  `;
+
+  // fetch from graphql
+  const response = await fetchFromGraphQL(chainId, query, variables);
+
+  const votes = response.data?.votingStrategies[0]?.votes;
+
+  let contributions: QFContribution[] = [];
+
+  votes.map((vote: any) => {
+    const contribution = {
+      projectId: vote.to, // TODO: we will have to update this to project id eventually
+      contributor: vote.from,
+      amount: Number(vote.amount),
+      token: vote.token,
+    };
+
+    contributions.push(contribution);
+  });
+
+  return contributions;
+};
+
+export const calculateHandler = async (
+  metadata: RoundMetadata,
+  contributions: QFContribution[]
+) => {
+  const totalProjectPoolAmount = metadata.totalPot;
+
+  // boolean determining the satisfaction of the quadratic funding amount constraint
+  let hasSaturated = false;
+
+  // the total amount of contributions per vote
+  let totalMatch = 0;
+
+  const contributionsByProjectId: QFContributionsByProjectId = {};
+  const projectMatchDistributions: ProjectMatch[] = [];
+
+  const contributionAddresses: Set<string> = new Set();
+
+  // group contributions by projectId
+  contributions.forEach((contribution: QFContribution) => {
+    if (!contributionsByProjectId[contribution.projectId]) {
+      contributionsByProjectId[contribution.projectId] = {
+        contributions: contribution
+          ? { [contribution.contributor]: contribution }
+          : {},
+      };
+      contributionAddresses.add(contribution.contributor);
+    }
+    // sum the contributions from the same address
+    if (
+      !contributionsByProjectId[contribution.projectId].contributions[
+        contribution.contributor
+      ]
+    ) {
+      contributionsByProjectId[contribution.projectId].contributions[
+        contribution.contributor
+      ] = { ...contribution };
+    } else {
+      contributionsByProjectId[contribution.projectId].contributions[
+        contribution.contributor
+      ].amount += contribution.amount;
+    }
+  });
+
+  // calculate the linear quadratic funding for each project
+  Object.values(contributionsByProjectId).forEach((project) => {
+    let sumOfSqrtContrib = 0;
+    let sumOfContrib = 0;
+    Object.values(project.contributions).forEach((contribution) => {
+      sumOfSqrtContrib += Math.sqrt(contribution.amount);
+      sumOfContrib += contribution.amount;
+    });
+
+    const match = Math.pow(sumOfSqrtContrib, 2) - sumOfContrib;
+    projectMatchDistributions.push({
+      projectId:
+        project.contributions[Object.keys(project.contributions)[0]].projectId,
+      match,
+    } as ProjectMatch);
+    totalMatch += match;
+  });
+
+  if (totalMatch > totalProjectPoolAmount) {
+    hasSaturated = true;
+  }
+
+  // normalize the match distributions
+  if (hasSaturated) {
+    // calculate the ratio of the total match to the total project pool amount
+    const ratio = totalProjectPoolAmount / totalMatch;
+    // calculate the match distribution based on the ratio
+    projectMatchDistributions.forEach((projectMatch) => {
+      projectMatch.match *= ratio;
+    });
+  }
+
+  // TODO: Save into DB
+
+  return {
+    distribution: projectMatchDistributions,
+    hasSaturated: hasSaturated,
+  };
+};
---
+++ packages/api/types.ts
@@ -0,0 +1,49 @@
+/***************/
+/* = General = */
+/***************/
+
+export enum ChainId {
+  GOERLI_CHAIN_ID = "5",
+  OPTIMISM_MAINNET_CHAIN_ID = "10",
+  FANTOM_MAINNET_CHAIN_ID = "250",
+  FANTOM_TESTNET_CHAIN_ID = "4002",
+  LOCAL_ROUND_LAB = "3", // TODO: remove this later
+}
+
+export type Results = {
+  distribution: ProjectMatch[];
+  hasSaturated?: boolean;
+};
+
+export type ProjectMatch = {
+  projectId: string;
+  match: number;
+};
+
+export type RoundMetadata = {
+  votingStrategy: {
+    id: string;
+    strategyName: string;
+  };
+  token: string;
+  totalPot: number;
+};
+
+/****************/
+/* = LinearQF = */
+/****************/
+
+export type QFContribution = {
+  projectId: string;
+  amount: number;
+  contributor: string;
+  timestamp?: number;
+};
+
+export type QFContributionsByProjectId = {
+  [projectId: string]: {
+    contributions: {
+      [contributor: string]: QFContribution;
+    };
+  };
+};
