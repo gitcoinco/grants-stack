683d12ea3266639fafecb063531c298e5d8e67fb
Andrea Franz <andrea@gravityblast.com>
2022-12-01 10:42:11 +0100

    fix colors

--- client/src/components/base/PageNotFound.tsx
+++ client/src/components/base/PageNotFound.tsx
@@ -5,7 +5,7 @@ function PageNotFound() {
     <div className="w-full flex flex-col justify-center items-center">
       <div className="w-full lg:w-3/5 sm:w-2/3">
         <div className="p-8 flex flex-col">
-          <p className="text-sm font-normal gh-text-pink uppercase text-center">
+          <p className="text-sm font-normal text-gitcoin-pink-400 uppercase text-center">
             404 Error
           </p>
           <p className="mt-2 w-full text-center text-4xl font-medium text-black tracking-wide">
@@ -25,7 +25,7 @@ function PageNotFound() {
           <div className="w-full flex justify-center">
             <Link
               to="/"
-              className="bg-violet-100 hover:bg-blue-700 text-blue-700 py-2 px-4 rounded"
+              className="bg-gitcoin-violet-100 hover:bg-gitcoin-violet-400  hover:text-white text-blue-700 py-2 px-4 rounded"
             >
               Go back home
             </Link>
