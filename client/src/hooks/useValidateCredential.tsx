3edc2a65ee2cbdd5f76d9211518a7e5ed8739f3d
Kurt <Kurt@Merbeth.io>
2022-12-08 10:55:16 +0100

    added useValidateCredential hook

---
+++ client/src/hooks/useValidateCredential.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { PassportVerifier } from "@gitcoinco/passport-sdk-verifier";
+import { VerifiableCredential } from "@gitcoinco/passport-sdk-types";
+import { CredentialProvider } from "../types";
+
+const IAM_SERVER = "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC";
+const verifier = new PassportVerifier();
+
+export default function useValidateCredential(
+  vc: VerifiableCredential | undefined,
+  providerId: CredentialProvider,
+  handle: string | undefined
+) {
+  const [isValid, setIsValid] = useState(false);
+  async function validateCredential() {
+    if (vc && providerId && handle) {
+      const credential = vc;
+      credential.credentialSubject.provider = `${providerId}#${handle}`;
+
+      const validCredential = await verifier.verifyCredential(credential);
+      const validIssuer = IAM_SERVER === credential.issuer;
+      // TODO: add owner check
+      // address of vc.credentialSubject.id should be a project owner
+      setIsValid(validCredential && validIssuer);
+    } else {
+      setIsValid(false);
+    }
+  }
+
+  useEffect(() => {
+    validateCredential();
+  }, [vc, providerId, handle]);
+
+  return isValid;
+}
