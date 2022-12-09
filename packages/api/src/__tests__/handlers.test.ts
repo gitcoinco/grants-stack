e00aecfdc6c939e5dbe113b0908802a133c4dba8
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-12-09 12:38:57 +0530

    indent fix

--- packages/api/src/__tests__/handlers.test.ts
+++ packages/api/src/__tests__/handlers.test.ts
@@ -1,21 +1,25 @@
-import { Request, Response } from 'express';
-import {fetchRoundStatusHandler} from "../handlers";
+import { Request, Response } from "express";
+import { fetchRoundStatusHandler } from "../handlers";
 
-describe('fetchRoundStatusHandler', () => {
-    it('fetches round status successfully', async () => {
-        const mockRequest = {
-            query: {
-                roundId: '123',
-                projectId: '456',
-                chainId: '3',
-            },
-        };
-        const mockResponse = {
-            send: jest.fn(),
-            json: (c: any) => {console.log(c)},
-        };
+describe("fetchRoundStatusHandler", () => {
+  it("fetches round status successfully", async () => {
+    const mockRequest = {
+      query: {
+        roundId: "123",
+        projectId: "456",
+        chainId: "3",
+      },
+    };
+    const mockResponse = {
+      send: jest.fn(),
+      json: (c: any) => {
+        console.log(c);
+      },
+    };
 
-        await fetchRoundStatusHandler(mockRequest as unknown as Request, mockResponse as unknown as Response);
-
-    });
+    await fetchRoundStatusHandler(
+      mockRequest as unknown as Request,
+      mockResponse as unknown as Response
+    );
+  });
 });
