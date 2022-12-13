dfc63abc04edd476bc5debf3641f7342dba0df9e
hmrtn <hmrtn@pm.me>
2022-12-12 22:06:50 -0800

    fix: rm duplicate fn

--- packages/api/src/handlers/calculateHandler.ts
+++ packages/api/src/handlers/calculateHandler.ts
@@ -165,31 +165,4 @@ export const fetchMatchingHandler = async (req: Request, res: Response) => {
   return handleResponse(res, 200, "fetched info sucessfully", results);
 };
 
-const convertPriceRequestSchema = yup.object().shape({
-  contract: yup.string().required(),
-  chain: yup.string().required(),
-});
 
-/**
- * Converts between crypto and fiat prices
- */
-export const convertPriceHandler = async (
-  req: Request<any, any, { contract: string; chain: ChainName }>,
-  res: Response
-) => {
-  try {
-    await convertPriceRequestSchema.validate(req.body);
-  } catch (err: any) {
-    return handleResponse(res, 400, err.errors);
-  }
-
-  try {
-    let result = await getPriceForToken(req.body.contract, req.body.chain);
-    handleResponse(res, 200, "fetched info sucessfully", result);
-  } catch (err) {
-    handleResponse(res, 500, err as string); // FIXME: this won't work because error cannot be serialized
-    // (its properties arent enumerable)
-    // so we either need a custom error message or bear the rist of leaking things when
-    // we override the serialization block
-  }
-};
