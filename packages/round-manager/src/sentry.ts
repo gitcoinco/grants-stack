217e5d47fc75eaf40c3572e40edeb8c1e1bcd3f4
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-12-16 15:14:36 +0530

    feat: setup sentry on rm + ge (#843)
    fixes #844
    
    Co-authored-by: Josef Vacek <vacekj@outlook.com>

---
+++ packages/round-manager/src/sentry.ts
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+export const initSentry = () => {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN || "",
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+};
