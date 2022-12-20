74bde7bcf1d914b694a0aaebe15309a81c26b8de
Nick Kammerdiener <nicolas@kammerdiener.us>
2022-12-19 21:03:35 -0500

    fixing CIDR

--- infra/staging/index.js
+++ infra/staging/index.js
@@ -42,7 +42,7 @@ const private_subnet = new aws.ec2.Subnet("private", {
 });
 
 const private_subnet_two = new aws.ec2.Subnet("private-two", {
-    cidrBlock: "10.0.3.0/24",
+    cidrBlock: "10.0.10.0/24",
     vpcId: vpc.id,
     availabilityZone: "us-west-2b",
     tags: {
