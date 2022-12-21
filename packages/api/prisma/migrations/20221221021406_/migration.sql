7a33f4c131626b6877ab942433856e15c0afd83d
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-12-21 12:36:00 +0530

    cleanup migrations

--- packages/api/prisma/migrations/20221221021406_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[projectId]` on the table `match` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "match_projectId_key" ON "match"("projectId");
--- packages/api/prisma/migrations/20221221032539_/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "match_projectId_key";
