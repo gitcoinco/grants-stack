cb7b7eb13cb1762878534f29df6fe08418c18f2c
Nick Kammerdiener <nicolas@kammerdiener.us>
2022-12-21 15:51:39 -0500

    getting ssl setup

--- infra/staging/index.js
+++ infra/staging/index.js
@@ -245,6 +245,21 @@ const listener = new aws.lb.Listener("grants", {
     loadBalancerArn: grant_lb.arn,
     port: 80,
     protocol: "HTTP",
+    defaultAction: {
+        type: "redirect",
+        redirect: {
+        protocol: "HTTPS",
+        port: "443",
+        statusCode: "HTTP_301",
+        },
+    },
+});
+
+const listener_https = new aws.lb.Listener("grants_https", {
+    loadBalancerArn: grant_lb.arn,
+    port: 443,
+    certificateArn: certificateValidation.certificateArn,
+    protocol: "HTTPS",
     defaultActions: [
         {
         type: "forward",
@@ -252,18 +267,6 @@ const listener = new aws.lb.Listener("grants", {
         },
     ],
 });
-
-// const listener_https = new aws.lb.Listener("grants_https", {
-//     loadBalancerArn: grant_lb.arn,
-//     port: 443,
-//     protocol: "HTTPS",
-//     defaultActions: [
-//         {
-//         type: "forward",
-//         targetGroupArn: grant_target.arn,
-//         },
-//     ],
-// });
 
 // Fargate Instance
 const FargateLogGroup = new aws.cloudwatch.LogGroup("fargateLogGroup", {});
