aa23275a8178628ad1ca84d696adb8f527574c8e
bhargavaparoksham <aparokshambhargav@gmail.com>
2022-12-26 18:29:44 +0530

    feat: add api hooks in round-manager

---
+++ packages/round-manager/src/features/api/api.ts
@@ -0,0 +1,146 @@
+import { useMemo, useState } from "react";
+
+export type QFDistribution = {
+  projectId: string;
+  matchAmountInUSD: number;
+  totalContributionsInUSD: number;
+  matchPoolPercentage: number;
+  matchAmountInToken: number;
+  projectPayoutAddress: string;
+  uniqueContributorsCount: number;
+};
+
+export type QFContributionSummary = {
+  contributionCount: number;
+  uniqueContributors: number;
+  totalContributionsInUSD?: number;
+  averageUSDContribution?: number;
+};
+
+export function useRoundSummary({
+  chainId,
+  roundId,
+}: {
+  chainId: string;
+  roundId: string;
+}) {
+  const [roundSummary, setRoundSummary] = useState();
+  const [error, setError] = useState<Response | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  useMemo(() => {
+    setLoading(true);
+    const url = `${process.env.REACT_APP_GRANTS_API_ENDPOINT}/summary/${chainId}/${roundId}`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          console.log("error", resp.json());
+          setError(resp);
+          setLoading(false);
+        }
+      })
+      .then((data) => {
+        setRoundSummary(data);
+        setLoading(false);
+      });
+  }, [chainId, roundId]);
+
+  return {
+    data: roundSummary,
+    error,
+    loading,
+  };
+}
+
+export function useProjectSummary({
+  chainId,
+  roundId,
+  projectId,
+}: {
+  chainId: string;
+  roundId: string;
+  projectId: string;
+}) {
+  const [projectSummary, setProjectSummary] = useState();
+  const [error, setError] = useState<Response | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  useMemo(() => {
+    setLoading(true);
+    const url = `${
+      process.env.REACT_APP_GRANTS_API_ENDPOINT
+    }/summary/${chainId}/${roundId}/${new URLSearchParams({ projectId })}`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          setError(resp);
+          setLoading(false);
+        }
+      })
+      .then((data) => {
+        setProjectSummary(data);
+        setLoading(false);
+      });
+  }, [chainId, roundId, projectId]);
+
+  return {
+    data: projectSummary,
+    error,
+    loading,
+  };
+}
+
+export function useRoundMatchData({
+  chainId,
+  roundId,
+}: {
+  chainId: string;
+  roundId: string;
+}) {
+  const [roundMatchData, setRoundMatchData] = useState<QFDistribution[]>();
+  const [error, setError] = useState<Response | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  useMemo(() => {
+    setLoading(true);
+    const url = `${process.env.REACT_APP_GRANTS_API_ENDPOINT}/data/match/round/${chainId}/${roundId}`;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          setError(resp);
+          setLoading(false);
+        }
+      })
+      .then((data) => {
+        setRoundMatchData(data);
+        setLoading(false);
+      });
+  }, [chainId, roundId]);
+
+  return {
+    data: roundMatchData,
+    error,
+    loading,
+  };
+}
