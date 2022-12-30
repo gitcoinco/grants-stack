3560b87bf883dc5ff446fb59f2f57ee0b9871001
Nick Kammerdiener <nicolas@kammerdiener.us>
2022-12-29 22:36:53 -0500

    adding new NAT

--- infra/staging/index.js
+++ infra/staging/index.js
@@ -86,6 +86,20 @@ const nat_ip = new aws.ec2.Eip("nat_ip_new", {
 
 const nat_gateway = new aws.ec2.NatGateway("grants_private_nat", {
     allocationId: nat_ip.id,
+    subnetId: private_subnet.id,
+    tags: {
+        App: "Grants",
+    },
+}, {
+    dependsOn: [gw.gw],
+});
+
+const nat_ip_public = new aws.ec2.Eip("nat_ip_public", {
+    vpc: true,
+});
+
+const nat_gateway_public = new aws.ec2.NatGateway("grants_public_nat", {
+    allocationId: nat_ip.id,
     subnetId: public_subnet.id,
     tags: {
         App: "Grants",
@@ -124,7 +138,7 @@ const private_route_table = new aws.ec2.RouteTable('private', {
     routes: [
       {
         cidrBlock: '0.0.0.0/0',
-        gatewayId: nat_gateway.id
+        gatewayId: nat_gateway_public.id
       }
     ],
     vpcId: vpc.id
