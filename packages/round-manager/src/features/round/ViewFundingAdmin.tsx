aee27d1cfaa72d74be10d8709b11a85c7815803f
hmrtn <hmrtn@pm.me>
2023-01-02 19:34:43 -0800

    feat: add finalize round component

--- packages/round-manager/src/features/round/ViewFundingAdmin.tsx
+++ packages/round-manager/src/features/round/ViewFundingAdmin.tsx
@@ -4,6 +4,7 @@ import { MatchingStatsData, Round } from "../api/types";
 import { useRoundMatchData } from "../api/api";
 import { Button } from "../common/styles";
 import { saveObjectAsJson } from "../api/utils";
+import { RadioGroup } from "@headlessui/react";
 
 export default function ViewFundingAdmin(props: {
   round: Round | undefined;
@@ -69,21 +70,27 @@ function InformationContent(props: {
   const matchingData: MatchingStatsData[] | undefined = data?.map((data) => {
     const project = props.round?.approvedProjects?.filter(
       (project) =>
-        project.projectRegistryId.toLowerCase() === data.projectId.toLowerCase()
+        project.grantApplicationId.toLowerCase() ===
+        data.projectId.toLowerCase()
     )[0];
     return {
-      projectName: project?.projectMetadata?.title,
+      projectName: project?.projectMetadata?.title
+        ? project.projectMetadata.title
+        : "Unknown",
       projectId: data.projectId,
       uniqueContributorsCount: data.uniqueContributorsCount,
       matchPoolPercentage: data.matchPoolPercentage,
     };
   });
   return (
-    <div>
-      {loading && <Spinner text="We're fetching the matching data." />}
-      {!error && !loading && <InformationTable matchingData={matchingData} />}
-      {error && <ErrorMessage />}
-    </div>
+    <>
+      <div>
+        {loading && <Spinner text="We're fetching the matching data." />}
+        {!error && !loading && <InformationTable matchingData={matchingData} />}
+        {error && <ErrorMessage />}
+      </div>
+      {!error && !loading && <FinalizeRound />}
+    </>
   );
 }
 
@@ -162,4 +169,144 @@ function InformationTable(props: {
       </div>
     </div>
   );
+}
+
+function FinalizeRound() {
+  return (
+    <div className="w-full pt-12">
+      <span>Finalize Round</span>
+      <hr className="mt-2 mb-4" />
+      <div className="flex columns-2 pl-8">
+        <div className="w-full pt-2">
+          <form className="mt-4 space-y-3 w-full" action="#" method="POST">
+            <div className="grid grid-cols-2 space-y-2">
+              <div className="w-full pt-2">
+                <RadioGroup data-testid="default-or-upload-radio">
+                  <RadioGroup.Label className="block text-sm">
+                    <p className="text-sm">
+                      <span>
+                        Use default finalized matching stats or upload your own?
+                      </span>
+                    </p>
+                  </RadioGroup.Label>
+                  <div className="flex flex-row gap-4 mt-3">
+                    <RadioGroup.Option value={true}>
+                      {({ checked, active }) => (
+                        <span className="flex items-center text-sm">
+                          <span
+                            className={classNames(
+                              checked
+                                ? "bg-indigo-600 border-transparent"
+                                : "bg-white border-gray-300",
+                              active
+                                ? "ring-2 ring-offset-2 ring-indigo-500"
+                                : "",
+                              "h-4 w-4 rounded-full border flex items-center justify-center"
+                            )}
+                            aria-hidden="true"
+                          >
+                            <span className="rounded-full bg-white w-1.5 h-1.5" />
+                          </span>
+                          <RadioGroup.Label
+                            as="span"
+                            className="ml-3 block text-sm text-gray-700"
+                            data-testid="use-default-true"
+                          >
+                            Use Default
+                          </RadioGroup.Label>
+                        </span>
+                      )}
+                    </RadioGroup.Option>
+                    <RadioGroup.Option value={false}>
+                      {({ checked, active }) => (
+                        <span className="flex items-center text-sm">
+                          <span
+                            className={classNames(
+                              checked
+                                ? "bg-indigo-600 border-transparent"
+                                : "bg-white border-gray-300",
+                              active
+                                ? "ring-2 ring-offset-2 ring-indigo-500"
+                                : "",
+                              "h-4 w-4 rounded-full border flex items-center justify-center"
+                            )}
+                            aria-hidden="true"
+                          >
+                            <span className="rounded-full bg-white w-1.5 h-1.5" />
+                          </span>
+                          <RadioGroup.Label
+                            as="span"
+                            className="ml-3 block text-sm text-gray-700"
+                            data-testid="upload-own-true"
+                          >
+                            Upload my own JSON
+                          </RadioGroup.Label>
+                        </span>
+                      )}
+                    </RadioGroup.Option>
+                  </div>
+                </RadioGroup>
+              </div>
+              <div className="w-full pt-2">
+                <p className="text-sm">
+                  <span>
+                    Upload JSON file with finalized matching percentage
+                  </span>
+                </p>
+                <div className="flex items-center justify-center w-full mt-4">
+                  <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-42 p-10 group text-center">
+                    <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        stroke="currentColor"
+                        className="mx-auto w-12 h-12 text-gray-400"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M24 32.5V19.75m0 0l6 6m-6-6l-6 6M13.5 39.5a9 9 0 01-2.82-17.55 10.5 10.5 0 0120.465-4.66 6 6 0 017.517 7.696A7.504 7.504 0 0136 39.5H13.5z"
+                        />
+                      </svg>
+                      <p className="pointer-none text-gray-500 ">
+                        <span>
+                          <a
+                            href=""
+                            id=""
+                            className="text-purple-600 hover:underline"
+                          >
+                            Upload a file
+                          </a>{" "}
+                          or drag and drop
+                        </span>
+                        <br />
+                        JSON up to xMB
+                      </p>
+                    </div>
+                    <input type="file" className="hidden" />
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="grid justify-items-end">
+              <div className="w-fit">
+                <Button
+                  type="submit"
+                  className="my-5 w-full flex justify-center tracking-wide focus:outline-none focus:shadow-outline shadow-lg cursor-pointer"
+                >
+                  Finalize and save to contract
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
 }
