3cb4e955d9d8fc7de88898466a95777262facc50
Kurt <Kurt@Merbeth.io>
2023-01-04 16:08:57 +0100

    taught jest to deal with image imports

--- client/jest.config.js
+++ client/jest.config.js
@@ -9,4 +9,11 @@ module.exports = {
   // @rainbow-me/rainbowkit is already an ESM module and
   // it trips Jest when it tries to transform it, this ignores it
   transformIgnorePatterns: ["node_modules\\/(?!@rainbow-me\\/.*)"],
+  moduleNameMapper: { 
+    "^.+.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "jest-transform-stub"
+  },
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "jest-transform-stub"
+  },
 };
