b64c719bb02f557171d4b79479ad16c09338f7e7
hmrtn <hmrtn@pm.me>
2023-01-06 17:29:51 -0800

    fix: add basic test, bug squashin

--- packages/grant-explorer/src/features/api/passport.ts
+++ packages/grant-explorer/src/features/api/passport.ts
@@ -7,10 +7,22 @@ export enum PassportState {
   MATCH_ELIGIBLE,
   MATCH_INELIGIBLE,
   LOADING,
+  ERROR,
 }
 
-export function usePassport() {
-  const [passport, setPassport] = useState();
+type PassportResponse = {
+  address?: string;
+  score?: string;
+  detail?: string;
+}
+
+export function usePassport(): {
+  passport: PassportResponse | undefined;
+  error: Response | undefined;
+  passportState: PassportState;
+  setPassportState: (state: PassportState) => void;
+} {
+  const [passport, setPassport] = useState<PassportResponse | undefined>();
   const [error, setError] = useState<Response | undefined>();
   const { address, isConnected } = useAccount();
 
@@ -18,39 +30,54 @@ export function usePassport() {
 
   const PASSPORT_THRESHOLD = 0.0; // TODO: determine passport threshold
 
-  const checkPassport = () => {
-    setPassportState(PassportState.LOADING);
-    const url = `${process.env.REACT_APP_PASSPORT_API_ENDPOINT}/registry/score/${process.env.REACT_APP_PASSPORT_API_COMMUNITY_ID}/${address}`;
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.REACT_APP_PASSPORT_API_KEY}`
-      },
-    })
-      .then((resp) => {
-        if (resp.ok) {
-          return resp.json();
-        } else {
-          setError(resp);
-        }
-      })
-      .then((data) => {
-        if (data) {
-          // TODO: figure out a good score threshold
-          if (Number(data.score) >= PASSPORT_THRESHOLD) {
-            setPassportState(PassportState.MATCH_ELIGIBLE);
-          } else {
-            setPassportState(PassportState.MATCH_INELIGIBLE);
-          }
-          setPassport(data);
-        } else {
-          setPassportState(PassportState.INVALID_PASSPORT);
-        }
-      });
-  };
-
   useEffect(() => {
+    const checkPassport = () => {
+      setPassportState(PassportState.LOADING);
+      const url = `${process.env.REACT_APP_PASSPORT_API_ENDPOINT}/registry/score/${process.env.REACT_APP_PASSPORT_API_COMMUNITY_ID}/${address}`;
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.REACT_APP_PASSPORT_API_KEY}`
+        },
+      })
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          } else {
+            setError(resp);
+            throw resp;
+          }
+        })
+        .then((data: PassportResponse) => {
+          if (data) {
+            if (Number(data.score) >= PASSPORT_THRESHOLD) {
+              setPassportState(PassportState.MATCH_ELIGIBLE);
+            } else {
+              setPassportState(PassportState.MATCH_INELIGIBLE);
+            }
+            setPassport(data);
+          } else {
+            setPassportState(PassportState.INVALID_PASSPORT);
+          }
+        })
+        .catch((err: Response) => {
+          switch (err.status) {
+            case 400: // unregistered/nonexistent passport address
+              setPassportState(PassportState.INVALID_PASSPORT);
+              console.log(err.json());
+              break;
+            case 401: // invalid API key
+              setPassportState(PassportState.ERROR);
+              console.log(err.json());
+              break;
+            default:
+              setPassportState(PassportState.ERROR);
+              console.error("Error fetching passport", err);
+          }
+        });
+    };
+
     if (isConnected) {
       // NOTE: we should cache these results and state
       checkPassport();
@@ -60,7 +87,7 @@ export function usePassport() {
   }, [isConnected, address]);
 
   return {
-    data: passport,
+    passport,
     error,
     passportState,
     setPassportState,
