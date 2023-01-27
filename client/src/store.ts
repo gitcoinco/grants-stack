b2f5a0f5832dc3ebef151686550ef48636c5ffff
Mohamed Boudra <boudra.moha@gmail.com>
2023-01-27 10:22:53 +0100

    remove redux logger

--- client/src/store.ts
+++ client/src/store.ts
@@ -3,7 +3,6 @@ import {
   createStore,
   Dispatch,
   Middleware,
-  MiddlewareAPI,
   PreloadedState,
 } from "redux";
 import { createRouterMiddleware } from "@lagunovsky/redux-react-router";
@@ -12,30 +11,10 @@ import { composeWithDevTools } from "@redux-devtools/extension";
 import { RootState, createRootReducer } from "./reducers";
 import history from "./history";
 
-const logger: Middleware =
-  ({ getState }: MiddlewareAPI) =>
-  (next: Dispatch) =>
-  (action) => {
-    console.log("dispatch", action);
-    const returnValue = next(action);
-    console.log("state", getState());
-    return returnValue;
-  };
-
 const routerMiddleware = createRouterMiddleware(history);
 const composeEnhancers = composeWithDevTools({});
 
 let middlewares: Middleware[] = [thunkMiddleware, routerMiddleware];
-
-const urlParams = new URLSearchParams(window.location.search);
-if (
-  (process.env.NODE_ENV !== "production" &&
-    process.env.NODE_ENV !== "test" &&
-    process.env.REACT_APP_ENABLE_REDUX_LOGGER !== "false") ||
-  urlParams.get("debug") !== null
-) {
-  middlewares = [...middlewares, logger];
-}
 
 const setupStore = (preloadedState?: PreloadedState<RootState>) =>
   createStore(
