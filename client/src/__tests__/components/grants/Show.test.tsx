ab1f45f95f6b8e2bb7ee7d19e5fe39e4cbfba384
Mohamed Boudra <boudra.moha@gmail.com>
2023-01-27 10:05:20 +0100

    format

--- client/src/__tests__/components/grants/Show.test.tsx
+++ client/src/__tests__/components/grants/Show.test.tsx
@@ -19,12 +19,12 @@ jest.mock("react-router-dom", () => ({
 
 jest.mock("../../../actions/projects", () => ({
   ...jest.requireActual("../../../actions/projects"),
-  loadAllChainsProjects: () => ({ type: "TEST", })
+  loadAllChainsProjects: () => ({ type: "TEST" }),
 }));
 
 jest.mock("../../../actions/grantsMetadata", () => ({
   ...jest.requireActual("../../../actions/grantsMetadata"),
-  fetchGrantData: () => ({ type: "TEST", })
+  fetchGrantData: () => ({ type: "TEST" }),
 }));
 
 jest.mock("../../../utils/projects", () => ({
@@ -32,20 +32,23 @@ jest.mock("../../../utils/projects", () => ({
   fetchProjectOwners: jest.fn(),
 }));
 
-import {fetchProjectOwners} from "../../../utils/projects";
+import { fetchProjectOwners } from "../../../utils/projects";
 import { web3AccountLoaded, web3ChainIDLoaded } from "../../../actions/web3";
 import { grantMetadataFetched } from "../../../actions/grantsMetadata";
-
 
 describe("<Show />", () => {
   let store: any;
 
   beforeEach(() => {
     store = setupStore();
-    store.dispatch(grantMetadataFetched(buildProjectMetadata({
-      id: "1:0x0000000000000000000000000000000000000001:1",
-      credentials: {},
-    })));
+    store.dispatch(
+      grantMetadataFetched(
+        buildProjectMetadata({
+          id: "1:0x0000000000000000000000000000000000000001:1",
+          credentials: {},
+        })
+      )
+    );
     store.dispatch(web3ChainIDLoaded(5));
     store.dispatch(web3AccountLoaded("0x123"));
   });
@@ -62,8 +65,8 @@ describe("<Show />", () => {
       expect((fetchProjectOwners as jest.Mock).mock.calls[0][1]).toBe("1");
 
       await waitFor(() => {
-        expect(dom.getByText("Edit")).toBeInTheDocument()
-      })
+        expect(dom.getByText("Edit")).toBeInTheDocument();
+      });
     });
 
     it("hides when the user is not an owner", async () => {
@@ -77,9 +80,8 @@ describe("<Show />", () => {
       expect((fetchProjectOwners as jest.Mock).mock.calls[0][1]).toBe("1");
 
       await waitFor(() => {
-        expect(dom.queryByText("Edit")).not.toBeInTheDocument()
-      })
+        expect(dom.queryByText("Edit")).not.toBeInTheDocument();
+      });
     });
   });
-
 });
