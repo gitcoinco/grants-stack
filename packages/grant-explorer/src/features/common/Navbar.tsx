9509dd22be9cb3f1193859bb18ff77c327b32dae
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-01-31 15:16:47 +0530

    feat: update nav bar link

--- packages/grant-explorer/src/features/common/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { ReactComponent as GitcoinLogo } from "../../assets/gitcoinlogo-black.svg";
 import { ReactComponent as GrantsExplorerLogo } from "../../assets/topbar-logos-black.svg";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -11,6 +11,7 @@ export interface NavbarProps {
 
 export default function Navbar(props: NavbarProps) {
   const [shortlist] = useBallot();
+  const { chainId, roundId } = useParams();
 
   return (
     <nav className="bg-white fixed w-full z-10">
@@ -18,7 +19,7 @@ export default function Navbar(props: NavbarProps) {
         <div className="flex justify-between h-16">
           <div className="flex">
             <Link
-              to="#"
+              to={`/round/${chainId}/${roundId}`}
               className="flex-shrink-0 flex items-center"
               data-testid={"home-link"}
             >
