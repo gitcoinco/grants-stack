29519cd3289c36a7fd86457cf65277859c7ca7b1
Josef Vacek <vacekj@outlook.com>
2023-02-10 16:56:14 +0100

    fix: type eslint

--- packages/eslint-config-gitcoin/index.js
+++ packages/eslint-config-gitcoin/index.js
@@ -2,9 +2,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:react/jsx-runtime",
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
