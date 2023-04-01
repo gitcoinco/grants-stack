9d38499fe79e00d562c82a2e9780f2f06442e3a0
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-03-31 22:49:10 -0700

    chore: final navbar touches

--- packages/grant-explorer/src/features/common/NavbarCart.tsx
+++ packages/grant-explorer/src/features/common/NavbarCart.tsx
@@ -7,7 +7,10 @@ import { CheckIcon, EyeIcon } from "@heroicons/react/24/outline";
 import DefaultLogoImage from "../../assets/default_logo.png";
 import markdown from "../../app/markdown";
 
-export default function NavbarCart(props: { cart: Project[]; roundUrlPath: string }) {
+export default function NavbarCart(props: {
+  cart: Project[];
+  roundUrlPath: string;
+}) {
   const menuRef = useRef(null);
   const [open, setOpen] = useState<boolean>(false);
   const [listening, setListening] = useState<boolean>(false);
@@ -22,86 +25,86 @@ export default function NavbarCart(props: { cart: Project[]; roundUrlPath: strin
   );
 
   return (
-    <div
-      className="relative"
-      onClick={toggleMenu}
-      ref={menuRef}
-    >
-      <QuickViewIcon count={projectCount}/>
-      
+    <div className="relative flex-row" onClick={toggleMenu} ref={menuRef}>
+      <QuickViewIcon count={projectCount} />
+
       {open && Boolean(projectCount) && (
-        <QuickViewSummary
-          roundUrlPath={props.roundUrlPath}
-          cart={props.cart}
-        />
+        <QuickViewSummary roundUrlPath={props.roundUrlPath} cart={props.cart} />
       )}
     </div>
   );
 }
 
-function QuickViewIcon (props: {count: number;}) {
+function QuickViewIcon(props: { count: number }) {
   return (
     <>
-      <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 7H3L3.4 9M5 17H15L19 9H3.4M5 17L3.4 9M5 17L2.70711 19.2929C2.07714 19.9229 2.52331 21 3.41421 21H15M15 21C13.8954 21 13 21.8954 13 23C13 24.1046 13.8954 25 15 25C16.1046 25 17 24.1046 17 23C17 21.8954 16.1046 21 15 21ZM7 23C7 24.1046 6.10457 25 5 25C3.89543 25 3 24.1046 3 23C3 21.8954 3.89543 21 5 21C6.10457 21 7 21.8954 7 23Z" stroke="#0E0333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        { props.count ? <circle cx="18" cy="8" r="8" fill="#6F3FF5"/> : null }
+      <svg
+        width="26"
+        height="26"
+        viewBox="0 0 26 26"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 7H3L3.4 9M5 17H15L19 9H3.4M5 17L3.4 9M5 17L2.70711 19.2929C2.07714 19.9229 2.52331 21 3.41421 21H15M15 21C13.8954 21 13 21.8954 13 23C13 24.1046 13.8954 25 15 25C16.1046 25 17 24.1046 17 23C17 21.8954 16.1046 21 15 21ZM7 23C7 24.1046 6.10457 25 5 25C3.89543 25 3 24.1046 3 23C3 21.8954 3.89543 21 5 21C6.10457 21 7 21.8954 7 23Z"
+          stroke="#0E0333"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        {props.count ? <circle cx="18" cy="8" r="8" fill="#6F3FF5" /> : null}
       </svg>
 
       {Boolean(props.count) && (
         <div
-        className="inline-flex absolute top-0.5 pt-0.5 pl-1 left-2 justify-center items-center w-4 h-3 text-white"
-        style={{
-          fontSize: 7.5,
-        }}
+          className="inline-flex absolute top-0.5 pt-0.5 pl-1 left-2 justify-center items-center w-4 h-3 text-white"
+          style={{
+            fontSize: 7.5,
+          }}
         >
           {props.count}
         </div>
       )}
     </>
-  )
+  );
 }
 
-function QuickViewSummary (props: {
-  roundUrlPath: string;
-  cart: Project[]
-}) {
+function QuickViewSummary(props: { roundUrlPath: string; cart: Project[] }) {
   const navigate = useNavigate();
 
   return (
-    <div className="w-72 flex flex-col absolute right-0 rounded bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)] py-4 px-4 z-20">
-      <p className="flex border-b mb-4 gap-2">
+    <div className="mt-4 w-72 flex flex-col absolute right-0 rounded bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)] py-4 px-4 z-20">
+      <p className="flex border-b mb-4 pb-4 gap-2">
         <CheckIcon className="w-5" />
         Projects in your cart
       </p>
 
       {/* TODO: Add fixed height + scroll */}
-      <div className="mb-4 max-h-[100px] overflow-y-scroll">
-        {props.cart.map((project) =>
-          <ProjectQuickView
-            roundUrlPath={props.roundUrlPath}
-            project={project}
-          />
-        )}
+      <div className="max-h-[100px] overflow-y-scroll">
+        {props.cart &&
+          props.cart.map((project) => (
+            <ProjectQuickView
+              roundUrlPath={props.roundUrlPath}
+              project={project}
+            />
+          ))}
       </div>
 
       <Button
         type="button"
         $variant="solid"
         className="px-3 bg-violet-400 text-white border-0 text-xs"
-        onClick={() =>
-          navigate(`${props.roundUrlPath}/cart`)
-        }
+        onClick={() => navigate(`${props.roundUrlPath}/cart`)}
       >
-        Checkout Cart
+        Checkout
       </Button>
-
     </div>
-  )
+  );
 }
 
-function ProjectQuickView(props: { roundUrlPath: string; project: Project} ) {
-  return(
-    <div className="flex mb-3">
+function ProjectQuickView(props: { roundUrlPath: string; project: Project }) {
+  return (
+    <div className="flex mb-4">
       <div className="relative overflow-hidden bg-no-repeat bg-cover  min-w-[64px] w-16 max-h-[64px] mt-auto mb-auto">
         <img
           className="inline-block"
@@ -113,7 +116,9 @@ function ProjectQuickView(props: { roundUrlPath: string; project: Project} ) {
           alt={"Project Logo"}
         />
         <div className="min-w-[64px] w-16 max-h-[64px] absolute top-0 right-0 bottom-0 left-0 overflow-hidden bg-fixed opacity-0 hover:opacity-70 transition duration-300 ease-in-out bg-gray-500 justify-center flex items-center">
-          <Link to={`${props.roundUrlPath}/${props.project.grantApplicationId}`}>
+          <Link
+            to={`${props.roundUrlPath}/${props.project.grantApplicationId}`}
+          >
             <EyeIcon
               className="fill-gray-200 w-6 h-6 cursor-pointer"
               data-testid={`${props.project.projectRegistryId}-project-link`}
@@ -123,21 +128,17 @@ function ProjectQuickView(props: { roundUrlPath: string; project: Project} ) {
       </div>
 
       <div className="pl-4 mt-1">
-        <Link
-          to={`${props.roundUrlPath}/${props.project.grantApplicationId}`}
-        >
-        <p className="font-semibold mb-2 text-sm text-ellipsis line-clamp-1">
-          {props.project.projectMetadata.title}
-        </p>
+        <Link to={`${props.roundUrlPath}/${props.project.grantApplicationId}`}>
+          <p className="font-semibold mb-2 text-sm text-ellipsis line-clamp-1">
+            {props.project.projectMetadata.title}
+          </p>
         </Link>
         <p className="text-xs text-ellipsis line-clamp-3">
-          {
-            markdown
-              .renderToPlainText(props.project.projectMetadata.description)
-              .substring(0, 20)
-          }
+          {markdown
+            .renderToPlainText(props.project.projectMetadata.description)
+            .substring(0, 20)}
         </p>
       </div>
     </div>
-  )
+  );
 }
