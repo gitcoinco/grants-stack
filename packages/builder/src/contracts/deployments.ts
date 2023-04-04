3f73761f8b297e24405b85429c1080ad63f3187d
jaxcoder / <codenamejason@outlook.com>
2023-04-04 09:06:31 -0400

    chore: update fe for latest goerli deploy (#1336)
    * chore: update project registry address
    
    * update round implementation
    
    * update round contract addresses
    
    * fix: put in wrong place
    
    * fix: update round address
    
    * feat: update contracts
    
    ---------
    
    Co-authored-by: bhargavaparoksham <aparokshambhargav@gmail.com>

--- packages/builder/src/contracts/deployments.ts
+++ packages/builder/src/contracts/deployments.ts
@@ -19,13 +19,13 @@ export const addresses: { [key: string]: any } = {
     projectRegistry: "0x03506eD3f57892C85DB20C36846e9c808aFe9ef4",
   },
   goerli: {
-    projectRegistry: "0x832c5391dc7931312CbdBc1046669c9c3A4A28d5",
+    projectRegistry: "0xa71864fAd36439C50924359ECfF23Bb185FFDf21",
   },
   fantomTestnet: {
     projectRegistry: "0x984749e408FF0446d8ADaf20E293F2F299396631",
   },
   fantom: {
-    projectRegistry: "0x8e1bD5Da87C14dd8e08F7ecc2aBf9D1d558ea174",
+    projectRegistry: "0x8e1bD5Da87C14dd808F7ecc2aBf9D1d558ea174",
   },
 };
 
