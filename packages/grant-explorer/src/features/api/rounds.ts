e65954736252fcccfa5be8fb285f1b9c44907e2e
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-04-17 14:55:56 +0530

    chore: add subgraph query

---
+++ packages/grant-explorer/src/features/api/rounds.ts
@@ -0,0 +1,130 @@
+import { ChainId, graphql_fetch } from "common";
+import { RoundMetadata } from "./round";
+import { MetadataPointer } from "./types";
+import { fetchFromIPFS } from "./utils";
+
+interface GetRoundOverviewResult {
+  data: {
+    rounds: RoundOverview[];
+  };
+}
+
+type RoundOverview = {
+  id: string;
+  roundMetaPtr: MetadataPointer;
+  applicationMetaPtr: MetadataPointer;
+  applicationsStartTime: string;
+  applicationsEndTime: string;
+  roundStartTime: string;
+  roundEndTime: string;
+  matchAmount: string;
+  token: string;
+  name ?: string;
+}
+
+async function fetchRoundsByTimestamp(query: string, chainId: string): Promise<RoundOverview[]> {
+  
+  try {
+    const chainIdEnumValue = ChainId[chainId as keyof typeof ChainId];
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+
+    const res: GetRoundOverviewResult = await graphql_fetch(
+      query,
+      chainIdEnumValue,
+      { currentTimestamp }
+    );
+
+    let rounds: RoundOverview[] = res.data.rounds;
+    rounds.forEach(async (round, index) => {
+
+      const roundMetadata: RoundMetadata = await fetchFromIPFS(
+        round.roundMetaPtr.pointer
+      );
+      rounds[index].name = roundMetadata.name;
+    });
+
+    return rounds;
+
+  } catch (error) {
+    console.log("error: fetchRoundsByTimestamp", error);
+    return [];
+  }
+}
+
+export async function getRoundsInApplicationPhase(): Promise<RoundOverview[]> {
+  try {
+
+    const chainIds = Object.keys(ChainId)
+    let rounds: RoundOverview[] = [];
+
+    const query = `
+      query GetRoundsInApplicationPhase($current_timestamp: Number) {
+        rounds(where: {
+          applicationsStartTime_lt: $current_timestamp 
+          applicationsEndTime_gt: $current_timestamp
+        }) {
+          id
+          roundMetaPtr {
+            protocol
+            pointer
+          }
+          applicationsStartTime
+          applicationsEndTime
+          roundStartTime
+          roundEndTime
+          matchAmount
+          token
+        }
+      }
+    `;
+
+    chainIds.forEach(async chainId => {
+      let roundsForChainId = await fetchRoundsByTimestamp(query, chainId);
+      rounds.push(...roundsForChainId);
+    })
+
+    return rounds;
+  } catch (error) {
+    console.error("getRoundsInApplicationPhase", error);
+    throw Error("Unable to fetch rounds");
+  }
+}
+
+export async function getActiveRounds(): Promise<RoundOverview[]> {
+  try {
+
+    const chainIds = Object.keys(ChainId)
+    let rounds: RoundOverview[] = [];
+
+    const query = `
+      query GetRoundsInApplicationPhase($current_timestamp: Number) {
+        rounds(where: {
+          roundStartTime_lt: $current_timestamp 
+          roundEndTime_gt: $current_timestamp
+        }) {
+          id
+          roundMetaPtr {
+            protocol
+            pointer
+          }
+          applicationsStartTime
+          applicationsEndTime
+          roundStartTime
+          roundEndTime
+          matchAmount
+          token
+        }
+      }
+    `;
+
+    chainIds.forEach(async chainId => {
+      let roundsForChainId = await fetchRoundsByTimestamp(query, chainId);
+      rounds.push(...roundsForChainId);
+    })
+
+    return rounds;
+  } catch (error) {
+    console.error("getActiveRounds", error);
+    throw Error("Unable to fetch rounds");
+  }
+}
