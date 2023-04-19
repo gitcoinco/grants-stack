48f187798cd6e3f24c8aa8ca393ef97fbcfbb9dc
Jaxcoder <codenamejason@outlook.com>
2023-04-19 12:22:57 -0400

    adding props, css and more layout items

--- packages/grant-explorer/src/features/discovery/LandingPage.tsx
+++ packages/grant-explorer/src/features/discovery/LandingPage.tsx
@@ -1,34 +1,54 @@
 import { Round } from "../api/types";
 import RoundCard from "./RoundCard";
 
-const mockRoundData: Round = {
-  id: "0x1234",
-  roundMetadata: undefined,
-  store: undefined,
-  applicationStore: undefined,
-  votingStrategy: "",
-  roundStartTime: new Date(),
-  roundEndTime: new Date(),
-  applicationsStartTime: new Date(),
-  applicationsEndTime: new Date(),
-  token: "",
-  ownedBy: "",
-  approvedProjects: [],
-};
+const mockRoundData: Round[] = [
+  {
+    id: "0x1234",
+    roundMetadata: undefined,
+    store: undefined,
+    applicationStore: undefined,
+    votingStrategy: "",
+    roundStartTime: new Date(),
+    roundEndTime: new Date(),
+    applicationsStartTime: new Date(),
+    applicationsEndTime: new Date(),
+    token: "",
+    ownedBy: "",
+    approvedProjects: [],
+  },
+  {
+    id: "0x1235",
+    roundMetadata: undefined,
+    store: undefined,
+    applicationStore: undefined,
+    votingStrategy: "",
+    roundStartTime: new Date(),
+    roundEndTime: new Date(),
+    applicationsStartTime: new Date(),
+    applicationsEndTime: new Date(),
+    token: "",
+    ownedBy: "",
+    approvedProjects: [],
+  },
+];
 
 console.log("mockRoundData", mockRoundData);
 
 const LandingPage = () => {
+  // todo: fetch all the round data from the indexer
+  // todo: sort the rounds by application period and then by voting period
+
+  const roundData = mockRoundData;
   return (
     <div className="">
       {/* <LandingHeader /> */}
       <TitleSection />
       <hr className="text-grey-100 mx-4 my-8" />
       <div>
-        <ApplyNowSection />
+        <ApplyNowSection roundData={roundData} />
       </div>
       <div>
-        <AllActviveRoundsSection />
+        <AllActviveRoundsSection roundData={roundData} />
       </div>
     </div>
   );
@@ -37,41 +57,44 @@ const LandingPage = () => {
 const TitleSection = () => {
   return (
     <div className="mt-96">
-      <span className="text-[30px] text-grey-500 mx-4">
+      <span className="md:text-[30px] text-[24px] text-grey-500 mx-4">
         Browse through active rounds
       </span>
     </div>
   );
 };
 
-const ApplyNowSection = () => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ApplyNowSection = (props: { roundData: Round[] }) => {
   return (
     <div className="mx-4">
       <div>
-        <span className="text-grey-400 text-[24px]">Apply Now</span>
-        <div className="flex items-center justify-between">
+        <span className="text-grey-400 md:text-[24] text-[20px]">
+          Apply Now
+        </span>
+        <div className="flex flex-row items-center justify-between">
           <span className="text-grey-400">
             Rounds currently accepting applications
           </span>
-          <a className="cursor-pointerß" href="/">
-            View All (10)
+          <a className="cursor-pointer mr-1" href="/">
+            View All ({props.roundData.length.toString()})
           </a>
         </div>
       </div>
-      <div className="flex justify-between items-center p-2 m-2">
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
+      <div className="flex lg:flex-row md:flex-row sm:flex-row flex-col justify-between items-center">
+        {props.roundData.map((round) => {
+          return <RoundCard round={round} />;
+        })}
       </div>
     </div>
   );
 };
 
-const AllActviveRoundsSection = () => {
+const AllActviveRoundsSection = (props: { roundData: Round[] }) => {
   return (
     <div className="mx-4">
       <span className="text-grey-400 text-[24px]">
-        All Active Rounds<span> (20)</span>
+        All Active Rounds<span> ({props.roundData.length.toString()})</span>
       </span>
       <div className="flex justify-between items-center mb-1">
         <div className="flex">
@@ -92,16 +115,17 @@ const AllActviveRoundsSection = () => {
         </div>
       </div>
 
-      <div className="flex justify-between items-center">
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
-        <RoundCard round={mockRoundData} />
+      <div className="flex lg:flex-row md:flex-row flex-col justify-between items-center">
+        {props.roundData.map((round) => {
+          return <RoundCard round={round} />;
+        })}
+        {/* todo: added for layout purposes, remove after */}
+        <RoundCard round={mockRoundData[0]} />
+        <RoundCard round={mockRoundData[0]} />
+        <RoundCard round={mockRoundData[0]} />
+        <RoundCard round={mockRoundData[0]} />
+        <RoundCard round={mockRoundData[0]} />
+        <RoundCard round={mockRoundData[0]} />
       </div>
     </div>
   );
--- packages/grant-explorer/src/features/discovery/RoundCard.tsx
+++ packages/grant-explorer/src/features/discovery/RoundCard.tsx
@@ -1,11 +1,14 @@
 import { Card, CardBody, Image, Stack } from "@chakra-ui/react";
+import { useNetwork } from "wagmi";
 import { Round } from "../api/types";
+import { CHAINS } from "../api/utils";
 
-const RoundCard = ({ round }: { round: Round }) => {
-  console.log("round", round);
+const RoundCard = (props: { round: Round }) => {
+  // todo: this is just a mock to show an image, remove this when we have the round data
+  const { chain } = useNetwork();
 
   return (
-    <Card className="border border-grey-100 m-1 rounded shadow-lg min-h-[300px]">
+    <Card className="border border-grey-100 m-1 rounded shadow-lg min-h-[300px] min-w-[400px]">
       <CardBody>
         <Image
           src="https://s3-alpha-sig.figma.com/img/c71c/562a/94d2299f9156d7042db50416393a5b35?Expires=1682899200&Signature=B6jQh~BCXJo5H5Vx6OOP9nrsz9AuuH1y9xwtS36e2iO5j-2MBcSO4o2Ld45Eam1kh4wYxoVqEtDwB9Z0L3z69Xyk79vHw-SxVhTiSJTWM6RHR-ud1MSvZGGz-EiYr05TF7FeCKauQb3hOvtyBLs-w5kUpcThcTPBR8Lgq7nPHCQAPZJUnEnDnu7zwj-5qK64A6bVqHtvl8upKV1aFeDodi5W6dN8T-O~~B1bsRzWsLnsdowQfxgeUZ8ZWkg803YRV8XRMUbAlXN4~GZV2DV3SbJZDkQqB~o~PNPcERinOAz7BGrNK0IEykFTONeqhlrZGkADuVhdW08QQ5dVbyhPYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -20,7 +23,11 @@ const RoundCard = ({ round }: { round: Round }) => {
             lacus felis et et nisl eget nisl eu. Fringilla lorem libero vel ut
             orci varius iaculis augue pulvinar.
           </p>
-          <span className="text-xs mx-1">5 Days left in round</span>
+          <hr />
+          <div className="flex items-center justify-between">
+            <span className="text-xs mx-1">{props.round.roundEndTime.toString()}</span>
+            <img src={CHAINS[chain?.id ?? 1].logo} alt="Chain Logo" />
+          </div>
         </Stack>
       </CardBody>
     </Card>
