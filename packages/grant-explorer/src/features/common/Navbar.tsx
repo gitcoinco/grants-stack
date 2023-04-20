634f5703e5856cf11f8c5efdf0c9b88f5cb72fa3
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-04-20 11:49:16 +0530

    chore: refactor UI

--- packages/grant-explorer/src/features/common/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar.tsx
@@ -1,35 +1,50 @@
-// import { ReactComponent as GitcoinLogo } from "../../assets/gitcoinlogo-black.svg";
-// import { ReactComponent as GrantsExplorerLogo } from "../../assets/topbar-logos-black.svg";
+import { Link, useParams } from "react-router-dom";
+import { ReactComponent as GitcoinLogo } from "../../assets/gitcoinlogo-black.svg";
+import { ReactComponent as GrantsExplorerLogo } from "../../assets/topbar-logos-black.svg";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useCart } from "../../context/CartContext";
 import CustomerSupport from "./CustomerSupport";
-import NavBarLogo from "./NavBarLogo";
 import NavbarCart from "./NavbarCart";
 
 export interface NavbarProps {
   roundUrlPath: string;
   customBackground?: string;
   isBeforeRoundEndDate?: boolean;
+  hideWalletInteraction?: boolean
 }
 
 export default function Navbar(props: NavbarProps) {
   const [cart] = useCart();
-  // const { chainId, roundId } = useParams();
+  const { chainId, roundId } = useParams();
 
   return (
     <nav className={`bg-white fixed w-full z-10 ${props.customBackground}`}>
       <div className="mx-auto px-4 sm:px-6 lg:px-20">
         <div className="flex justify-between h-16">
-          <NavBarLogo url="/" />
-          <div className="flex items-center gap-6">
-            <div data-testid="connect-wallet-button" id="connect-wallet-button">
-              <ConnectButton />
-            </div>
-            {props.isBeforeRoundEndDate &&
-              <NavbarCart cart={cart} roundUrlPath={props.roundUrlPath} />
-            }
-            <CustomerSupport />
+          <div className="flex">
+            <Link
+              to={`/round/${chainId}/${roundId}`}
+              className="flex-shrink-0 flex items-center"
+              data-testid={"home-link"}
+            >
+              <GitcoinLogo className="block h-8 w-auto" />
+              <div className="hidden lg:block md:block">
+                <span className="mx-6 text-grey-400">|</span>
+                <GrantsExplorerLogo className="lg:inline-block md:inline-block" />
+              </div>
+            </Link>
           </div>
+          {props.hideWalletInteraction && 
+            <div className="flex items-center gap-6">
+              <div data-testid="connect-wallet-button" id="connect-wallet-button">
+                <ConnectButton />
+              </div>
+              {props.isBeforeRoundEndDate &&
+                <NavbarCart cart={cart} roundUrlPath={props.roundUrlPath} />
+              }
+              <CustomerSupport />
+            </div>
+          }
         </div>
       </div>
     </nav>
--- packages/grant-explorer/src/features/discovery/RoundCard.tsx
+++ packages/grant-explorer/src/features/discovery/RoundCard.tsx
@@ -1,60 +1,105 @@
-import { Card, CardBody, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useNetwork } from "wagmi";
 // import { RoundOverview } from "../api/rounds";
 import { CHAINS } from "../api/utils";
+import { BasicCard, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../common/styles";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const RoundCard = (props: { round: any }) => {
-  // todo: this is just a mock to show an image, remove this when we have the round data
+
   const { chain } = useNetwork();
-  // const roundName = props.round?.roundMetadata?.name;
   const roundEndTime = props.round && Number(props.round.roundEndTime);
-  const daysLeftInMs: number = roundEndTime - new Date().getSeconds();
+  const daysLeftInMs = roundEndTime - new Date().getSeconds();
   const daysLeft = Math.ceil(daysLeftInMs / (1000 * 60 * 60 * 24));
-  console.log("roundName: ", { daysLeft, props });
+  const matchAmount = 100;
+  const token = "ETH";
+  const approvedApplicationsCount = 10;
+  const roundName = "Round Name";
 
   return (
-    <div className="mb-6">
-      <Link to="/round/id">
-        <Card className="border border-grey-100 m-1 rounded shadow-lg">
-          <CardBody>
-            {/* todo: use random images provided */}
-            <Image
-              src="https://s3-alpha-sig.figma.com/img/c71c/562a/94d2299f9156d7042db50416393a5b35?Expires=1682899200&Signature=B6jQh~BCXJo5H5Vx6OOP9nrsz9AuuH1y9xwtS36e2iO5j-2MBcSO4o2Ld45Eam1kh4wYxoVqEtDwB9Z0L3z69Xyk79vHw-SxVhTiSJTWM6RHR-ud1MSvZGGz-EiYr05TF7FeCKauQb3hOvtyBLs-w5kUpcThcTPBR8Lgq7nPHCQAPZJUnEnDnu7zwj-5qK64A6bVqHtvl8upKV1aFeDodi5W6dN8T-O~~B1bsRzWsLnsdowQfxgeUZ8ZWkg803YRV8XRMUbAlXN4~GZV2DV3SbJZDkQqB~o~PNPcERinOAz7BGrNK0IEykFTONeqhlrZGkADuVhdW08QQ5dVbyhPYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-              alt="Round Header Image"
-              borderRadius="sm"
-            />
-            <div className="m-2">
-              <span className="text-[16px] text-grey-500">{"Round Name"}</span>
-              <span className="text-sm">
-                Round description here lorem ipsum dolor sit amet consectetur.
-                Magna pulvinar sit tincidunt viverra lectus malesuada et.
-                Elementum eros lacus felis et et nisl eget nisl eu. Fringilla
-                lorem libero vel ut orci varius iaculis augue pulvinar.
-              </span>
-              <div className="flex items-center justify-between">
-                <span className="text-xs mt-2">
-                  {daysLeft} {daysLeft === 1 ? "day" : "days"} left
-                </span>
-              </div>
-              <hr className="my-2" />
-              <div className="flex flex-row items-center justify-between text-xs">
-                <div>
-                  <span className="mr-1">123 Projects</span>
-                  <span className="mr-1">|</span>
-                  <span className="mr-1">$456K funded</span>
-                  <span className="mr-1">|</span>
-                  <span>$1M match payout</span>
-                </div>
-                <img src={CHAINS[chain?.id ?? 1].logo} alt="Chain Logo" />
-              </div>
-            </div>
-          </CardBody>
-        </Card>
+    <BasicCard className="w-full mb-3">
+      <Link
+        to="/round/id" // TODO: replace with actual round id
+        data-testid="round-card"
+      >
+        <CardHeader>
+          <RoundBanner />
+        </CardHeader>
+
+        <CardContent>
+          <CardTitle data-testid="round-title">
+            {roundName}
+          </CardTitle>
+          <CardDescription data-testid="round-description" className="h-[90px]">
+            {/* {renderToPlainText(project.projectMetadata.description)} */}
+            Round description here lorem ipsum dolor sit amet consectetur.
+            Magna pulvinar sit tincidunt viverra lectus malesuada et.
+            Elementum eros lacus felis et et nisl eget nisl eu. Fringilla
+            lorem libero vel ut orci varius iaculis augue pulvinar.
+          </CardDescription>
+          <p className="mt-4 text-xs" data-testid="days-left">
+            {daysLeft} {daysLeft === 1 ? "day" : "days"} left in round
+          </p>
+        </CardContent>
       </Link>
-    </div>
+
+
+      <div className="bg-white border-t">
+        <CardContent className="text-xs mt-3 pb-0">
+          <RoundCardStat
+            chainId={chain!.id}
+            matchAmount={matchAmount}
+            token={token}
+            approvedApplicationsCount={approvedApplicationsCount}
+          />
+        </CardContent>
+      </div>
+    </BasicCard>
   );
 };
+
+function RoundBanner() {
+
+  const BANNER_IMAGE = "https://s3-alpha-sig.figma.com/img/c71c/562a/94d2299f9156d7042db50416393a5b35?Expires=1682899200&Signature=B6jQh~BCXJo5H5Vx6OOP9nrsz9AuuH1y9xwtS36e2iO5j-2MBcSO4o2Ld45Eam1kh4wYxoVqEtDwB9Z0L3z69Xyk79vHw-SxVhTiSJTWM6RHR-ud1MSvZGGz-EiYr05TF7FeCKauQb3hOvtyBLs-w5kUpcThcTPBR8Lgq7nPHCQAPZJUnEnDnu7zwj-5qK64A6bVqHtvl8upKV1aFeDodi5W6dN8T-O~~B1bsRzWsLnsdowQfxgeUZ8ZWkg803YRV8XRMUbAlXN4~GZV2DV3SbJZDkQqB~o~PNPcERinOAz7BGrNK0IEykFTONeqhlrZGkADuVhdW08QQ5dVbyhPYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+
+  return (
+    <div>
+      <img
+        className="bg-black h-[120px] w-full object-cover rounded-t"
+        src={BANNER_IMAGE}
+        alt="Round Banner"
+      />
+    </div>
+  );
+}
+
+function RoundCardStat(
+  props: {
+    chainId: number;
+    matchAmount: number;
+    token: string;
+    approvedApplicationsCount: number;
+  }
+) {
+
+  return (
+    <div className="flex justify-between my-auto">
+      <div className="flex text-xs my-auto">
+        <span data-testid="approved-applications-count">
+          {props.approvedApplicationsCount} projects |
+        </span>
+        <span className="mx-2" data-testid="match-amount">
+          {props.matchAmount}
+        </span>
+        <span data-testid="match-token">
+          {props.token}
+        </span>
+      </div>
+
+      <div>
+        <img className="w-5" src={CHAINS[props.chainId ?? 1].logo} alt="Round Chain Logo" />
+      </div>
+    </div>
+  );
+}
 
 export default RoundCard;
