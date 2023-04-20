fb0491cbe0f2982ff3fb079eeaf19686281aa578
Jaxcoder <codenamejason@outlook.com>
2023-04-20 00:01:21 -0400

    update some UI - add stock images

--- packages/grant-explorer/src/features/discovery/LandingPage.tsx
+++ packages/grant-explorer/src/features/discovery/LandingPage.tsx
@@ -1,54 +1,127 @@
-import { Round } from "../api/types";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
+import { FiSearch } from 'react-icons/fi';
+import { useAccount } from "wagmi";
 import RoundCard from "./RoundCard";
+// import { RoundOverview } from "../api/rounds";
 
-const mockRoundData: Round[] = [
+const mockRoundData: any[] = [
   {
-    id: "0x1234",
-    roundMetadata: undefined,
-    store: undefined,
-    applicationStore: undefined,
-    votingStrategy: "",
-    roundStartTime: new Date(),
-    roundEndTime: new Date(),
-    applicationsStartTime: new Date(),
-    applicationsEndTime: new Date(),
-    token: "",
-    ownedBy: "",
-    approvedProjects: [],
+    id: "0x2ffe6a9176349d8ac2f1d1ed2035cc4e48c4efdc",
+    roundMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreia6rhfcnpclbgodphfj5u3sd4cj3w2rkw4tewzox7mcic2ucydxha",
+    },
+    applicationMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreihvaiu2duyijp2se3lorvqruqun3gniwzdx3imwhfaft4pfigmgi4",
+    },
+    applicationsStartTime: "1681812000",
+    applicationsEndTime: "1682380800",
+    roundStartTime: "1682467200",
+    roundEndTime: "1682812800",
+    matchAmount: "10000000000000000000",
+    token: "0x0000000000000000000000000000000000000000",
   },
   {
-    id: "0x1235",
-    roundMetadata: undefined,
-    store: undefined,
-    applicationStore: undefined,
-    votingStrategy: "",
-    roundStartTime: new Date(),
-    roundEndTime: new Date(),
-    applicationsStartTime: new Date(),
-    applicationsEndTime: new Date(),
-    token: "",
-    ownedBy: "",
-    approvedProjects: [],
+    id: "0x3d3f053e6b0e337a5d536ded49b41044ea41325a",
+    roundMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreiarezofvxzfghevjdj6ch7zgbcqeb3ff5ikrf7kcaqjlyw6vv3blu",
+    },
+    applicationMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreih3tt7nud7pobaveca3frnjx4vpotuoi5kqmjuuvjxkaxuq7cy5qy",
+    },
+    applicationsStartTime: "1681149600",
+    applicationsEndTime: "1682640000",
+    roundStartTime: "1682726400",
+    roundEndTime: "1688515200",
+    matchAmount: "1000000000000000000",
+    token: "0x0000000000000000000000000000000000000000",
+  },
+  {
+    id: "0x4bbd7baa41d38952dd1c16c08759947d9d86723b",
+    roundMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreiayjt4t3gf3smsmv7h7dsbwjrajeys3rnwl3znhcdcd2ex53fy2ku",
+    },
+    applicationMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreib74i7kcvswn4lh3sgv756siswtpgwz3czafkklsewjcsyf32hxfa",
+    },
+    applicationsStartTime: "1680864300",
+    applicationsEndTime: "1703894400",
+    roundStartTime: "1703980800",
+    roundEndTime: "1714435200",
+    matchAmount: "10000000000000000",
+    token: "0x0000000000000000000000000000000000000000",
+  },
+  {
+    id: "0xa2eb29ab42629f47fd98e3f4b595a5aa79f004fb",
+    roundMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreidbg4dzwwtlo7foyboq3qgfotpmiheyldueiqb5ou4idnyr3f36lu",
+    },
+    applicationMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreickg52gaujti7t4aqhqov76j4cvpizic3nyq54umt4kkljvcudafu",
+    },
+    applicationsStartTime: "1680865200",
+    applicationsEndTime: "1703980800",
+    roundStartTime: "1704067200",
+    roundEndTime: "1722384000",
+    matchAmount: "10000000000000000",
+    token: "0x0000000000000000000000000000000000000000",
+  },
+  {
+    id: "0xbfb1aba5c8efff130ac5c12f325726684fe701af",
+    roundMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreiayh5ju4dlwvuiqnkq2vhqvai3ew3abqviotideb3dbfhdxecocwe",
+    },
+    applicationMetaPtr: {
+      protocol: 1,
+      pointer: "bafkreifhrqrpo2a6z3vmi7gx5y4kelbdf54tt5jqikamsinwddlymz42be",
+    },
+    applicationsStartTime: "1681754400",
+    applicationsEndTime: "1685491200",
+    roundStartTime: "1685577600",
+    roundEndTime: "1692576000",
+    matchAmount: "10000000000000000000000",
+    token: "0x11fe4b6ae13d2a6055c8d9cf65c55bac32b5d844",
   },
 ];
 
 console.log("mockRoundData", mockRoundData);
 
 const LandingPage = () => {
+  const { address } = useAccount();
   // todo: fetch all the round data from the indexer
+  // const [roundData, setRoundData] = useState<RoundOverview[]>([]);
+
+  useEffect(() => {
+    const fetchRounds = async () => {
+      // const roundsForChain = await getRoundsInApplicationPhase();
+      // setRoundData(roundsForChain);
+    };
+
+    fetchRounds();
+  }, [address]);
   // todo: sort the rounds by application period and then by voting period
 
-  const roundData = mockRoundData;
+  console.log("Landing Page => ", { address });
+
   return (
     <div className="">
       {/* <LandingHeader /> */}
       <TitleSection />
       <hr className="text-grey-100 mx-4 my-8" />
       <div>
-        <ApplyNowSection roundData={roundData} />
+        <ApplyNowSection roundData={mockRoundData} />
       </div>
       <div>
-        <AllActviveRoundsSection roundData={roundData} />
+        <AllActviveRoundsSection roundData={mockRoundData} />
       </div>
     </div>
   );
@@ -65,7 +138,7 @@ const TitleSection = () => {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ApplyNowSection = (props: { roundData: Round[] }) => {
+const ApplyNowSection = (props: { roundData: any[] }) => {
   return (
     <div className="mx-4">
       <div>
@@ -73,39 +146,55 @@ const ApplyNowSection = (props: { roundData: Round[] }) => {
           Apply Now
         </span>
         <div className="flex flex-row items-center justify-between">
-          <span className="text-grey-400">
+          <span className="text-grey-400 mb-4 mt-2">
             Rounds currently accepting applications
           </span>
-          <a className="cursor-pointer mr-1" href="/">
+          <a className="cursor-pointer mr-1 text-violet-400 underline" href="/">
             View All ({props.roundData.length.toString()})
           </a>
         </div>
       </div>
-      <div className="flex lg:flex-row md:flex-row sm:flex-row flex-col justify-between items-center">
-        {props.roundData.map((round) => {
-          return <RoundCard round={round} />;
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
+        {props.roundData.map((round, index) => {
+          return <RoundCard key={index} round={round} />;
         })}
       </div>
     </div>
   );
 };
 
-const AllActviveRoundsSection = (props: { roundData: Round[] }) => {
+// todo: finish the onclick event handler
+const SearchInput = () => {
   return (
-    <div className="mx-4">
-      <span className="text-grey-400 text-[24px]">
-        All Active Rounds<span> ({props.roundData.length.toString()})</span>
-      </span>
+    <div className="relative w-64">
+      <input
+        type="search"
+        placeholder="Search..."
+        className="border border-gray-400 rounded-md pl-10 pr-4 py-2 w-full focus:outline-none focus:border-violet-500"
+      />
+      <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+    </div>
+  )
+}
+
+const AllActviveRoundsSection = (props: { roundData: any[] }) => {
+  return (
+    <div className="mx-4 my-6">
       <div className="flex justify-between items-center mb-1">
-        <div className="flex">
-          <span className="text-grey-400">Rounds that are ongoing</span>
+        <div className="flex flex-col my-4">
+          <span className="text-grey-400 md:text-[24] text-[20px]">
+            All Active Rounds<span> ({props.roundData.length.toString()})</span>
+          </span>
+          <span className="text-grey-400 mb-4 mt-2">Rounds that are ongoing</span>
         </div>
-        <div className="">
-          <div className="">
-            <input className="rounded mx-2" type="search" />
-            <span className="">Sort by</span>
+
+        {/* todo: search/sort feature */}
+        <div>
+          <div className="flex items-center justify-between">
+            {/* <SearchInput /> */}
+            <span className="ml-8">Sort by</span>
             <select
-              className="border-0 cursor-pointer"
+              className="border-0 cursor-pointer text-violet-400 underline"
               placeholder="Select Filter"
             >
               <option>Round End (Earliest)</option>
@@ -115,17 +204,10 @@ const AllActviveRoundsSection = (props: { roundData: Round[] }) => {
         </div>
       </div>
 
-      <div className="flex lg:flex-row md:flex-row flex-col justify-between items-center">
-        {props.roundData.map((round) => {
-          return <RoundCard round={round} />;
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
+        {props.roundData.map((round, index) => {
+          return <RoundCard key={index} round={round} />;
         })}
-        {/* todo: added for layout purposes, remove after */}
-        <RoundCard round={mockRoundData[0]} />
-        <RoundCard round={mockRoundData[0]} />
-        <RoundCard round={mockRoundData[0]} />
-        <RoundCard round={mockRoundData[0]} />
-        <RoundCard round={mockRoundData[0]} />
-        <RoundCard round={mockRoundData[0]} />
       </div>
     </div>
   );
--- packages/grant-explorer/src/features/discovery/RoundCard.tsx
+++ packages/grant-explorer/src/features/discovery/RoundCard.tsx
@@ -1,36 +1,59 @@
-import { Card, CardBody, Image, Stack } from "@chakra-ui/react";
+import { Card, CardBody, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { useNetwork } from "wagmi";
-import { Round } from "../api/types";
+// import { RoundOverview } from "../api/rounds";
 import { CHAINS } from "../api/utils";
 
-const RoundCard = (props: { round: Round }) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const RoundCard = (props: { round: any }) => {
   // todo: this is just a mock to show an image, remove this when we have the round data
   const { chain } = useNetwork();
+  // const roundName = props.round?.roundMetadata?.name;
+  const roundEndTime = props.round && Number(props.round.roundEndTime);
+  const daysLeftInMs: number = roundEndTime - new Date().getSeconds();
+  const daysLeft = Math.ceil(daysLeftInMs / (1000 * 60 * 60 * 24));
+  console.log("roundName: ", { daysLeft, props });
 
   return (
-    <Card className="border border-grey-100 m-1 rounded shadow-lg min-h-[300px] min-w-[400px]">
-      <CardBody>
-        <Image
-          src="https://s3-alpha-sig.figma.com/img/c71c/562a/94d2299f9156d7042db50416393a5b35?Expires=1682899200&Signature=B6jQh~BCXJo5H5Vx6OOP9nrsz9AuuH1y9xwtS36e2iO5j-2MBcSO4o2Ld45Eam1kh4wYxoVqEtDwB9Z0L3z69Xyk79vHw-SxVhTiSJTWM6RHR-ud1MSvZGGz-EiYr05TF7FeCKauQb3hOvtyBLs-w5kUpcThcTPBR8Lgq7nPHCQAPZJUnEnDnu7zwj-5qK64A6bVqHtvl8upKV1aFeDodi5W6dN8T-O~~B1bsRzWsLnsdowQfxgeUZ8ZWkg803YRV8XRMUbAlXN4~GZV2DV3SbJZDkQqB~o~PNPcERinOAz7BGrNK0IEykFTONeqhlrZGkADuVhdW08QQ5dVbyhPYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-          alt="Round Header Image"
-          borderRadius="sm"
-        />
-        <Stack className="m-2">
-          <span className="text-[16px] text-grey-500">Round Name</span>
-          <p className="text-sm">
-            Round description here lorem ipsum dolor sit amet consectetur. Magna
-            pulvinar sit tincidunt viverra lectus malesuada et. Elementum eros
-            lacus felis et et nisl eget nisl eu. Fringilla lorem libero vel ut
-            orci varius iaculis augue pulvinar.
-          </p>
-          <hr />
-          <div className="flex items-center justify-between">
-            <span className="text-xs mx-1">{props.round.roundEndTime.toString()}</span>
-            <img src={CHAINS[chain?.id ?? 1].logo} alt="Chain Logo" />
-          </div>
-        </Stack>
-      </CardBody>
-    </Card>
+    <div className="mb-6">
+      <Link to="/round/id">
+        <Card className="border border-grey-100 m-1 rounded shadow-lg">
+          <CardBody>
+            {/* todo: use random images provided */}
+            <Image
+              src="https://s3-alpha-sig.figma.com/img/c71c/562a/94d2299f9156d7042db50416393a5b35?Expires=1682899200&Signature=B6jQh~BCXJo5H5Vx6OOP9nrsz9AuuH1y9xwtS36e2iO5j-2MBcSO4o2Ld45Eam1kh4wYxoVqEtDwB9Z0L3z69Xyk79vHw-SxVhTiSJTWM6RHR-ud1MSvZGGz-EiYr05TF7FeCKauQb3hOvtyBLs-w5kUpcThcTPBR8Lgq7nPHCQAPZJUnEnDnu7zwj-5qK64A6bVqHtvl8upKV1aFeDodi5W6dN8T-O~~B1bsRzWsLnsdowQfxgeUZ8ZWkg803YRV8XRMUbAlXN4~GZV2DV3SbJZDkQqB~o~PNPcERinOAz7BGrNK0IEykFTONeqhlrZGkADuVhdW08QQ5dVbyhPYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt="Round Header Image"
+              borderRadius="sm"
+            />
+            <div className="m-2">
+              <span className="text-[16px] text-grey-500">{"Round Name"}</span>
+              <span className="text-sm">
+                Round description here lorem ipsum dolor sit amet consectetur.
+                Magna pulvinar sit tincidunt viverra lectus malesuada et.
+                Elementum eros lacus felis et et nisl eget nisl eu. Fringilla
+                lorem libero vel ut orci varius iaculis augue pulvinar.
+              </span>
+              <div className="flex items-center justify-between">
+                <span className="text-xs mt-2">
+                  {daysLeft} {daysLeft === 1 ? "day" : "days"} left
+                </span>
+              </div>
+              <hr className="my-2" />
+              <div className="flex flex-row items-center justify-between text-xs">
+                <div>
+                  <span className="mr-1">123 Projects</span>
+                  <span className="mr-1">|</span>
+                  <span className="mr-1">$456K funded</span>
+                  <span className="mr-1">|</span>
+                  <span>$1M match payout</span>
+                </div>
+                <img src={CHAINS[chain?.id ?? 1].logo} alt="Chain Logo" />
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+      </Link>
+    </div>
   );
 };
 
