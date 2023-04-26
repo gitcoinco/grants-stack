358d2154fb12d4478c4e9e4f4cf546d13dbefd96
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-04-26 20:04:51 +0530

    fix search

--- packages/grant-explorer/src/features/discovery/LandingPage.tsx
+++ packages/grant-explorer/src/features/discovery/LandingPage.tsx
@@ -10,7 +10,6 @@ import ActiveRoundsSection from "./ActiveRoundSection";
 import ApplyNowSection from "./ApplyNowSection";
 
 const LandingPage = () => {
-  window.location.replace("https://grants.gitcoin.co");
 
   const [searchQuery, setSearchQuery] = useState("");
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -18,6 +17,8 @@ const LandingPage = () => {
     RoundOverview[]
   >([]);
   const [activeRounds, setActiveRounds] = useState<RoundOverview[]>([]);
+  const [allActiveRounds, setAllActiveRounds] = useState<RoundOverview[]>([]);
+
   const [applyRoundsLoading, setApplyRoundsLoading] = useState<boolean>(true);
   const [activeRoundsLoading, setActiveRoundsLoading] = useState<boolean>(true);
 
@@ -30,7 +31,7 @@ const LandingPage = () => {
       );
       return () => clearTimeout(timeOutId);
     } else {
-      setActiveRounds(activeRounds);
+      setActiveRounds(allActiveRounds);
     }
   });
 
@@ -65,6 +66,8 @@ const LandingPage = () => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { isLoading, error, rounds } = await getActiveRounds();
         setActiveRounds(rounds);
+
+        setAllActiveRounds(rounds);
         setActiveRoundsLoading(isLoading);
       } catch (error) {
         setActiveRoundsLoading(false);
