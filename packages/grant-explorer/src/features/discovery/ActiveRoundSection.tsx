2dddba33f73c217509b3e32c52550ea74ccbe329
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-04-26 12:53:08 +0530

    explorer cleanup (#1604)
    * improve UX card
    
    * chore: fix chain logo issue
    
    * chore: ensure only round by gitcoin are loaded
    
    * fix

--- packages/grant-explorer/src/features/discovery/ActiveRoundSection.tsx
+++ packages/grant-explorer/src/features/discovery/ActiveRoundSection.tsx
@@ -49,7 +49,7 @@ const ActiveRoundsSection = (props: ActiveRounds) => {
           </div>
         ) : null}
         {activeRoundsCount > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-6 2xl:grid-cols-4">
             {props.roundOverview.map((round, index) => {
               return <RoundCard key={index} round={round} />;
             })}
