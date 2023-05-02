d49943034ea307d0267e00d42c6555a37ac7006e
Jaxcoder <codenamejason@outlook.com>
2023-05-02 09:25:41 -0400

    remove formatting from prettier

--- packages/grant-explorer/src/features/discovery/ApplyNowPage.tsx
+++ packages/grant-explorer/src/features/discovery/ApplyNowPage.tsx
@@ -23,16 +23,12 @@ const ApplyNowPage = () => {
 
   function sortRoundsByTime(rounds: RoundOverview[], order: string) {
     // If order is round_asc, sort in ascending order. Otherwise, sort in descending order.
-    const isAscending = order === "round_asc";
+    const isAscending = order === 'round_asc';
 
     // Use the sort method to sort the rounds array based on the start or end time
     rounds.sort((a: RoundOverview, b: RoundOverview) => {
-      const timeA = isAscending
-        ? Number(a.roundStartTime)
-        : Number(a.roundEndTime);
-      const timeB = isAscending
-        ? Number(b.roundStartTime)
-        : Number(b.roundEndTime);
+      const timeA = isAscending ? Number(a.roundStartTime) : Number(a.roundEndTime);
+      const timeB = isAscending ? Number(b.roundStartTime) : Number(b.roundEndTime);
       return timeA - timeB;
     });
 
@@ -148,11 +144,9 @@ const ApplyNowPage = () => {
           <Spinner />
         ) : applyNowRoundsCount > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-6 2xl:grid-cols-4">
-            {sortRoundsByTime(filteredRoundsInApplicationPhase, order).map(
-              (round, index) => {
-                return <RoundCard key={index} round={round} />;
-              }
-            )}
+            {sortRoundsByTime(filteredRoundsInApplicationPhase, order).map((round, index) => {
+              return <RoundCard key={index} round={round} />;
+            })}
           </div>
         ) : (
           <NoRounds type="apply" />
