0d12e2b5918435f5d48f5faf0d3d23000e157c68
Jaxcoder <codenamejason@outlook.com>
2023-05-18 09:12:06 -0400

    remove an async

--- packages/round-manager/src/context/application/__tests__/BulkUpdateGrantApplicationContext.test.tsx
+++ packages/round-manager/src/context/application/__tests__/BulkUpdateGrantApplicationContext.test.tsx
@@ -7,7 +7,10 @@ import { updateApplicationStatuses } from "../../../features/api/application";
 import { waitForSubgraphSyncTo } from "../../../features/api/subgraph";
 import { ProgressStatus } from "../../../features/api/types";
 import { makeApplication } from "../../../test-utils";
-import { BulkUpdateGrantApplicationProvider, useBulkUpdateGrantApplications } from "../BulkUpdateGrantApplicationContext";
+import {
+  BulkUpdateGrantApplicationProvider,
+  useBulkUpdateGrantApplications,
+} from "../BulkUpdateGrantApplicationContext";
 jest.mock("../../../features/api/application");
 jest.mock("../../../features/api/subgraph");
 jest.mock("../../../features/common/Auth", () => ({
@@ -46,174 +49,219 @@ describe("<BulkUpdateGrantApplicationProvider />", () => {
 
       renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
 
-      await act(async () => fireEvent.click(screen.getByTestId("update-grant-application")));
+      await act(async () =>
+        fireEvent.click(screen.getByTestId("update-grant-application"))
+      );
 
-      await waitFor(() => {
-        expect(
-          screen.findByTestId(`contract-updating-status-is-${ProgressStatus.IN_PROGRESS}`)
-        );
-      }, { timeout: 30000 });
+      await waitFor(
+        () => {
+          expect(
+            screen.findByTestId(
+              `contract-updating-status-is-${ProgressStatus.IN_PROGRESS}`
+            )
+          );
+        },
+        { timeout: 30000 }
+      );
     });
 
-    // it("sets update status to complete when updating contract succeeds", async () => {
-    //   (updateApplicationStatuses as jest.Mock).mockResolvedValue({
-    //     transactionBlockNumber: 100,
-    //   });
-    //   renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
+    it("sets update status to complete when updating contract succeeds", async () => {
+      (updateApplicationStatuses as jest.Mock).mockResolvedValue({
+        transactionBlockNumber: 100,
+      });
+      renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
 
-    //   await act(async () => fireEvent.click(screen.getByTestId("update-grant-application")));
+      await act(async () =>
+        fireEvent.click(screen.getByTestId("update-grant-application"))
+      );
 
-    //   await waitFor(() => {
-    //     expect(
-    //       screen.findByTestId(`contract-updating-status-is-${ProgressStatus.IS_SUCCESS}`)
-    //     );
-    //   }, { timeout: 30000 });
-    // });
+      await waitFor(
+        () => {
+          expect(
+            screen.findByTestId(
+              `contract-updating-status-is-${ProgressStatus.IS_SUCCESS}`
+            )
+          );
+        },
+        { timeout: 30000 }
+      );
+    });
 
-    // it("sets indexing status to in progress when waiting for subgraph to index", async () => {
-    //   const transactionBlockNumber = 10;
-    //   (updateApplicationStatuses as jest.Mock).mockResolvedValue({
-    //     transactionBlockNumber,
-    //   });
-    //   (waitForSubgraphSyncTo as jest.Mock).mockReturnValue(
-    //     new Promise(() => {
-    //       /* do nothing.*/
-    //     })
-    //   );
+    it("sets indexing status to in progress when waiting for subgraph to index", async () => {
+      const transactionBlockNumber = 10;
+      (updateApplicationStatuses as jest.Mock).mockResolvedValue({
+        transactionBlockNumber,
+      });
+      (waitForSubgraphSyncTo as jest.Mock).mockReturnValue(
+        new Promise(() => {
+          /* do nothing.*/
+        })
+      );
 
-    //   renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
+      renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
 
-    //   const createContract = screen.getByTestId("update-grant-application");
-    //   await act(async () => fireEvent.click(createContract));
+      const createContract = screen.getByTestId("update-grant-application");
+      await act(async () => fireEvent.click(createContract));
 
-    //   await waitFor(() => {
-    //     expect(
-    //       screen.findByTestId(`indexing-status-is-${ProgressStatus.IN_PROGRESS}`)
-    //     );
-    //   }, { timeout: 30000 });
-    // });
+      await waitFor(
+        () => {
+          expect(
+            screen.findByTestId(
+              `indexing-status-is-${ProgressStatus.IN_PROGRESS}`
+            )
+          );
+        },
+        { timeout: 30000 }
+      );
+    });
 
-    // it("sets indexing status to completed when subgraph is finished indexing", async () => {
-    //   const transactionBlockNumber = faker.datatype.number();
-    //   (updateApplicationStatuses as jest.Mock).mockResolvedValue({
-    //     transactionBlockNumber,
-    //   });
-    //   (waitForSubgraphSyncTo as jest.Mock).mockResolvedValue({});
+    it("sets indexing status to completed when subgraph is finished indexing", async () => {
+      const transactionBlockNumber = faker.datatype.number();
+      (updateApplicationStatuses as jest.Mock).mockResolvedValue({
+        transactionBlockNumber,
+      });
+      (waitForSubgraphSyncTo as jest.Mock).mockResolvedValue({});
 
-    //   renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
+      renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
 
-    //   const createContract = screen.getByTestId("update-grant-application");
-    //   await act(async () => fireEvent.click(createContract));
+      const createContract = screen.getByTestId("update-grant-application");
+      await act(async () => fireEvent.click(createContract));
 
-    //   await waitFor(() => {
-    //     expect(
-    //       screen.findByTestId(`indexing-status-is-${ProgressStatus.IS_SUCCESS}`)
-    //     );
-    //   }, { timeout: 30000 });
-    // });
+      await waitFor(
+        () => {
+          expect(
+            screen.findByTestId(
+              `indexing-status-is-${ProgressStatus.IS_SUCCESS}`
+            )
+          );
+        },
+        { timeout: 30000 }
+      );
+    });
 
-    // describe("useBulkUpdateGrantApplication Errors", () => {
-    //   let consoleErrorSpy: jest.SpyInstance;
+    describe("useBulkUpdateGrantApplication Errors", () => {
+      let consoleErrorSpy: jest.SpyInstance;
 
-    //   beforeEach(() => {
-    //     consoleErrorSpy = jest
-    //       .spyOn(console, "error")
-    //       .mockImplementation(() => {
-    //         /* do nothing.*/
-    //       });
-    //   });
+      beforeEach(() => {
+        consoleErrorSpy = jest
+          .spyOn(console, "error")
+          .mockImplementation(() => {
+            /* do nothing.*/
+          });
+      });
 
-    //   afterEach(() => {
-    //     consoleErrorSpy.mockClear();
-    //   });
+      afterEach(() => {
+        consoleErrorSpy.mockClear();
+      });
 
-    //   it("sets indexing status to error when waiting for subgraph to sync fails", async () => {
-    //     (updateApplicationStatuses as jest.Mock).mockResolvedValue({
-    //       transactionBlockNumber: 100,
-    //     });
-    //     (waitForSubgraphSyncTo as jest.Mock).mockRejectedValue(new Error(":("));
+      it("sets indexing status to error when waiting for subgraph to sync fails", async () => {
+        (updateApplicationStatuses as jest.Mock).mockResolvedValue({
+          transactionBlockNumber: 100,
+        });
+        (waitForSubgraphSyncTo as jest.Mock).mockRejectedValue(new Error(":("));
 
-    //     renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
-    //     await act(async () =>
-    //       fireEvent.click(screen.getByTestId("update-grant-application")));
+        renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
+        await act(async () =>
+          fireEvent.click(screen.getByTestId("update-grant-application"))
+        );
 
-    //     await waitFor(() => {
-    //       expect(
-    //         screen.getByTestId(`indexing-status-is-${ProgressStatus.IS_ERROR}`)
-    //       ).toBeInTheDocument();
-    //     }, { timeout: 30000 });
-    //   });
+        await waitFor(
+          () => {
+            expect(
+              screen.getByTestId(
+                `indexing-status-is-${ProgressStatus.IS_ERROR}`
+              )
+            ).toBeInTheDocument();
+          },
+          { timeout: 30000 }
+        );
+      });
 
-    //   it("if contract update fails, resets contract updating status when bulk update is retried", async () => {
-    //     (updateApplicationStatuses as jest.Mock)
-    //       .mockRejectedValueOnce(new Error(":("))
-    //       .mockReturnValue(
-    //         new Promise(() => {
-    //           /* do nothing.*/
-    //         })
-    //       );
+      it("if contract update fails, resets contract updating status when bulk update is retried", async () => {
+        (updateApplicationStatuses as jest.Mock)
+          .mockRejectedValueOnce(new Error(":("))
+          .mockReturnValue(
+            new Promise(() => {
+              /* do nothing.*/
+            })
+          );
 
-    //     renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
-    //     await act(async () => fireEvent.click(screen.getByTestId("update-grant-application")));
+        renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
+        await act(async () =>
+          fireEvent.click(screen.getByTestId("update-grant-application"))
+        );
 
-    //     // retry bulk update operation
-    //     await waitFor(() => {
-    //       screen.findByTestId(
-    //         `contract-updating-status-is-${ProgressStatus.IS_ERROR}`
-    //       );
-    //     });
+        // retry bulk update operation
+        await waitFor(() => {
+          screen.findByTestId(
+            `contract-updating-status-is-${ProgressStatus.IS_ERROR}`
+          );
+        });
 
-    //     await act(async () => fireEvent.click(screen.getByTestId("update-grant-application")));
+        await act(async () =>
+          fireEvent.click(screen.getByTestId("update-grant-application"))
+        );
 
-    //     await waitFor(() => {
-    //       expect(
-    //         screen.queryByTestId(
-    //           `contract-updating-status-is-${ProgressStatus.IS_ERROR}`
-    //         )
-    //       ).not.toBeInTheDocument();
-    //     }, { timeout: 30000 });
+        await waitFor(
+          () => {
+            expect(
+              screen.queryByTestId(
+                `contract-updating-status-is-${ProgressStatus.IS_ERROR}`
+              )
+            ).not.toBeInTheDocument();
+          },
+          { timeout: 30000 }
+        );
 
-    //     await waitFor(() => {
-    //       expect(
-    //         screen.queryByTestId(
-    //           `contract-updating-status-is-${ProgressStatus.IS_ERROR}`
-    //         )
-    //       ).not.toBeInTheDocument();
-    //     }, { timeout: 30000 });
-    //   });
+        await waitFor(
+          () => {
+            expect(
+              screen.queryByTestId(
+                `contract-updating-status-is-${ProgressStatus.IS_ERROR}`
+              )
+            ).not.toBeInTheDocument();
+          },
+          { timeout: 30000 }
+        );
+      });
 
-    //   it("if indexing fails, resets indexing status when bulk update is retried", async () => {
-    //     (updateApplicationStatuses as jest.Mock).mockResolvedValue({
-    //       transactionBlockNumber: 100,
-    //     });
-    //     (waitForSubgraphSyncTo as jest.Mock)
-    //       .mockRejectedValueOnce(new Error(":("))
-    //       .mockReturnValue(
-    //         new Promise(() => {
-    //           /* do nothing.*/
-    //         })
-    //       );
+      it("if indexing fails, resets indexing status when bulk update is retried", async () => {
+        (updateApplicationStatuses as jest.Mock).mockResolvedValue({
+          transactionBlockNumber: 100,
+        });
+        (waitForSubgraphSyncTo as jest.Mock)
+          .mockRejectedValueOnce(new Error(":("))
+          .mockReturnValue(
+            new Promise(() => {
+              /* do nothing.*/
+            })
+          );
 
-    //     renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
-    //     await act(async () => fireEvent.click(screen.getByTestId("update-grant-application")));
+        renderWithProvider(<TestUseBulkUpdateGrantApplicationComponent />);
+        await act(async () =>
+          fireEvent.click(screen.getByTestId("update-grant-application"))
+        );
 
-    //     // retry bulk update operation
-    //     await waitFor(async () => {
-    //       await screen.findByTestId(
-    //         `indexing-status-is-${ProgressStatus.IS_ERROR}`
-    //       );
-    //     });
-    //     await act(async () => fireEvent.click(await screen.findByTestId("update-grant-application")));
+        // retry bulk update operation
+        await waitFor(() => {
+          screen.findByTestId(`indexing-status-is-${ProgressStatus.IS_ERROR}`);
+        });
+        await act(async () =>
+          fireEvent.click(await screen.findByTestId("update-grant-application"))
+        );
 
-    //     await waitFor(() => {
-    //       expect(
-    //         screen.queryByTestId(
-    //           `indexing-status-is-${ProgressStatus.IS_ERROR}`
-    //         )
-    //       ).not.toBeInTheDocument();
-    //     }, { timeout: 30000 });
-    //   });
-    // });
+        await waitFor(
+          () => {
+            expect(
+              screen.queryByTestId(
+                `indexing-status-is-${ProgressStatus.IS_ERROR}`
+              )
+            ).not.toBeInTheDocument();
+          },
+          { timeout: 30000 }
+        );
+      });
+    });
   });
 });
 
@@ -261,3 +309,4 @@ function renderWithProvider(ui: JSX.Element) {
 }
 
 export { };
+
