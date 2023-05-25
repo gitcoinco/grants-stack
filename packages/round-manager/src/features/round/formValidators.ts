4494a783d77807bb39fdf434733486dd3a13487c
hans <hmrtn@pm.me>
2023-05-24 17:24:34 -0700

    refactor: rename to formValidators

--- packages/round-manager/src/features/round/formValidators.ts
+++ packages/round-manager/src/features/round/formValidators.ts
@@ -68,3 +68,85 @@ export const applicationValidationSchema = yup.object().shape({
       "Round end date must be later than the round start date"
     ),
 });
+
+export const roundDetailsValidationSchema = yup.object().shape({
+  roundName: yup  
+  .string()
+  .defined()
+  .required("This field is required.")
+  .min(8, "Round name must be at least 8 characters.")
+  .default(""),
+
+  roundSupport: yup
+  .object({
+    type: yup
+    .string()
+    .required("You must select a support type."),
+    input: yup 
+    .string()
+    .required("This field is required.")
+    .when("type", {
+      is: "Email",
+      then: yup
+      .string()
+      .email()
+      .required("You must provide a valid email address."),
+    })
+    .when("type", {
+      is: (val: string) => val && val != "Email",
+      then: yup.string().url().required("You must provide a valid URL."),
+    })
+  }),
+
+  roundApplicationsStartTime: yup
+  .date()
+  .required("This field is required.")
+  .min(
+    yup.ref("roundApplicationsStartTime"),
+    "You must enter a date and time in the future."
+  ) 
+  .max(
+    yup.ref("roundApplicationsStartTime"),
+    "Applications start date must be before the round start date."
+  )
+  .max(
+    yup.ref("roundApplicationsEndTime"),
+    "Applications start date must be before the round end date."
+  ),
+
+  roundApplicationsEndTime: yup
+  .date()
+  .required("This field is required.")
+  .min(
+    yup.ref("roundApplicationsStartTime"),
+    "Applications end date must be later than applications start date."
+  )
+  .max(
+    yup.ref("roundApplicationsEndTime"),
+    "Applications end date must be before the round end date."
+  ),
+
+  roundVotingStartTime: yup
+  .date()
+  .required("This field is required.")
+  .min(
+    yup.ref("roundVotingStartTime"),
+    "Round start date must be later than the applications start date."
+  )
+  .max(
+    yup.ref("roundVotingEndTime"),
+    "Round start date must be earlier than the round end date."
+  ),
+
+  roundVotingEndTime: yup
+  .date()
+  .required("This field is required.")
+  .min(
+    yup.ref("roundVotingStartTime"),
+    "Round end date must be later than the round start date."
+  ),
+
+  roundVisibility: yup
+  .string()
+  .required("This field is required."),
+});
