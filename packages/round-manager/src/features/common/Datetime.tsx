b4be7ea4caf0d4772c53b92e38732ff59538e022
hans <hmrtn@pm.me>
2023-05-24 17:22:07 -0700

    refactor: generic datetime component

---
+++ packages/round-manager/src/features/common/Datetime.tsx
@@ -0,0 +1,47 @@
+import moment from "moment";
+import { ReactElement } from "react";
+import { Control, Controller, FieldValues } from "react-hook-form";
+
+interface DatetimeProps<T extends FieldValues> {
+    control: Control<T>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    name: any;
+    label: string;
+    date?: moment.Moment;
+    setDate?: (date: moment.Moment) => void;  
+    minDate?: moment.Moment;
+  }
+  
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  export const Datetime = <T extends FieldValues>({ control, name, label, date, setDate, minDate }: DatetimeProps<T>): ReactElement => {
+    const now = moment().format('YYYY-MM-DDTHH:mm');
+    return (
+      <div className="relative w-full border-0 p-0 placeholder-grey-40 focus:ring-0 text-sm">
+        <label
+          htmlFor={name}
+          className="block text-[10px]"
+        >
+          {label}
+        </label>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field }) => (
+            <input
+              data-testid={`${name}-testid`} 
+              type="datetime-local"
+              {...field}
+              min={minDate ? minDate.format('YYYY-MM-DDTHH:mm') : now}
+              className="block w-full border-0 p-0 focus:ring-0"
+              onChange={(e) => {
+                // Convert the local datetime to UTC
+                const date = moment.utc(e.target.value);
+                if (setDate) setDate(date);
+                field.onChange(e.target.value);
+              }}
+            />
+          )}
+        />
+      </div>
+    );
+  };
