59a53f48f5246278af1309ec24cd95443b5d172b
Mohamed Boudra <boudra.moha@gmail.com>
2023-05-26 15:59:22 +0200

    feat: data fetching and table

--- packages/grant-explorer/src/index.tsx
+++ packages/grant-explorer/src/index.tsx
@@ -109,7 +109,7 @@ root.render(
                   />
 
                   <Route
-                    path="/contributors/:address/history"
+                    path="/:chainId/contributors/:address/history"
                     element={<ViewContributionHistory />}
                   />
 
