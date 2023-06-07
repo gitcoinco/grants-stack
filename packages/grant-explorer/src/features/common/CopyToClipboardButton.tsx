6e8ee2c55ca014328171fe42a81588a753cda7e4
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-06-07 08:49:33 +0530

    feat: add share profile btn in donation history

---
+++ packages/grant-explorer/src/features/common/CopyToClipboardButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from "common/src/styles";
+import { LinkIcon } from "@heroicons/react//24/solid";
+import { useState } from "react";
+
+type CopyToClipboardType = {
+  textToCopy: string;
+  styles?: string;
+  iconStyle?: string;
+};
+
+export default function CopyToClipboardButton(props: CopyToClipboardType) {
+  const [active, setActive] = useState(false);
+
+  return (
+    <Button
+      type="button"
+      className={`inline-flex bg-violet-100 text-violet-600 w-30 justify-center ${
+        active && "animate-[violetTransition_20s_ease-in]"
+      } ${props.styles}`}
+      onClick={async () => {
+        setActive(true);
+        setTimeout(() => setActive(false), 1000);
+
+        await navigator.clipboard.writeText(props.textToCopy);
+      }}
+    >
+      <LinkIcon className={props.iconStyle} aria-hidden="true" />
+      {active ? "Link Copied" : "Share Profile"}
+    </Button>
+  );
+}
