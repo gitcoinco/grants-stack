77f57339b6ed169056563c532fccdff0101b03ae
hans <hmrtn@pm.me>
2023-06-08 10:58:38 -0700

    fix: 1901 edit button funk

--- packages/round-manager/src/features/round/ViewRoundSettings.tsx
+++ packages/round-manager/src/features/round/ViewRoundSettings.tsx
@@ -374,7 +374,7 @@ export default function ViewRoundSettings(props: { id?: string }) {
                 type="button"
                 $variant="outline"
                 onClick={onEditClick}
-                disabled={hasRoundEnded || editMode.canEditOnlyRoundEndDate}
+                disabled={hasRoundEnded}
               >
                 <span className="flex flex-row items-center">
                   <FaEdit className="mr-2 mb-1" />
