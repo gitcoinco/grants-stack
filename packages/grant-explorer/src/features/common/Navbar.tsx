b1bd5bb0444e25d6df4829552aa4921365ef4c2e
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-06-15 19:17:44 +0530

    feat: update donation history url

--- packages/grant-explorer/src/features/common/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar(props: NavbarProps) {
             {walletAddress && (
               <div>
                 <a
-                  href={`${currentOrigin}#/profile/${walletAddress}`}
+                  href={`${currentOrigin}#/contributor/${walletAddress}`}
                   className="flex-shrink-0 flex items-center"
                   data-testid={"contributions-link"}
                 >
--- packages/grant-explorer/src/index.tsx
+++ packages/grant-explorer/src/index.tsx
@@ -100,7 +100,7 @@ root.render(
                   />
 
                   <Route
-                    path="/profile/:address"
+                    path="/contributor/:address"
                     element={<ViewContributionHistory />}
                   />
 
