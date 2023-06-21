b96265f4473d9281e363b6620353912047c12620
Daniele Salatti <me@danielesalatti.com>
2023-06-21 15:43:52 -0700

    fix: mock matchMedia for tests

--- packages/builder/src/setupTests.ts
+++ packages/builder/src/setupTests.ts
@@ -7,3 +7,19 @@ import "@testing-library/jest-dom/extend-expect";
 import { TextEncoder } from "util";
 
 global.TextEncoder = TextEncoder;
+
+beforeEach(() => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
