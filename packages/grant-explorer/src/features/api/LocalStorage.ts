a9bacbdd26c8a92939b7e231880d5f351ea7ff64
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-07-13 20:25:48 +0530

    feat: update saveCart & loadCart

--- packages/grant-explorer/src/features/api/LocalStorage.ts
+++ packages/grant-explorer/src/features/api/LocalStorage.ts
@@ -1,25 +1,22 @@
-import { Project } from "./types";
+import { CartProject } from "./types";
 
-export function saveCartToLocalStorage(cart: Project[], roundId: string): void {
-  window.localStorage.setItem(`cart-round-${roundId}`, JSON.stringify(cart));
+export function saveCartToLocalStorage(cart: CartProject[]): void {
+  window.localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-export function loadCartFromLocalStorage(roundId: string): Project[] {
-  const serializedCart = window.localStorage.getItem(`cart-round-${roundId}`);
+export function loadCartFromLocalStorage(): CartProject[] {
+  const serializedCart = window.localStorage.getItem("cart");
   if (!serializedCart) {
     return [];
   }
   return JSON.parse(serializedCart);
 }
 
-export function reloadPageOnLocalStorageEvent(
-  roundId: string,
-  event: StorageEvent
-): void {
+export function reloadPageOnLocalStorageEvent(event: StorageEvent): void {
   // Check if the event is related to localStorage changes
   if (event.storageArea === window.localStorage) {
     // Check if the updated item is 'cart-round-roundId'
-    if (event.key === `cart-round-${roundId}`) {
+    if (event.key === "cart") {
       // Check if it's a different tab
       if (document.visibilityState === "hidden") {
         // Reload the page
