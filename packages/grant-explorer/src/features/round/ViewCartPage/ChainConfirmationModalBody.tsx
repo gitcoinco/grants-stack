c3067ba5194e2552862cbcb8accdf23f08cf877c
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-08-04 09:30:18 +0530

    feat: update chainConfirmation modal ui

--- packages/grant-explorer/src/features/round/ViewCartPage/ChainConfirmationModalBody.tsx
+++ packages/grant-explorer/src/features/round/ViewCartPage/ChainConfirmationModalBody.tsx
@@ -19,12 +19,13 @@ export function ChainConfirmationModalBody({
         Checkout all your carts across different networks or select the cart you
         wish to checkout now.
       </p>
-      <div className="my-8">
-        {Object.keys(projectsByChain).map((chainId) => (
+      <div className="my-4">
+        {Object.keys(projectsByChain).map((chainId, index) => (
           <ChainSummary
             chainId={Number(chainId) as ChainId}
             selectedPayoutToken={payoutTokens[Number(chainId) as ChainId]}
             totalDonation={totalDdonationsPerChain[Number(chainId)]}
+            isLastItem={index === Object.keys(projectsByChain).length - 1}
           />
         ))}
       </div>
@@ -36,24 +37,31 @@ type ChainSummaryProps = {
   totalDonation: BigNumber;
   selectedPayoutToken: PayoutToken;
   chainId: ChainId;
+  isLastItem: boolean;
 };
 
 export function ChainSummary({
   selectedPayoutToken,
   totalDonation,
   chainId,
+  isLastItem,
 }: ChainSummaryProps) {
   return (
-    <div className="flex flex-col justify-between mt-4 text-bold">
+    <div
+      className={`flex flex-col justify-center mt-2 font-semibold ${
+        isLastItem ? "" : "border-b"
+      } px-2 py-4`}
+    >
       <p>
+        <input type="checkbox" className="mr-2 rounded-sm" />
         <img
-          className="inline mr-2"
+          className="inline mr-2 w-5 h-5"
           alt={CHAINS[chainId].name}
           src={CHAINS[chainId].logo}
         />
         Checkout {CHAINS[chainId].name} cart
       </p>
-      <p>
+      <p className="ml-7 mt-1">
         <span data-testid={"totalDonation"} className="mr-2">
           {ethers.utils.formatUnits(totalDonation, selectedPayoutToken.decimal)}
         </span>
