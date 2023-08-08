68882ff8a12b6565f628fc215af533990384783f
eagle <aparokshambhargav@gmail.com>
2023-08-08 18:23:24 +0530

    Add MRC Chain Progress steps (#2154)
    * feat: add mrc chain progress step
    
    * feat: update MRC modal chain step
    
    * feat: update ui

--- packages/grant-explorer/src/features/round/ViewCartPage/MRCProgressModalBody.tsx
+++ packages/grant-explorer/src/features/round/ViewCartPage/MRCProgressModalBody.tsx
@@ -24,8 +24,85 @@ export function MRCProgressModalBody({
 }: MRCProgressModalBodyProps) {
   const { chain } = useNetwork();
   const chainId = (chain?.id ?? chainIdsBeingCheckedOut[0]) as ChainId;
+  const chainStatus = "IN_PROGRESS" as ProgressStatus;
   return (
     <>
+      {chainIdsBeingCheckedOut.length > 1 && (
+        <div className="flex px-2 py-4 justify-between">
+          {chainIdsBeingCheckedOut.map((chainId, idx) => (
+            <>
+              {chainStatus === ProgressStatus.IN_PROGRESS ? (
+                <MRCModalChainStep
+                  key={chainId}
+                  icon={
+                    <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-violet-500 rounded-full">
+                      <span className="h-2.5 w-2.5 bg-violet-500 rounded-full animate-pulse-scale" />
+                    </span>
+                  }
+                  line={
+                    <div
+                      className="flex-grow h-0.5 bg-grey-100"
+                      style={{ minWidth: "300px" }}
+                    ></div>
+                  }
+                  isLastStep={idx === chainIdsBeingCheckedOut.length - 1}
+                />
+              ) : chainStatus === ProgressStatus.IS_SUCCESS ? (
+                <MRCModalChainStep
+                  key={chainId}
+                  icon={
+                    <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-teal-500 rounded-full">
+                      <CheckIcon
+                        className="w-5 h-5 text-white"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  }
+                  line={
+                    <div
+                      className="flex-grow h-0.5 bg-teal-500"
+                      style={{ minWidth: "300px" }}
+                    ></div>
+                  }
+                  isLastStep={idx === chainIdsBeingCheckedOut.length - 1}
+                />
+              ) : chainStatus === ProgressStatus.IS_ERROR ? (
+                <MRCModalChainStep
+                  key={chainId}
+                  icon={
+                    <span className="relative z-10 w-8 h-8 flex items-center justify-center border-2 bg-white border-pink-500 rounded-full">
+                      <XMarkIcon className="w-5 h-5 text-pink-500" />
+                    </span>
+                  }
+                  line={
+                    <div
+                      className="flex-grow h-0.5 bg-grey-100"
+                      style={{ minWidth: "300px" }}
+                    ></div>
+                  }
+                  isLastStep={idx === chainIdsBeingCheckedOut.length - 1}
+                />
+              ) : chainStatus === ProgressStatus.NOT_STARTED ? (
+                <MRCModalChainStep
+                  key={chainId}
+                  icon={
+                    <span className="relative z-10 w-6 h-6 flex items-center justify-center bg-white border-2 rounded-full border-grey-400"></span>
+                  }
+                  line={
+                    <div
+                      className="flex-grow h-0.5 bg-grey-100"
+                      style={{ minWidth: "300px" }}
+                    ></div>
+                  }
+                  isLastStep={idx === chainIdsBeingCheckedOut.length - 1}
+                />
+              ) : (
+                <></>
+              )}
+            </>
+          ))}
+        </div>
+      )}
       <div className="px-2 py-4 font-bold">
         <p>
           <img
@@ -41,14 +118,14 @@ export function MRCProgressModalBody({
           {steps.map((step, stepIdx) => (
             <li
               key={stepIdx}
-              className={`relative ${stepIdx !== steps.length - 1 && "pb-10"}`}
+              className={`relative ${stepIdx !== steps.length - 1 && "pb-4"}`}
             >
               {step.status === ProgressStatus.IS_SUCCESS ? (
                 <MRCModalStep
                   step={step}
                   icon={
                     <span
-                      className="relative z-10 w-8 h-8 flex items-center justify-center bg-teal-500 rounded-full"
+                      className="relative z-10 w-6 h-6 flex items-center justify-center bg-teal-500 rounded-full"
                       data-testid={`${step.name}-complete-icon`}
                     >
                       <CheckIcon
@@ -57,54 +134,33 @@ export function MRCProgressModalBody({
                       />
                     </span>
                   }
-                  line={
-                    <div
-                      className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-teal-500"
-                      aria-hidden="true"
-                    />
-                  }
                   nameColor={"text-grey-500"}
                   descriptionColor={"text-grey-500"}
-                  isLastStep={stepIdx === steps.length - 1}
                 />
               ) : step.status === ProgressStatus.IN_PROGRESS ? (
                 <MRCModalStep
                   step={step}
                   icon={
-                    <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-violet-500 rounded-full">
+                    <span className="relative z-10 w-6 h-6 flex items-center justify-center bg-white border-2 border-violet-500 rounded-full">
                       <span
-                        className="h-2.5 w-2.5 bg-violet-500 rounded-full animate-pulse-scale"
+                        className="h-2.5 w-2.5 bg-violet-500 rounded-full"
                         data-testid={`${step.name}-current-icon`}
                       />
                     </span>
                   }
-                  line={
-                    <div
-                      className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-grey-200"
-                      aria-hidden="true"
-                    />
-                  }
                   nameColor="text-violet-500"
-                  isLastStep={stepIdx === steps.length - 1}
                 />
               ) : step.status === ProgressStatus.IS_ERROR ? (
                 <MRCModalStep
                   step={step}
                   icon={
-                    <span className="relative z-10 w-8 h-8 flex items-center justify-center border-2 bg-white border-pink-500 rounded-full">
+                    <span className="relative z-10 w-6 h-6 flex items-center justify-center border-2 bg-white border-pink-500 rounded-full">
                       <XMarkIcon
                         className="w-5 h-5 text-pink-500"
                         data-testid={`${step.name}-error-icon`}
                       />
                     </span>
                   }
-                  line={
-                    <div
-                      className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-grey-300"
-                      aria-hidden="true"
-                    />
-                  }
-                  isLastStep={stepIdx === steps.length - 1}
                   nameColor="text-grey-500"
                 />
               ) : step.status === ProgressStatus.NOT_STARTED ? (
@@ -112,17 +168,10 @@ export function MRCProgressModalBody({
                   step={step}
                   icon={
                     <span
-                      className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 rounded-full border-grey-400"
+                      className="relative z-10 w-6 h-6 flex items-center justify-center bg-white border-2 rounded-full border-grey-400"
                       data-testid={`${step.name}-upcoming-icon`}
                     ></span>
                   }
-                  line={
-                    <div
-                      className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-grey-300"
-                      aria-hidden="true"
-                    />
-                  }
-                  isLastStep={stepIdx === steps.length - 1}
                   nameColor="text-grey-400"
                 />
               ) : (
@@ -139,16 +188,13 @@ export function MRCProgressModalBody({
 function MRCModalStep(props: {
   step: Step;
   icon: JSX.Element;
-  line: JSX.Element;
   nameColor: string;
   descriptionColor?: string;
   isAriaHidden?: boolean;
   isAriaCurrent?: boolean;
-  isLastStep?: boolean;
 }) {
   return (
     <>
-      {!props.isLastStep ? props.line : null}
       <div
         className="relative flex items-start group"
         aria-current={!!props.isAriaCurrent}
@@ -173,5 +219,26 @@ function MRCModalStep(props: {
         </span>
       </div>
     </>
+  );
+}
+
+function MRCModalChainStep(props: {
+  icon: JSX.Element;
+  line: JSX.Element;
+  isAriaHidden?: boolean;
+  isAriaCurrent?: boolean;
+  isLastStep?: boolean;
+}) {
+  console.log(props);
+  return (
+    <div className="flex items-center">
+      <span
+        className="h-9 flex items-center"
+        aria-hidden={!!props.isAriaHidden}
+      >
+        {props.icon}
+      </span>
+      {!props.isLastStep && props.line}
+    </div>
   );
 }
