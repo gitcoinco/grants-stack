a29cb667b6b1903f0cf7f678ca1ba17cebcd2932
nicosampler <nf.dominguez.87@gmail.com>
2023-08-10 09:09:49 -0300

    uncomment tests

--- packages/round-manager/src/features/round/__tests__/ApplicationDirectPayout.test.tsx
+++ packages/round-manager/src/features/round/__tests__/ApplicationDirectPayout.test.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ApplicationDirectPayout from "../ApplicationDirectPayout";
 import { makeGrantApplicationData, makeRoundData } from "../../../test-utils";
@@ -80,208 +86,208 @@ describe("<ApplicationDirectPayout />", () => {
     (useNetwork as jest.Mock).mockReturnValue(mockNetwork);
   });
 
-  // it('should trigger if "Payout token" answer is not present', () => {
-  //   const mockProps = {
-  //     round: makeRoundData({
-  //       operatorWallets: [mockAddress],
-  //     }),
-  //     application: makeGrantApplicationData({
-  //       applicationIndex: 1,
-  //       payoutStrategy: {
-  //         id: "1",
-  //         strategyName: ROUND_PAYOUT_DIRECT,
-  //         payouts: [],
-  //       },
-  //     }),
-  //     answerBlocks: [
-  //       {
-  //         questionId: 0,
-  //         question: "Email Address",
-  //         answer: "johndoe@example.com",
-  //       },
-  //     ],
-  //   };
+  it('should trigger if "Payout token" answer is not present', () => {
+    const mockProps = {
+      round: makeRoundData({
+        operatorWallets: [mockAddress],
+      }),
+      application: makeGrantApplicationData({
+        applicationIndex: 1,
+        payoutStrategy: {
+          id: "1",
+          strategyName: ROUND_PAYOUT_DIRECT,
+          payouts: [],
+        },
+      }),
+      answerBlocks: [
+        {
+          questionId: 0,
+          question: "Email Address",
+          answer: "johndoe@example.com",
+        },
+      ],
+    };
 
-  //   try {
-  //     render(<ApplicationDirectPayout {...mockProps} />);
-  //   } catch (error: any) {
-  //     expect(error.message).toBe('"Payout token" not found in answers!');
-  //   }
-  // });
+    try {
+      render(<ApplicationDirectPayout {...mockProps} />);
+    } catch (error: any) {
+      expect(error.message).toBe('"Payout token" not found in answers!');
+    }
+  });
 
-  // it("should trigger if the token of the answer is not known", () => {
-  //   const mockProps = {
-  //     round: makeRoundData({
-  //       operatorWallets: [mockAddress],
-  //     }),
-  //     application: makeGrantApplicationData({
-  //       applicationIndex: 1,
-  //       payoutStrategy: {
-  //         id: "1",
-  //         strategyName: ROUND_PAYOUT_DIRECT,
-  //         payouts: [],
-  //       },
-  //     }),
-  //     answerBlocks: [
-  //       {
-  //         questionId: 0,
-  //         question: "Email Address",
-  //         answer: "johndoe@example.com",
-  //       },
-  //       {
-  //         questionId: 1,
-  //         question: "Payout token",
-  //         answer: "UNKNOWN-TOKEN",
-  //       },
-  //     ],
-  //   };
+  it("should trigger if the token of the answer is not known", () => {
+    const mockProps = {
+      round: makeRoundData({
+        operatorWallets: [mockAddress],
+      }),
+      application: makeGrantApplicationData({
+        applicationIndex: 1,
+        payoutStrategy: {
+          id: "1",
+          strategyName: ROUND_PAYOUT_DIRECT,
+          payouts: [],
+        },
+      }),
+      answerBlocks: [
+        {
+          questionId: 0,
+          question: "Email Address",
+          answer: "johndoe@example.com",
+        },
+        {
+          questionId: 1,
+          question: "Payout token",
+          answer: "UNKNOWN-TOKEN",
+        },
+      ],
+    };
 
-  //   try {
-  //     render(<ApplicationDirectPayout {...mockProps} />);
-  //   } catch (error: any) {
-  //     expect(error.message).toContain("Token info not found for chain id");
-  //   }
-  // });
+    try {
+      render(<ApplicationDirectPayout {...mockProps} />);
+    } catch (error: any) {
+      expect(error.message).toContain("Token info not found for chain id");
+    }
+  });
 
-  // it('should trigger if the "Payout wallet address" answer is not present', () => {
-  //   const mockProps = {
-  //     round: makeRoundData({
-  //       operatorWallets: [mockAddress],
-  //     }),
-  //     application: makeGrantApplicationData({
-  //       applicationIndex: 1,
-  //       payoutStrategy: {
-  //         id: "1",
-  //         strategyName: ROUND_PAYOUT_DIRECT,
-  //         payouts: [],
-  //       },
-  //     }),
-  //     answerBlocks: [
-  //       {
-  //         questionId: 0,
-  //         question: "Email Address",
-  //         answer: "johndoe@example.com",
-  //       },
-  //       {
-  //         questionId: 1,
-  //         question: "Payout token",
-  //         answer: "DAI",
-  //       },
-  //     ],
-  //   };
+  it('should trigger if the "Payout wallet address" answer is not present', () => {
+    const mockProps = {
+      round: makeRoundData({
+        operatorWallets: [mockAddress],
+      }),
+      application: makeGrantApplicationData({
+        applicationIndex: 1,
+        payoutStrategy: {
+          id: "1",
+          strategyName: ROUND_PAYOUT_DIRECT,
+          payouts: [],
+        },
+      }),
+      answerBlocks: [
+        {
+          questionId: 0,
+          question: "Email Address",
+          answer: "johndoe@example.com",
+        },
+        {
+          questionId: 1,
+          question: "Payout token",
+          answer: "DAI",
+        },
+      ],
+    };
 
-  //   try {
-  //     render(<ApplicationDirectPayout {...mockProps} />);
-  //   } catch (error: any) {
-  //     expect(error.message).toBe(
-  //       '"Payout wallet address" not found in answers!'
-  //     );
-  //   }
-  // });
+    try {
+      render(<ApplicationDirectPayout {...mockProps} />);
+    } catch (error: any) {
+      expect(error.message).toBe(
+        '"Payout wallet address" not found in answers!'
+      );
+    }
+  });
 
-  // it('should display "Payouts have not been made yet." when there are no payouts', () => {
-  //   const mockProps = {
-  //     round: makeRoundData({
-  //       operatorWallets: [mockAddress],
-  //     }),
-  //     application: makeGrantApplicationData({
-  //       payoutStrategy: {
-  //         id: "1",
-  //         strategyName: ROUND_PAYOUT_DIRECT,
-  //         payouts: [], // Empty payouts array
-  //       },
-  //     }),
-  //     answerBlocks: correctAnswerBlocks,
-  //   };
+  it('should display "Payouts have not been made yet." when there are no payouts', () => {
+    const mockProps = {
+      round: makeRoundData({
+        operatorWallets: [mockAddress],
+      }),
+      application: makeGrantApplicationData({
+        payoutStrategy: {
+          id: "1",
+          strategyName: ROUND_PAYOUT_DIRECT,
+          payouts: [], // Empty payouts array
+        },
+      }),
+      answerBlocks: correctAnswerBlocks,
+    };
 
-  //   render(<ApplicationDirectPayout {...mockProps} />);
+    render(<ApplicationDirectPayout {...mockProps} />);
 
-  //   expect(
-  //     screen.queryByText("Payouts have not been made yet.")
-  //   ).toBeInTheDocument();
-  // });
+    expect(
+      screen.queryByText("Payouts have not been made yet.")
+    ).toBeInTheDocument();
+  });
 
-  // it("should display payout information when there are payouts", () => {
-  //   const mockProps = {
-  //     round: makeRoundData({
-  //       operatorWallets: [mockAddress],
-  //     }),
-  //     application: makeGrantApplicationData({
-  //       applicationIndex: 1,
-  //       payoutStrategy: {
-  //         id: "1",
-  //         strategyName: ROUND_PAYOUT_DIRECT,
-  //         payouts: [
-  //           {
-  //             amount: parseUnits("1", 18).toString(),
-  //             applicationIndex: 1,
-  //             createdAt: (
-  //               moment().subtract(3, "day").valueOf() / 1000
-  //             ).toString(),
-  //             txnHash: "0x00001",
-  //           },
-  //           {
-  //             amount: parseUnits("2", 18).toString(),
-  //             applicationIndex: 1,
-  //             createdAt: (
-  //               moment().subtract(2, "day").valueOf() / 1000
-  //             ).toString(),
-  //             txnHash: "0x00002",
-  //           },
-  //           {
-  //             amount: parseUnits("20", 18).toString(),
-  //             applicationIndex: 2, // NOTE: This payout is for a different application
-  //             createdAt: (
-  //               moment().subtract(1, "day").valueOf() / 1000
-  //             ).toString(),
-  //             txnHash: "0x00003",
-  //           },
-  //         ],
-  //       },
-  //     }),
-  //     answerBlocks: correctAnswerBlocks,
-  //   };
+  it("should display payout information when there are payouts", () => {
+    const mockProps = {
+      round: makeRoundData({
+        operatorWallets: [mockAddress],
+      }),
+      application: makeGrantApplicationData({
+        applicationIndex: 1,
+        payoutStrategy: {
+          id: "1",
+          strategyName: ROUND_PAYOUT_DIRECT,
+          payouts: [
+            {
+              amount: parseUnits("1", 18).toString(),
+              applicationIndex: 1,
+              createdAt: (
+                moment().subtract(3, "day").valueOf() / 1000
+              ).toString(),
+              txnHash: "0x00001",
+            },
+            {
+              amount: parseUnits("2", 18).toString(),
+              applicationIndex: 1,
+              createdAt: (
+                moment().subtract(2, "day").valueOf() / 1000
+              ).toString(),
+              txnHash: "0x00002",
+            },
+            {
+              amount: parseUnits("20", 18).toString(),
+              applicationIndex: 2, // NOTE: This payout is for a different application
+              createdAt: (
+                moment().subtract(1, "day").valueOf() / 1000
+              ).toString(),
+              txnHash: "0x00003",
+            },
+          ],
+        },
+      }),
+      answerBlocks: correctAnswerBlocks,
+    };
 
-  //   render(<ApplicationDirectPayout {...mockProps} />);
+    render(<ApplicationDirectPayout {...mockProps} />);
 
-  //   const tbody = screen.getByTestId("direct-payout-payments-table");
-  //   const filas = within(tbody).getAllByRole("row");
-  //   const totalPaidOut = screen.getByTestId("direct-payout-payments-total");
+    const tbody = screen.getByTestId("direct-payout-payments-table");
+    const filas = within(tbody).getAllByRole("row");
+    const totalPaidOut = screen.getByTestId("direct-payout-payments-total");
 
-  //   expect(filas.length).toBe(2);
-  //   expect(filas[0].textContent).toContain("0x00001");
-  //   expect(filas[1].textContent).toContain("0x00002");
-  //   expect(totalPaidOut.textContent).toContain("3.0 DAI");
-  // });
+    expect(filas.length).toBe(2);
+    expect(filas[0].textContent).toContain("0x00001");
+    expect(filas[1].textContent).toContain("0x00002");
+    expect(totalPaidOut.textContent).toContain("3.0 DAI");
+  });
 
-  // it("should not trigger payout if not amount or vault address is entered", async () => {
-  //   const mockProps = {
-  //     round: makeRoundData({
-  //       operatorWallets: [mockAddress],
-  //     }),
-  //     application: makeGrantApplicationData({
-  //       applicationIndex: 1,
-  //       payoutStrategy: {
-  //         id: "1",
-  //         strategyName: ROUND_PAYOUT_DIRECT,
-  //         payouts: [],
-  //       },
-  //     }),
-  //     answerBlocks: correctAnswerBlocks,
-  //   };
+  it("should not trigger payout if not amount or vault address is entered", async () => {
+    const mockProps = {
+      round: makeRoundData({
+        operatorWallets: [mockAddress],
+      }),
+      application: makeGrantApplicationData({
+        applicationIndex: 1,
+        payoutStrategy: {
+          id: "1",
+          strategyName: ROUND_PAYOUT_DIRECT,
+          payouts: [],
+        },
+      }),
+      answerBlocks: correctAnswerBlocks,
+    };
 
-  //   render(<ApplicationDirectPayout {...mockProps} />);
+    render(<ApplicationDirectPayout {...mockProps} />);
 
-  //   const button = screen.getByTestId("trigger-payment");
-  //   fireEvent.click(button);
+    const button = screen.getByTestId("trigger-payment");
+    fireEvent.click(button);
 
-  //   await waitFor(() => {
-  //     expect(
-  //       screen.getByText("Payment amount must be a number")
-  //     ).toBeInTheDocument();
-  //     expect(screen.getByText("Vault address is required")).toBeInTheDocument();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(
+        screen.getByText("Payment amount must be a number")
+      ).toBeInTheDocument();
+      expect(screen.getByText("Vault address is required")).toBeInTheDocument();
+    });
+  });
 
   it("should inform that allowance is not enough for the payout strategy when connected wallet is different than vault address", async () => {
     const mockProps = {
