27cdfdb0e8e3949e9c95f422172053785ff58b08
Atris <vacekj@outlook.com>
2023-08-14 21:29:52 +0200

    fix: thank you page

--- packages/grant-explorer/src/features/round/ThankYou.tsx
+++ packages/grant-explorer/src/features/round/ThankYou.tsx
@@ -78,6 +78,7 @@ export default function ThankYou() {
         ProgressStatus.NOT_STARTED
       );
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   /** If there are projects left to check out, show a Back to cart button */
