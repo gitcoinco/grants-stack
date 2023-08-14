f7de0dc6525530fd99f0d58414019dc3303c2bc0
Atris <vacekj@outlook.com>
2023-08-14 21:03:28 +0200

    fix: contrib link on non-home pages

--- packages/grant-explorer/src/features/common/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar.tsx
@@ -69,7 +69,7 @@ export default function Navbar(props: NavbarProps) {
             {walletAddress && (
               <div>
                 <Link
-                  to={`contributors/${walletAddress}`}
+                  to={`/contributors/${walletAddress}`}
                   className="flex-shrink-0 flex items-center"
                   data-testid={"contributions-link"}
                 >
