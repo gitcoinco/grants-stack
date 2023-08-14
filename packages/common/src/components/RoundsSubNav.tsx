ceee02ba21615e04b7a5b3b979a1e3ea67aad9f6
Andrea Franz <andrea@gravityblast.com>
2023-08-15 00:14:20 +0200

    group gg18 rounds

--- packages/common/src/components/RoundsSubNav.tsx
+++ packages/common/src/components/RoundsSubNav.tsx
@@ -10,56 +10,71 @@ type Round = {
   id: `0x${string}`;
 };
 
-const ROUNDS: Round[] = [
+type RoundsGroup = {
+  name: string;
+  rounds: Round[];
+};
+
+const ROUNDS_GROUPS: RoundGroups[] = [
   {
-    name: "Climate solutions",
-    chainId: 10,
-    id: "0xb6be0ecafdb66dd848b0480db40056ff94a9465d",
+    name: "Core Rounds",
+    rounds: [
+      {
+        name: "Climate solutions",
+        chainId: 10,
+        id: "0xb6be0ecafdb66dd848b0480db40056ff94a9465d",
+      },
+      {
+        name: "Ethereum Infrastructure",
+        chainId: 424,
+        id: "0x222ea76664ed77d18d4416d2b2e77937b76f0a35",
+      },
+      {
+        name: "Web3 Community and Education",
+        chainId: 10,
+        id: "0x2871742b184633f8dc8546c6301cbc209945033e",
+      },
+      {
+        name: "Web3 Open Source Software",
+        chainId: 10,
+        id: "0x8de918f0163b2021839a8d84954dd7e8e151326d",
+      },
+    ],
   },
   {
-    name: "Ethereum Infrastructure",
-    chainId: 424,
-    id: "0x222ea76664ed77d18d4416d2b2e77937b76f0a35",
-  },
-  {
-    name: "Web3 Community and Education",
-    chainId: 10,
-    id: "0x2871742b184633f8dc8546c6301cbc209945033e",
-  },
-  {
-    name: "Web3 Open Source Software",
-    chainId: 10,
-    id: "0x8de918f0163b2021839a8d84954dd7e8e151326d",
-  },
-  {
-    name: "Zuzalu",
-    chainId: 10,
-    id: "0x5b95acf46c73fd116f0fedadcbedf453530e35d0",
-  },
-  {
-    name: "Token Engineering (TEC)",
-    chainId: 10,
-    id: "0xc5fdf5cff79e92fac1d6efa725c319248d279200",
-  },
-  {
-    name: "Latin America",
-    chainId: 10,
-    id: "0xf591e42dfdfe8e62c2085ccaadfe05f84d89d0c6",
-  },
-  {
-    name: "Web3 Social",
-    chainId: 10,
-    id: "0x9331fde4db7b9d9d1498c09d30149929f24cf9d5",
-  },
-  {
-    name: "Global Chinese Community",
-    chainId: 10,
-    id: "0x30c381033aa2830ceb0aa372c2e4d28f004b3db9",
-  },
-  {
-    name: "ReFiDAO",
-    chainId: 10,
-    id: "0x10be322de44389ded49c0b2b73d8c3a1e3b6d871",
+    name: "Featured Rounds",
+    rounds: [
+      {
+        name: "Token Engineering (TEC)",
+        chainId: 10,
+        id: "0xc5fdf5cff79e92fac1d6efa725c319248d279200",
+      },
+      {
+        name: "Zuzalu",
+        chainId: 10,
+        id: "0x5b95acf46c73fd116f0fedadcbedf453530e35d0",
+      },
+      {
+        name: "Latin America",
+        chainId: 10,
+        id: "0xf591e42dfdfe8e62c2085ccaadfe05f84d89d0c6",
+      },
+      {
+        name: "Web3 Social",
+        chainId: 10,
+        id: "0x9331fde4db7b9d9d1498c09d30149929f24cf9d5",
+      },
+      {
+        name: "Global Chinese Community",
+        chainId: 10,
+        id: "0x30c381033aa2830ceb0aa372c2e4d28f004b3db9",
+      },
+      {
+        name: "ReFiDAO",
+        chainId: 10,
+        id: "0x10be322de44389ded49c0b2b73d8c3a1e3b6d871",
+      },
+    ],
   },
 ];
 
@@ -96,23 +111,28 @@ export function RoundsSubNav() {
                 </span>
               </span>
               <div
-                className={`absolute right-0 top-8 bg-grey-150 py-4 px-3 rounded-lg text-right whitespace-nowrap ${
+                className={`absolute right-0 top-8 bg-grey-150 py-4 px-6 rounded-lg text-right whitespace-nowrap ${
                   open ? "block" : "hidden"
                 }`}
               >
                 <ul>
-                  <li className="border-b border-solid border-grey-200 py-1 mb-2">
-                    Core Rounds
-                  </li>
-                  {ROUNDS.map((round: Round) => (
-                    <li>
-                      <a
-                        href={`/#/round/${round.chainId}/${round.id}`}
-                        className="block py-2"
-                      >
-                        {round.name}
-                      </a>
-                    </li>
+                  {ROUNDS_GROUPS.map((group: RoundsGroup) => (
+                    <>
+                      <li className="border-b border-solid border-grey-200 py-1 mb-2 font-bold">
+                        {group.name}
+                      </li>
+
+                      {group.rounds.map((round: Round) => (
+                        <li>
+                          <a
+                            href={`/#/round/${round.chainId}/${round.id}`}
+                            className="block py-2 "
+                          >
+                            {round.name}
+                          </a>
+                        </li>
+                      ))}
+                    </>
                   ))}
                 </ul>
               </div>
