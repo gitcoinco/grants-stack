d5111625f06f9695c9c1d83db0da4deda2344087
Andrea Franz <andrea@gravityblast.com>
2023-08-15 14:54:01 +0200

    add PGN logo to navbar (#2099)
    * add PGN bridge logo link to all 3 dapps
    
    * update PGN text icon
    
    * add rounds subnav for GG18
    
    * remove PGN span wrapper
    
    * splat props in GG18 icons
    
    * add ../common to tailwind content
    
    * typo
    
    * group gg18 rounds
    
    * remove unused span wrappers
    
    * fix PGN logo props
    
    * add onClick prop to sub nav
    
    * remove unused key
    
    * remove unused useCart

---
+++ packages/common/src/icons/PGN.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export function PGN(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="11.5856"
+        fill="white"
+        stroke="black"
+        strokeWidth="0.828761"
+      />
+      <rect
+        x="6.64045"
+        y="6.66646"
+        width="6.8351"
+        height="6.8351"
+        fill="black"
+        stroke="black"
+        strokeWidth="0.828761"
+      />
+      <rect
+        x="10.5223"
+        y="10.4984"
+        width="6.8351"
+        height="6.8351"
+        fill="black"
+        stroke="black"
+        strokeWidth="0.828761"
+      />
+      <rect
+        x="10.5123"
+        y="10.4984"
+        width="2.96491"
+        height="2.96491"
+        fill="white"
+        stroke="black"
+        strokeWidth="0.828761"
+      />
+    </svg>
+  );
+}
