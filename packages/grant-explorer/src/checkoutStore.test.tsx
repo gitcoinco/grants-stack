da4c54e900f30424c93216ca70f7c2018a6fd229
Atris <vacekj@outlook.com>
2023-08-22 22:32:30 +0200

    tests: add checkoutStore tests

---
+++ packages/grant-explorer/src/checkoutStore.test.tsx
@@ -0,0 +1,49 @@
+import { useCheckoutStore } from "./checkoutStore"; // Adjust this import to your file structure
+import { ProgressStatus } from "./features/api/types";
+import { ChainId } from "common";
+
+const store = useCheckoutStore;
+
+it("Checkout Store - permitStatus manipulation", async () => {
+  const store = useCheckoutStore;
+  const chain: ChainId = ChainId.MAINNET; // Replace with actual ChainId
+
+  // Initially, the status should be NOT_STARTED for all chains
+  assert.equal(
+    store.getState().permitStatus[chain],
+    ProgressStatus.NOT_STARTED
+  );
+
+  // Update the status
+  store.getState().setPermitStatusForChain(chain, ProgressStatus.IN_PROGRESS);
+  assert.equal(
+    store.getState().permitStatus[chain],
+    ProgressStatus.IN_PROGRESS
+  );
+});
+
+it("Checkout Store - voteStatus manipulation", async () => {
+  const chain: ChainId = ChainId.MAINNET; // Replace with actual ChainId
+
+  assert.equal(store.getState().voteStatus[chain], ProgressStatus.NOT_STARTED);
+
+  store.getState().setVoteStatusForChain(chain, ProgressStatus.IN_PROGRESS);
+  assert.equal(store.getState().voteStatus[chain], ProgressStatus.IN_PROGRESS);
+});
+
+it("Checkout Store - chainSwitchStatus manipulation", async () => {
+  const chain: ChainId = ChainId.MAINNET; // Replace with actual ChainId
+
+  assert.equal(
+    store.getState().chainSwitchStatus[chain],
+    ProgressStatus.NOT_STARTED
+  );
+
+  store
+    .getState()
+    .setChainSwitchStatusForChain(chain, ProgressStatus.IN_PROGRESS);
+  assert.equal(
+    store.getState().chainSwitchStatus[chain],
+    ProgressStatus.IN_PROGRESS
+  );
+});
