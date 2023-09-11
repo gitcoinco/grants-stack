e1286928dc7fc9df89c178f5756bd13ddc1cd06a
Atris <vacekj@outlook.com>
2023-09-11 17:04:25 +0200

    feat: tests

--- packages/verify-env/src/index.test.ts
+++ packages/verify-env/src/index.test.ts
@@ -1,4 +1,37 @@
-import { it, expect } from "vitest";
-it("runs tests", () => {
-  expect(true).toEqual(true);
+import { it, expect, vi } from "vitest";
+import { extractEnvVars, verify, validateVariables } from "./verify.ts";
+import fs from "fs";
+
+it("extracts env vars from a string", () => {
+  const source = fs.readFileSync(__dirname + "/testSource.txt").toString();
+  const envVars = extractEnvVars(source);
+  expect(envVars).toStrictEqual([
+    "REACT_APP_ENV",
+    "REACT_APP_INFURA_ID",
+    "REACT_APP_WALLETCONNECT_PROJECT_ID",
+  ]);
+});
+
+it("verifies vars from files", () => {
+  const consoleMock = vi
+    .spyOn(console, "error")
+    .mockImplementation(() => undefined);
+  const processMock = vi.spyOn(process, "exit").mockImplementation(() => {
+    return undefined as never;
+  });
+
+  verify(__dirname + "/test");
+  expect(processMock).toHaveBeenCalledOnce();
+  expect(processMock).toHaveBeenCalledWith(1);
+  expect(consoleMock).toHaveBeenCalledWith(
+    "Error: Some environment variables are not set or are empty:"
+  );
+  expect(consoleMock).toHaveBeenCalledWith(
+    `
+REACT_APP_ENV
+REACT_APP_WALLETCONNECT_PROJECT_ID
+REACT_APP_INFURA_ID
+REACT_APP_ALCHEMY_ID
+`
+  );
 });
--- packages/verify-env/src/verify.ts
+++ packages/verify-env/src/verify.ts
@@ -2,53 +2,19 @@ import fs from "fs";
 import path from "path";
 
 export function verify(srcDir: string = "src/") {
-  /** Get all source files */
-  const getFilesWithExtensions = (
-    dir: string,
-    extensions: string[],
-    fileList: string[] = []
-  ) => {
-    const files = fs.readdirSync(dir);
-
-    files.forEach((file) => {
-      const filePath = path.join(dir, file);
-      const stat = fs.statSync(filePath);
-
-      if (stat.isDirectory()) {
-        getFilesWithExtensions(filePath, extensions, fileList); // Recurse into subdirectories
-      } else {
-        const ext = path.extname(file);
-        if (extensions.includes(ext)) {
-          fileList.push(filePath); // Add to fileList if it matches the extensions
-        }
-      }
-    });
-
-    return fileList;
-  };
-
+  // Get all source files
   // Extensions to look for
   const extensions = [".ts", ".tsx", ".js", ".jsx"];
-
   const files = getFilesWithExtensions(srcDir, extensions);
 
   // Initialize an array to store the extracted environment variables
   const envVars: string[] = [];
 
-  // Function to extract environment variables from file content
-  const extractEnvVars = (content: string) => {
-    const regex = /process\.env\.([A-Z0-9_]+)/gi;
-    let match;
-    while ((match = regex.exec(content)) !== null) {
-      envVars.push(match[1]);
-    }
-  };
-
   // Read each file and extract environment variables
   files.forEach((filePath) => {
     try {
       const fileContent = fs.readFileSync(filePath, "utf-8");
-      extractEnvVars(fileContent);
+      envVars.push(...extractEnvVars(fileContent));
     } catch (error) {
       console.error(`Error reading file ${filePath}: ${error}`);
     }
@@ -56,11 +22,56 @@ export function verify(srcDir: string = "src/") {
 
   // Remove duplicates
   const uniqueEnvVars = [...new Set(envVars)];
-  verifyEnvVariables(uniqueEnvVars);
+
+  // Validate
+  const badVars = validateVariables(uniqueEnvVars);
+  if (badVars.length > 0) {
+    console.error(
+      "Error: Some environment variables are not set or are empty:"
+    );
+    if (badVars.length > 0) {
+      console.error(`\n${badVars.join("\n")}\n`);
+    }
+    process.exit(1);
+  }
 }
 
-function verifyEnvVariables(variables: string[]) {
-  const emptyVariables = variables.filter((variable) => {
+const getFilesWithExtensions = (
+  dir: string,
+  extensions: string[],
+  fileList: string[] = []
+) => {
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      getFilesWithExtensions(filePath, extensions, fileList); // Recurse into subdirectories
+    } else {
+      const ext = path.extname(file);
+      if (extensions.includes(ext)) {
+        fileList.push(filePath); // Add to fileList if it matches the extensions
+      }
+    }
+  });
+
+  return fileList;
+};
+
+export const extractEnvVars = (content: string): string[] => {
+  const envVars = [];
+  const regex = /process\.env\.([A-Z0-9_]+)/gi;
+  let match;
+  while ((match = regex.exec(content)) !== null) {
+    envVars.push(match[1]);
+  }
+  return envVars;
+};
+
+export function validateVariables(variables: string[]) {
+  return variables.filter((variable) => {
     const value = process.env[variable];
     return (
       value === undefined ||
@@ -69,14 +80,4 @@ function verifyEnvVariables(variables: string[]) {
       value === "null"
     );
   });
-
-  if (emptyVariables.length > 0) {
-    console.error(
-      "Error: Some environment variables are not set or are empty:"
-    );
-    if (emptyVariables.length > 0) {
-      console.error(`\n${emptyVariables.join("\n")}\n`);
-    }
-    process.exit(1);
-  }
 }
