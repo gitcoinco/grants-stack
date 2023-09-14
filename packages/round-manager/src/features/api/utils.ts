6145544e7734814b94827b2716e4299ed93b7054
Atris <vacekj@outlook.com>
2023-09-14 15:49:39 +0200

    fix: revert removing https in front of pinata env var

--- packages/round-manager/src/features/api/utils.ts
+++ packages/round-manager/src/features/api/utils.ts
@@ -444,15 +444,15 @@ export const getPayoutTokenOptions = (chainId: ChainId): PayoutToken[] => {
  * @param cid - the unique content identifier that points to the data
  */
 export const fetchFromIPFS = (cid: string) => {
-  return fetch(`${process.env.REACT_APP_PINATA_GATEWAY}/ipfs/${cid}`).then(
-    (resp) => {
-      if (resp.ok) {
-        return resp.json();
-      }
-
-      return Promise.reject(resp);
+  return fetch(
+    `https://${process.env.REACT_APP_PINATA_GATEWAY}/ipfs/${cid}`
+  ).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
     }
-  );
+
+    return Promise.reject(resp);
+  });
 };
 
 /**
