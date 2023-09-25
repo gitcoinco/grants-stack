18cb8a26e86a494600ca9f57369b5ad4280715cb
Atris <vacekj@outlook.com>
2023-09-25 12:00:40 +0200

    feat: make cart handle new chains gracefully (#2372)

--- packages/grant-explorer/src/features/api/utils.ts
+++ packages/grant-explorer/src/features/api/utils.ts
@@ -312,7 +312,7 @@ const ARBITRUM_TOKENS: VotingToken[] = [
 const ARBITRUM_GOERLI_TOKENS: VotingToken[] = [
   {
     name: "ETH",
-    chainId: ChainId.PGN,
+    chainId: ChainId.ARBITRUM_GOERLI,
     address: zeroAddress,
     decimal: 18,
     logo: TokenNamesAndLogos["ETH"],
--- packages/grant-explorer/src/store.ts
+++ packages/grant-explorer/src/store.ts
@@ -3,6 +3,7 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { CartProject, VotingToken } from "./features/api/types";
 import { votingTokensMap } from "./features/api/utils";
+import { zeroAddress } from "viem";
 
 interface CartState {
   projects: CartProject[];
@@ -11,19 +12,38 @@ interface CartState {
   remove: (grantApplicationId: string) => void;
   updateDonationsForChain: (chainId: ChainId, amount: string) => void;
   updateDonationAmount: (grantApplicationId: string, amount: string) => void;
-  chainToPayoutToken: Record<ChainId, VotingToken>;
-  setPayoutTokenForChain: (chainId: ChainId, payoutToken: VotingToken) => void;
+  chainToVotingToken: Record<ChainId, VotingToken>;
+  getVotingTokenForChain: (chainId: ChainId) => VotingToken;
+  setVotingTokenForChain: (chainId: ChainId, votingToken: VotingToken) => void;
 }
 
-const ethOnlyPayoutTokens = Object.fromEntries(
-  Object.entries(votingTokensMap).map(
-    ([key, value]) =>
-      [
-        Number(key) as ChainId,
-        value.find((token) => token.defaultForVoting && token.canVote) ??
-          value[0],
-      ] as [ChainId, VotingToken]
-  )
+/**
+ * Consumes an array of voting tokens and returns the default one.
+ * If there's no default one, return the first one.
+ * If the array is empty,
+ * return the native token for the chain (Although this should never happen)
+ * */
+function getDefaultVotingToken(votingTokens: VotingToken[], chainId: ChainId) {
+  return (
+    votingTokens.find((token) => token.defaultForVoting && token.canVote) ??
+    votingTokens[0] ?? {
+      chainId,
+      canVote: true,
+      defaultForVoting: true,
+      decimal: 18,
+      name: "Native Token",
+      address: zeroAddress,
+    }
+  );
+}
+
+const defaultVotingTokens = Object.fromEntries(
+  Object.entries(votingTokensMap).map(([key, value]) => {
+    return [
+      Number(key) as ChainId,
+      getDefaultVotingToken(value, Number(key) as ChainId),
+    ] as [ChainId, VotingToken];
+  })
 ) as Record<ChainId, VotingToken>;
 
 export const useCartStorage = create<CartState>()(
@@ -77,8 +97,29 @@ export const useCartStorage = create<CartState>()(
           });
         }
       },
-      chainToPayoutToken: ethOnlyPayoutTokens,
-      setPayoutTokenForChain: (chainId: ChainId, payoutToken: VotingToken) => {
+      chainToVotingToken: defaultVotingTokens,
+      getVotingTokenForChain: (chainId: ChainId) => {
+        const tokenFromStore = get().chainToVotingToken[chainId];
+        if (!tokenFromStore) {
+          const defaultToken = getDefaultVotingToken(
+            votingTokensMap[chainId],
+            chainId
+          );
+          console.log(
+            "no token for chain",
+            chainId,
+            " defaulting to ",
+            defaultToken,
+            " and setting it as the default token for that chain"
+          );
+
+          get().setVotingTokenForChain(chainId, defaultToken);
+          return defaultToken;
+        } else {
+          return tokenFromStore;
+        }
+      },
+      setVotingTokenForChain: (chainId: ChainId, payoutToken: VotingToken) => {
         if (!Object.values(ChainId).includes(chainId)) {
           console.warn(
             "Tried setting payoutToken",
@@ -93,15 +134,14 @@ export const useCartStorage = create<CartState>()(
         }
 
         set({
-          chainToPayoutToken: {
-            ...get().chainToPayoutToken,
+          chainToVotingToken: {
+            ...get().chainToVotingToken,
             [chainId]: payoutToken,
           },
         });
       },
     }),
     {
-      /*This is the localStorage key. Change this whenever the shape of the stores objects changes. append a v1, v2. etc. */
       name: "cart-storage",
       version: 3,
     }
