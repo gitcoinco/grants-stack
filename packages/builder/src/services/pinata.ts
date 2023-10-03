46e5ebedffd989385d26f4c7ac429c886f511c4c
Andrea Franz <andrea@gravityblast.com>
2023-10-03 10:39:00 +0200

    make sure pinata requests don't fail if base URL ends with a '/'

--- packages/builder/src/services/pinata.ts
+++ packages/builder/src/services/pinata.ts
@@ -27,7 +27,11 @@ export default class PinataClient {
   }
 
   fileURL(cid: string) {
-    return `${GATEWAY}/ipfs/${cid}`;
+    // if GATEWAY is true it throws an exception in the constructor
+    const baseURL = new URL(GATEWAY!);
+    // using the baseURL makes sure that we don't have a double '/'
+    // for the path in case GATEWAY ends with one
+    return new URL(`/ipfs/${cid}`, baseURL).toString();
   }
 
   fetchText(cid: string) {
