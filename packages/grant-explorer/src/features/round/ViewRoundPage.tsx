0cd83e59a19cc691cb2bb24ae3823420047d179c
Andrea Franz <andrea@gravityblast.com>
2023-10-10 18:54:33 +0200

    add the PowerPool token (CVP) in the payout tokens (#2424)
    * add the PowerPool token (CVP) in the payout tokens
    
    * make direct grant question "payout token" editable
    
    * add power pool logo
    
    * feat: enable single option in a dropdown
    
    * fix: don't show matching funds in dg pre round
    
    * feat: enable custom payout in dg
    
    * fix: remove gas limit
    
    ---------
    
    Co-authored-by: eagle <aparokshambhargav@gmail.com>
    Co-authored-by: Atris <vacekj@outlook.com>

--- packages/grant-explorer/src/features/round/ViewRoundPage.tsx
+++ packages/grant-explorer/src/features/round/ViewRoundPage.tsx
@@ -730,36 +730,44 @@ function PreRoundPage(props: {
               )}
             </span>
           </p>
-          <p
-            className="text-lg my-2 text-grey-400 font-normal"
-            data-testid="matching-funds"
-          >
-            Matching Funds Available:
-            <span>
-              {" "}
-              &nbsp;
-              {round.roundMetadata?.quadraticFundingConfig?.matchingFundsAvailable.toLocaleString()}
-              &nbsp;
-              {tokenData?.symbol ?? "..."}
-            </span>
-          </p>
-          <p
-            className="text-lg my-2 text-grey-400 font-normal"
-            data-testid="matching-cap"
-          >
-            Matching Cap:
-            {round.roundMetadata?.quadraticFundingConfig?.matchingCapAmount ? (
-              <span>
-                {" "}
-                &nbsp;
-                {round.roundMetadata?.quadraticFundingConfig?.matchingCapAmount}
-                &nbsp;
-                {"%"}
-              </span>
-            ) : (
-              <span>None</span>
-            )}
-          </p>
+          {!isDirectRound(round) && (
+            <div>
+              <p
+                className="text-lg my-2 text-grey-400 font-normal"
+                data-testid="matching-funds"
+              >
+                Matching Funds Available:
+                <span>
+                  {" "}
+                  &nbsp;
+                  {round.roundMetadata?.quadraticFundingConfig?.matchingFundsAvailable.toLocaleString()}
+                  &nbsp;
+                  {tokenData?.symbol ?? "..."}
+                </span>
+              </p>
+              <p
+                className="text-lg my-2 text-grey-400 font-normal"
+                data-testid="matching-cap"
+              >
+                Matching Cap:
+                {round.roundMetadata?.quadraticFundingConfig
+                  ?.matchingCapAmount ? (
+                  <span>
+                    {" "}
+                    &nbsp;
+                    {
+                      round.roundMetadata?.quadraticFundingConfig
+                        ?.matchingCapAmount
+                    }
+                    &nbsp;
+                    {"%"}
+                  </span>
+                ) : (
+                  <span>None</span>
+                )}
+              </p>
+            </div>
+          )}
           <p className="text-lg my-5 text-grey-400 font-normal border-t py-5 border-b">
             <span>{round.roundMetadata?.eligibility.description}</span>
           </p>
