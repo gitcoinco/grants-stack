863bac1b93b2a8c5b410639b9ce555573bda3887
eagle <aparokshambhargav@gmail.com>
2023-10-20 18:12:18 +0530

    feat: passport cleanup

--- packages/grant-explorer/src/features/api/passport.ts
+++ packages/grant-explorer/src/features/api/passport.ts
@@ -1,3 +1,4 @@
+import { datadogLogs } from "@datadog/browser-logs";
 import {
   submitPassport,
   fetchPassport,
@@ -9,50 +10,83 @@ import { useEffect, useState } from "react";
 export { submitPassport, fetchPassport, PassportState };
 export type { PassportResponse };
 
-export function usePassport({ address }: { address: string }) {
-  const [passport, setPassport] = useState<PassportResponse | undefined>();
+export function usePassport({ address }: { address: string | undefined }) {
   const [, setError] = useState<Response | undefined>();
 
   const [passportState, setPassportState] = useState<PassportState>(
     PassportState.LOADING
   );
 
+  const [passportScore, setPassportScore] = useState<number>();
+  const [passportColor, setPassportColor] = useState<string>("");
+  const [donationImpact, setDonationImpact] = useState<number>(0);
+
   useEffect(() => {
     setPassportState(PassportState.LOADING);
 
     const PASSPORT_COMMUNITY_ID =
       process.env.REACT_APP_PASSPORT_API_COMMUNITY_ID;
+
     if (PASSPORT_COMMUNITY_ID === undefined) {
       throw new Error("passport community id not set");
     }
-    const PASSPORT_THRESHOLD = 0;
 
     if (address && PASSPORT_COMMUNITY_ID) {
       const callFetchPassport = async () => {
         const res = await fetchPassport(address, PASSPORT_COMMUNITY_ID);
-        if (res.ok) {
-          const json = await res.json();
 
-          if (json.status === "PROCESSING") {
+        if (!res) {
+          datadogLogs.logger.error(
+            `error: callFetchPassport - fetch failed`,
+            res
+          );
+          setPassportState(PassportState.ERROR);
+          return;
+        }
+
+        if (res.ok) {
+          const scoreResponse: PassportResponse = await res.json();
+
+          if (scoreResponse.status === "PROCESSING") {
             console.log("processing, calling again in 3000 ms");
             setTimeout(async () => {
               await callFetchPassport();
             }, 3000);
             return;
-          } else if (json.status === "ERROR") {
-            // due to error at passport end
-            setPassportState(PassportState.ERROR);
+          }
+
+          if (
+            !scoreResponse.score ||
+            !scoreResponse.evidence ||
+            scoreResponse.status === "ERROR"
+          ) {
+            datadogLogs.logger.error(
+              `error: callFetchPassport - invalid score response`,
+              scoreResponse
+            );
+            setPassportState(PassportState.INVALID_RESPONSE);
             return;
           }
 
-          setPassport(json);
-          setPassportState(
-            json.score >= PASSPORT_THRESHOLD
-              ? PassportState.MATCH_ELIGIBLE
-              : PassportState.MATCH_INELIGIBLE
-          );
+          const score = Number(scoreResponse.evidence.rawScore);
+          setPassportScore(score);
+          setPassportState(PassportState.SCORE_AVAILABLE);
+          if (score < 15) {
+            setPassportColor("orange");
+            setDonationImpact(0);
+          } else if (score >= 15 && score < 25) {
+            setPassportColor("yellow");
+            setDonationImpact(50);
+          } else {
+            setPassportColor("green");
+            setDonationImpact(100);
+          }
         } else {
           setError(res);
+          datadogLogs.logger.error(
+            `error: callFetchPassport - passport NOT OK`,
+            res
+          );
           switch (res.status) {
             case 400: // unregistered/nonexistent passport address
               setPassportState(PassportState.INVALID_PASSPORT);
@@ -76,6 +110,8 @@ export function usePassport({ address }: { address: string }) {
 
   return {
     passportState,
-    passport,
+    passportScore,
+    passportColor,
+    donationImpact,
   };
 }
