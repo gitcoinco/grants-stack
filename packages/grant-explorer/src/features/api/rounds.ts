f967bde1ca506afcf91410ed1534d0eadc84ca06
eagle <aparokshambhargav@gmail.com>
2023-11-03 19:18:22 +0530

    Update getConfig to fix failing tests (#2541)
    * chore: update useConfig to fix failing tests
    
    * Fix explorer tests
    
    ---------
    
    Co-authored-by: Carl Barrdahl <carlbarrdahl@gmail.com>

--- packages/grant-explorer/src/features/api/rounds.ts
+++ packages/grant-explorer/src/features/api/rounds.ts
@@ -29,7 +29,7 @@ export type RoundOverview = {
   matchAmount: string;
   token: string;
   roundMetadata?: RoundMetadata;
-  projects?: [];
+  projects?: { id: string }[];
   payoutStrategy: {
     id: string;
     strategyName: RoundPayoutType;
