608785e4cd0ef5ea6d08672939a2f94e605ac86a
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-11-08 17:52:47 +0100

    Fix test

--- packages/grant-explorer/src/features/discovery/utils/__tests__/createRoundsStatusFilter.test.tsx
+++ packages/grant-explorer/src/features/discovery/utils/__tests__/createRoundsStatusFilter.test.tsx
@@ -3,18 +3,15 @@ import { createRoundsStatusFilter } from "../createRoundsStatusFilter";
 describe("createRoundsStatusFilter", () => {
   it("single selected filter", async () => {
     const filter = createRoundsStatusFilter("active");
-    expect(filter.roundStartTime_lt).toBeDefined();
-    expect(filter.roundEndTime_gt).toBeDefined();
-    expect(filter.roundEndTime_lt).toBeDefined();
+
+    expect(filter[0].roundStartTime_lt).toBeDefined();
+    expect(filter[0].roundEndTime_gt).toBeDefined();
+    expect(filter[0].roundEndTime_lt).toBeDefined();
   });
   it("multi selected filters", async () => {
     const filter = createRoundsStatusFilter(
       "active,taking_applications,finished"
     );
-    expect(filter.applicationsEndTime_gte).toBeDefined();
-    expect(filter.applicationsStartTime_lte).toBeDefined();
-    expect(filter.roundStartTime_lt).toBeDefined();
-    expect(filter.roundEndTime_gt).toBeDefined();
-    expect(filter.roundEndTime_lt).toBeDefined();
+    expect(filter.length).toBe(3);
   });
 });
