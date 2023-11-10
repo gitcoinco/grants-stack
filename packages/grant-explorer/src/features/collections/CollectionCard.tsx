21cef2851afa13baf45d5b75b4fab84156e674d3
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-11-10 10:11:38 +0100

    CollectionCard layout fixes

--- packages/grant-explorer/src/features/collections/CollectionCard.tsx
+++ packages/grant-explorer/src/features/collections/CollectionCard.tsx
@@ -1,10 +1,4 @@
-import {
-  Badge,
-  BasicCard,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "../common/styles";
+import { Badge, BasicCard, CardHeader } from "../common/styles";
 import { CollectionBanner } from "../discovery/CardBanner";
 import { Collection } from "./hooks/useCollections";
 
@@ -20,17 +14,17 @@ const CollectionCard = ({ collection }: CollectionCardProps) => {
         <CardHeader>
           <CollectionBanner projectIds={projects} />
         </CardHeader>
-        <CardContent>
-          <CardTitle>{name}</CardTitle>
+        <div className="p-4 space-y-1">
+          <div className="font-medium truncate text-xl">{name}</div>
           <div className="flex justify-between items-center">
             <div className="text-grey-400 text-sm">
               {projects.length} projects
             </div>
             <div className="text-sm flex gap-2 items-center">
-              by <Badge>ensname.eth</Badge>
+              by <Badge rounded="full">ensname.eth</Badge>
             </div>
           </div>
-        </CardContent>
+        </div>
       </a>
     </BasicCard>
   );
