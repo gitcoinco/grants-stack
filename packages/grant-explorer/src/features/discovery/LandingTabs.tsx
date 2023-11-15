c2485ae79fee47729fb9531287a5737da5269779
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-11-15 18:23:50 +0100

    Add dynamic tab titles for mobile/desktop

--- packages/grant-explorer/src/features/discovery/LandingTabs.tsx
+++ packages/grant-explorer/src/features/discovery/LandingTabs.tsx
@@ -2,36 +2,42 @@ import { useLocation } from "react-router-dom";
 import { Tab, Tabs } from "../common/styles";
 import { toQueryString } from "./RoundsFilter";
 import { FilterStatus } from "./hooks/useFilterRounds";
+import { useMediaQuery } from "@chakra-ui/react";
 
 type Tab = {
   to: string;
   children: string;
   tabName: string;
 };
-const tabs: Tab[] = [
-  {
-    to: "/",
-    children: "Home",
-    tabName: "home-tab",
-  },
-  {
-    to: `/rounds?${toQueryString({
-      orderBy: "matchAmount",
-      orderDirection: "desc",
-      status: [FilterStatus.active, FilterStatus.taking_applications].join(","),
-    })}`,
-    children: "Explore rounds",
-    tabName: "home-rounds-tab",
-  },
-  {
-    to: "/projects",
-    children: "Explore projects",
-    tabName: "home-projects-tab",
-  },
-];
 
 export default function LandingTabs() {
   const { pathname } = useLocation();
+  const [isDesktop] = useMediaQuery("(min-width: 768px)");
+
+  const tabs: Tab[] = [
+    {
+      to: "/",
+      children: "Home",
+      tabName: "home-tab",
+    },
+    {
+      to: `/rounds?${toQueryString({
+        orderBy: "matchAmount",
+        orderDirection: "desc",
+        status: [FilterStatus.active, FilterStatus.taking_applications].join(
+          ","
+        ),
+      })}`,
+      children: isDesktop ? "Explore rounds" : "Rounds",
+      tabName: "home-rounds-tab",
+    },
+    {
+      to: "/projects",
+      children: isDesktop ? "Explore projects" : "Projects",
+      tabName: "home-projects-tab",
+    },
+  ];
+
   return (
     <Tabs>
       {tabs.map((tab) => {
