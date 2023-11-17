2e5c0aca5650843264cc82ad276e12d6ea34f305
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-11-17 16:46:53 +0100

    Hide grants veterans

--- packages/grant-explorer/src/features/collections/hooks/useCollections.ts
+++ packages/grant-explorer/src/features/collections/hooks/useCollections.ts
@@ -1,15 +1,15 @@
 import { firstTimeGranteesProject } from "../data/firstTimeGrantees";
-import { veteransProjects } from "../data/veterans";
+// import { veteransProjects } from "../data/veterans";
 import { getAddress } from "viem";
 
 import image01 from "../../../assets/collections/collection_01.jpg";
 import image02 from "../../../assets/collections/collection_02.jpg";
 import image03 from "../../../assets/collections/collection_03.jpg";
 import image04 from "../../../assets/collections/collection_04.jpg";
-import image05 from "../../../assets/collections/collection_05.jpg";
-import image06 from "../../../assets/collections/collection_06.jpg";
-import image07 from "../../../assets/collections/collection_07.jpg";
-import image08 from "../../../assets/collections/collection_08.jpg";
+// import image05 from "../../../assets/collections/collection_05.jpg";
+// import image06 from "../../../assets/collections/collection_06.jpg";
+// import image07 from "../../../assets/collections/collection_07.jpg";
+// import image08 from "../../../assets/collections/collection_08.jpg";
 import image09 from "../../../assets/collections/collection_09.jpg";
 import image10 from "../../../assets/collections/collection_10.jpg";
 import image11 from "../../../assets/collections/collection_11.jpg";
@@ -71,15 +71,15 @@ export const collections: Collection[] = [
       "This collection showcases all grantees in GG19 that have not participated in a past round on Grants Stack! Give these first-time grantees some love (and maybe some donations, too!).",
     projects: firstTimeGranteesProject.map(normalizeAddress),
   },
-  {
-    id: "grants-stack-veterans",
-    author: "Gitcoin",
-    name: "Grants Stack Veterans",
-    images: [image05, image06, image07, image08],
-    description:
-      "This collection showcases all grantees in GG19 that have participated in a past GG18 and/or Beta Round! Give these Grants Stack Veterans some love (and maybe some donations, too!).",
-    projects: veteransProjects.map(normalizeAddress),
-  },
+  // {
+  //   id: "grants-stack-veterans",
+  //   author: "Gitcoin",
+  //   name: "Grants Stack Veterans",
+  //   images: [image05, image06, image07, image08],
+  //   description:
+  //     "This collection showcases all grantees in GG19 that have participated in a past GG18 and/or Beta Round! Give these Grants Stack Veterans some love (and maybe some donations, too!).",
+  //   projects: veteransProjects.map(normalizeAddress),
+  // },
   {
     id: "devansh-mehtas-picks",
     author: "Devansh Mehta",
