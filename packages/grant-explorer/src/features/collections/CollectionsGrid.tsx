350365186230693de1dc451bda9eabef011fd965
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-11-23 10:55:06 +0000

    Shuffle collections

--- packages/grant-explorer/src/features/collections/CollectionsGrid.tsx
+++ packages/grant-explorer/src/features/collections/CollectionsGrid.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import CollectionCard from "./CollectionCard";
 import { Collection } from "./hooks/useCollections";
 
@@ -7,9 +8,12 @@ const collectionGridLayout = [0, 5, 6, 11];
 const DISPLAY_COUNT = 12;
 
 export function CollectionsGrid({ data }: { data: Collection[] }) {
+  // Shuffle the collections
+  const shuffled = useMemo(() => shuffle(data), [data]);
+
   return (
     <div className="md:grid space-y-4 md:space-y-0 md:grid-cols-4 gap-6">
-      {data?.slice(0, DISPLAY_COUNT).map((collection, i) => {
+      {shuffled?.slice(0, DISPLAY_COUNT).map((collection, i) => {
         const size = collectionGridLayout.includes(i) ? "big" : "small";
         return (
           <div
@@ -22,4 +26,17 @@ export function CollectionsGrid({ data }: { data: Collection[] }) {
       })}
     </div>
   );
+}
+
+// Random every time the page loads
+const SEED = Math.random();
+
+function shuffle<T>(array: T[]): T[] {
+  const _array = [...array];
+  for (let i = _array.length - 1; i > 0; i--) {
+    const j = Math.floor(SEED * (i + 1));
+    [_array[i], _array[j]] = [_array[j], _array[i]];
+  }
+
+  return _array;
 }
