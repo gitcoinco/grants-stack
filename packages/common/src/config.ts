f6ee32d5775cd962398d8a7f83a7e5edaed5f783
eagle <aparokshambhargav@gmail.com>
2023-12-13 00:12:03 +0530

    chore: update .env.test

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -100,6 +100,12 @@ export function getConfig(): Config {
         [ChainId.POLYGON_MUMBAI]: z
           .string()
           .parse(process.env.REACT_APP_SUBGRAPH_POLYGON_MUMBAI_API),
+        [ChainId.ZKSYNC_ERA_TESTNET_CHAIN_ID]: z
+          .string()
+          .parse(process.env.REACT_APP_SUBGRAPH_ZKSYNC_ERA_TESTNET_API),
+        [ChainId.ZKSYNC_ERA_MAINNET_CHAIN_ID]: z
+          .string()
+          .parse(process.env.REACT_APP_SUBGRAPH_ZKSYNC_ERA_MAINNET_API),
       },
     },
     pinata: {
