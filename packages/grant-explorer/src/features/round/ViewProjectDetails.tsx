8c6f11bdbf166db4a78d6c204ecd4978734fa637
Mohamed Boudra <boudra.moha@gmail.com>
2024-01-23 13:20:57 +0100

    remove unused verifier

--- packages/grant-explorer/src/features/round/ViewProjectDetails.tsx
+++ packages/grant-explorer/src/features/round/ViewProjectDetails.tsx
@@ -73,8 +73,6 @@ const boundFetch = fetch.bind(window);
 export const IAM_SERVER =
   "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC";
 
-const verifier = new PassportVerifier();
-
 export default function ViewProjectDetails() {
   const [selectedTab, setSelectedTab] = useState(0);
 
