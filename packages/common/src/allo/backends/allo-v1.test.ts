4108d839da49f23f73618814b77d0188039478fd
Atris <vacekj@outlook.com>
2024-01-29 14:01:54 +0100

    fix: test

--- packages/common/src/allo/backends/allo-v1.test.ts
+++ packages/common/src/allo/backends/allo-v1.test.ts
@@ -1,9 +1,9 @@
-import { describe, test, expect, vi, beforeEach } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { AlloV1 } from "./allo-v1";
-import { zeroAddress, Hex, encodeEventTopics } from "viem";
+import { encodeEventTopics, Hex, zeroAddress } from "viem";
 import {
-  TransactionReceipt,
   createMockTransactionSender,
+  TransactionReceipt,
 } from "../transaction-sender";
 import { Result, success } from "../common";
 import ProjectRegistry from "../abis/allo-v1/ProjectRegistry";
@@ -65,7 +65,7 @@ describe("AlloV1", () => {
     expect(result).toEqual(
       success({
         projectId:
-          "0xd0c4b8bf41dcf0607cd6c6d5f7c6423344ce99ddaaa72c31a7d8fb332a218878",
+          "0xa0affa31521afe084aee15c3ff5570c600b014cae2a9c45a9cc1e50b0c9852e5",
       })
     );
     expect(transactionSender.sentTransactions).toHaveLength(1);
