a56c4f5ce076644dc5eacceaa7d52b6222eae5f1
Andrea Franz <andrea@gravityblast.com>
2024-02-06 21:40:14 +0100

    enable Allo switch button if VERCEL_ENV === "preview"

--- packages/common/src/components/Footer.tsx
+++ packages/common/src/components/Footer.tsx
@@ -52,7 +52,8 @@ export default function Footer() {
     >
       <div className={"text-gray-500 text-xs"}>
         build {COMMIT_HASH}-{ALLO_VERSION}
-        {config.appEnv === "development" && (
+        {(process.env.VERCEL_ENV === "preview" ||
+          config.appEnv === "development") && (
           <button
             className="ml-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={() => switchAlloVersion(alloVersionAlternative)}
