ee832c689b2a9b0dea71c86b905a7e2df84227e9
Atris <vacekj@outlook.com>
2024-02-08 11:26:03 +0100

    fix: e2e test dir

--- packages/builder/playwright.config.js
+++ packages/builder/playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./e2e",
   timeout: 60 * 1000,
   expect: {
     timeout: 5000,
