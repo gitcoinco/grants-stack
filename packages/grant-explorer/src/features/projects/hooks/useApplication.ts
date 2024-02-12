371db1ce6509a758e82ca3fba3cd2fe1963931d5
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-02-12 10:47:30 +0100

    Explorer: [2670] Application page indexer data (#2712)
    * Application page redesign
    
    * Update RoundEndedBanner
    
    * Clean up
    
    * Remove unused file
    
    * Remove console.log
    
    * Remove unused code
    
    * Fix tests
    
    * Fix tests
    
    * Add loading skeleton for about text
    
    * Add wrapping div to reduce diff in component
    
    * Refactor hooks and ProjectStats into seperate files
    
    * Revert "Refactor hooks and ProjectStats into seperate files"
    
    This reverts commit ce73c96624d6490a53ea2001f7f1b65024b66d87.
    
    * Fix icon color
    
    * Create useApplication hook to fetch data from the indexer
    
    * fix: use real indexer v2 endpointˆ
    
    * fix: finish application page indexer v2 data
    
    * feat: don't show v2 projects on v1, disallow adding v2 projects to cart. fix adding v1 projects to cart
    
    * feat: don't show v2 projects on v1, disallow adding v2 projects to cart. fix adding v1 projects to cart
    
    * feat: type-safe non-optional useParams
    
    * wip: tests
    
    * fix: tests for application pageˆ
    
    * feat: simplify tests for application page
    
    * fix: fix cart tests
    
    * fix: types
    
    * Update packages/grant-explorer/src/features/round/__tests__/ViewProjectDetails.test.tsx
    
    Co-authored-by: Mohamed Boudra <boudra.moha@gmail.com>
    
    ---------
    
    Co-authored-by: Atris <vacekj@outlook.com>
    Co-authored-by: Mohamed Boudra <boudra.moha@gmail.com>

---
+++ packages/grant-explorer/src/features/projects/hooks/useApplication.ts
@@ -0,0 +1,154 @@
+import useSWR from "swr";
+import {
+  ApplicationStatus,
+  GrantApplicationFormAnswer,
+  Project,
+  ProjectMetadata,
+  Round,
+  RoundMetadata,
+} from "data-layer";
+import { getConfig } from "common/src/config";
+
+type Params = {
+  chainId?: number;
+  roundId?: string;
+  applicationId?: string;
+};
+
+const {
+  dataLayer: { gsIndexerEndpoint },
+  allo: { version },
+} = getConfig();
+
+const allo_v2_url = gsIndexerEndpoint + "/graphql";
+
+const APPLICATION_QUERY = `
+query Application($chainId: Int!, $applicationId: String!, $roundId: String!) {
+  application(chainId: $chainId, id: $applicationId, roundId: $roundId) {
+    id
+    chainId
+    roundId
+    projectId
+    status
+    totalAmountDonatedInUsd
+    uniqueDonorsCount
+    round {
+      donationsStartTime
+      donationsEndTime
+      applicationsStartTime
+      applicationsEndTime
+      matchTokenAddress
+      roundMetadata
+    }
+    metadata
+    project {
+      tags
+      id
+      metadata
+    }
+  }
+}
+`;
+
+export type Application = {
+  id: string;
+  chainId: string;
+  roundId: string;
+  projectId: string;
+  status: ApplicationStatus;
+  totalAmountDonatedInUsd: string;
+  totalDonationsCount: string;
+  round: {
+    donationsStartTime: string;
+    donationsEndTime: string;
+    applicationsStartTime: string;
+    applicationsEndTime: string;
+    roundMetadata: RoundMetadata;
+    matchTokenAddress: string;
+  };
+  project: {
+    id: string;
+    metadata: ProjectMetadata;
+  };
+  metadata: {
+    application: {
+      recipient: string;
+      answers: GrantApplicationFormAnswer[];
+    };
+  };
+};
+
+export function useApplication(params: Params) {
+  const shouldFetch = Object.values(params).every(Boolean);
+  return useSWR(shouldFetch ? ["applications", params] : null, async () => {
+    return request(allo_v2_url, {
+      query: APPLICATION_QUERY,
+      variables: params,
+    })
+      .then((r) => r.data?.application)
+      .then((application) => {
+        /* Don't fetch v2 rounds when allo version is set to v1 */
+        if (
+          version === "allo-v1" &&
+          application.project.tags.includes("allo-v2")
+        ) {
+          return;
+        }
+        return application;
+      });
+  });
+}
+
+// These functions map the application data to fit the shape of the view
+// Changing the view would require significant changes to the markup + cart storage
+export function mapApplicationToProject(
+  application?: Application
+): Project | undefined {
+  if (!application) return;
+  return {
+    grantApplicationId: application.id,
+    applicationIndex: Number(application.id),
+    projectRegistryId: application.projectId,
+    recipient: application.metadata.application.recipient,
+    projectMetadata: application.project.metadata,
+    status: application.status,
+    grantApplicationFormAnswers: application.metadata.application.answers ?? [],
+  };
+}
+
+export function mapApplicationToRound(
+  application?: Application
+): Round | undefined {
+  if (!application) return;
+  return {
+    roundEndTime: new Date(application.round.donationsEndTime),
+    roundStartTime: new Date(application.round.donationsStartTime),
+    applicationsStartTime: new Date(application.round.applicationsStartTime),
+    applicationsEndTime: new Date(application.round.applicationsEndTime),
+    roundMetadata: application.round.roundMetadata,
+    token: application.round.matchTokenAddress,
+    // This is missing from the indexer
+    payoutStrategy: {
+      id: "id",
+      strategyName: "MERKLE",
+    },
+    // These might not be used anywhere in the app
+    votingStrategy: "",
+    ownedBy: "",
+  };
+}
+
+async function request(url: string, body: unknown) {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then(async (res) => {
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return await res.json();
+  });
+}
