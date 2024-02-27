ec7ba89435fb67a3dc68fc1c9446d4adbd81f44c
Mohamed Boudra <boudra.moha@gmail.com>
2024-02-27 18:43:18 +0100

    log errors

--- packages/round-manager/src/features/common/useApplicationsByRoundId.tsx
+++ packages/round-manager/src/features/common/useApplicationsByRoundId.tsx
@@ -9,43 +9,51 @@ export const useApplicationsByRoundId = (roundId: string) => {
     chain: { id: chainId },
   } = useWallet();
 
-  return useSWR([roundId, chainId], async () => {
-    const dataLayerApplications = await dataLayer.getApplicationsForManager({
-      roundId,
-      chainId,
-    });
+  return useSWR(
+    [roundId, chainId],
+    async () => {
+      const dataLayerApplications = await dataLayer.getApplicationsForManager({
+        roundId,
+        chainId,
+      });
 
-    const applications: GrantApplication[] = dataLayerApplications.map(
-      (application) => {
-        return {
-          id: application.id,
-          applicationIndex: Number(application.id),
-          round: application.roundId,
-          status: application.status,
-          metadata: application.metadata,
-          project: {
-            ...application.metadata.application.project,
-            owners: application.project.roles,
-            id: application.projectId,
-          },
-          inReview: application.status === "IN_REVIEW",
-          recipient: application.metadata.application.recipient,
-          createdAt: "0",
-          projectsMetaPtr: { protocol: 1, pointer: "" },
-          payoutStrategy: {
-            strategyName: application.round.strategyName,
-            id: application.round.strategyAddress,
-            payouts: [],
-          },
-          statusSnapshots: application.statusSnapshots.map((snapshot) => ({
-            ...snapshot,
-            updatedAt: new Date(snapshot.updatedAt),
-          })),
-          answers: application.metadata.application.answers,
-        };
-      }
-    );
+      const applications: GrantApplication[] = dataLayerApplications.map(
+        (application) => {
+          return {
+            id: application.id,
+            applicationIndex: Number(application.id),
+            round: application.roundId,
+            status: application.status,
+            metadata: application.metadata,
+            project: {
+              ...application.metadata.application.project,
+              owners: application.project.roles,
+              id: application.projectId,
+            },
+            inReview: application.status === "IN_REVIEW",
+            recipient: application.metadata.application.recipient,
+            createdAt: "0",
+            projectsMetaPtr: { protocol: 1, pointer: "" },
+            payoutStrategy: {
+              strategyName: application.round.strategyName,
+              id: application.round.strategyAddress,
+              payouts: [],
+            },
+            statusSnapshots: application.statusSnapshots.map((snapshot) => ({
+              ...snapshot,
+              updatedAt: new Date(snapshot.updatedAt),
+            })),
+            answers: application.metadata.application.answers,
+          };
+        }
+      );
 
-    return applications;
-  });
+      return applications;
+    },
+    {
+      onError: (error) => {
+        console.error("useApplicationsByRoundId", error);
+      },
+    }
+  );
 };
