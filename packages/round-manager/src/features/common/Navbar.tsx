5211332b37f3b44741133aa74b75489508f660be
Mohamed Boudra <boudra.moha@gmail.com>
2024-03-20 22:03:44 +0700

    fix manager switcher

--- packages/round-manager/src/features/common/Navbar.tsx
+++ packages/round-manager/src/features/common/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar({
                 )}
               </div>
               {alloVersionSwitcherVisible && (
-                <AlloVersionSwitcher color="black" />
+                <AlloVersionSwitcher color="white" />
               )}
               <ConnectButton />
             </div>
