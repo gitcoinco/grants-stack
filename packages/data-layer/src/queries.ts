8320834a7abfe405e2e7de366363d6953df7bc8a
Andrea Franz <andrea@gravityblast.com>
2024-03-21 17:58:34 +0100

    fix(Explorer): add project alias to canonicalProject subquery (#3183)

--- packages/data-layer/src/queries.ts
+++ packages/data-layer/src/queries.ts
@@ -581,7 +581,7 @@ export const getRoundByIdAndChainIdWithApprovedApplications = gql`
         projectId
         status
         metadata
-        canonicalProject {
+        project: canonicalProject {
           id
           metadata
           anchorAddress
