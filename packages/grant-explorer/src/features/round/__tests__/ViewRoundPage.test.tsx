6af67b02a5e0ce2f3567ff103769aa380b54bd99
Mohamed Boudra <boudra.moha@gmail.com>
2024-03-22 20:25:37 +0700

    Fix allo v2 voting incorrect anchor address (#3192)
    * fix anchor address in explorer
    
    * fix tests
    
    * fix empty array default
    
    * remove console.log

--- packages/grant-explorer/src/features/round/__tests__/ViewRoundPage.test.tsx
+++ packages/grant-explorer/src/features/round/__tests__/ViewRoundPage.test.tsx
@@ -80,7 +80,7 @@ vi.mock("react-router-dom", async () => {
 });
 
 const mockDataLayer = {
-  getRoundByIdAndChainIdWithApprovedApplications: vi.fn().mockResolvedValue({
+  getRoundForExplorer: vi.fn().mockResolvedValue({
     rounds: [],
   }),
 } as unknown as DataLayer;
