4341195e9167531288b394d025a60395a4a10912
Jaxcoder <codenamejason@outlook.com>
2024-05-02 09:13:21 -0400

    update env name

--- packages/grant-explorer/src/posthog.ts
+++ packages/grant-explorer/src/posthog.ts
@@ -2,12 +2,9 @@ import posthog from "posthog-js";
 
 export const initPosthog = () => {
   console.log("Initializing Posthog");
-  if (
-    process.env.REACT_APP_PUBLIC_POSTHOG_KEY &&
-    process.env.REACT_APP_PUBLIC_POSTHOG_HOST
-  ) {
-    posthog.init(process.env.REACT_APP_PUBLIC_POSTHOG_KEY, {
-      api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  if (process.env.REACT_APP_POSTHOG_KEY && process.env.REACT_APP_POSTHOG_HOST) {
+    posthog.init(process.env.REACT_APP_POSTHOG_KEY, {
+      api_host: process.env.REACT_APP_POSTHOG_HOST,
       session_recording: {
         maskTextSelector: '[data-testid="rk-account-button"]',
       },
