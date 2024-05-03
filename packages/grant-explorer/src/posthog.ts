02ed7a3964d80cbdd41d9dfa9b5373bcc08983c1
jaxcoder / <codenamejason@outlook.com>
2024-05-03 00:23:44 -0400

    update posthog with new account (#3406)
    * wip/update posthog with new account
    
    * update env name
    
    * remove doxxing info/wallet
    
    * lint

--- packages/grant-explorer/src/posthog.ts
+++ packages/grant-explorer/src/posthog.ts
@@ -1,12 +1,19 @@
 import posthog from "posthog-js";
 
 export const initPosthog = () => {
-  if (process.env.REACT_APP_POSTHOG_TOKEN) {
-    posthog.init(process.env.REACT_APP_POSTHOG_TOKEN, {
-      api_host: "https://grants-stack-ux-events.gitcoin.co",
+  console.log("Initializing Posthog");
+  if (process.env.REACT_APP_POSTHOG_KEY && process.env.REACT_APP_POSTHOG_HOST) {
+    posthog.init(process.env.REACT_APP_POSTHOG_KEY, {
+      api_host: process.env.REACT_APP_POSTHOG_HOST,
       session_recording: {
         maskTextSelector: '[data-testid="rk-account-button"]',
       },
     });
+
+    console.log("Posthog initialized");
+
+    return posthog;
   }
+
+  console.log("Posthog not initialized");
 };
