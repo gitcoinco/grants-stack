ca7bd0a81f53f986d3b3531d47737a03dbbd8369
eagle <aparokshambhargav@gmail.com>
2024-05-09 19:46:12 +0530

    feat: update ViewManageTeam

--- packages/round-manager/src/features/round/ViewManageTeam.tsx
+++ packages/round-manager/src/features/round/ViewManageTeam.tsx
@@ -1,7 +1,15 @@
 export default function ViewManageTeam() {
   return (
     <div>
-      <h4>Manage Team</h4>
+      <p className="font-bold text-lg mt-2 mb-2">Manage Team</p>
+      <p className="text-sm text-gray-400 mb-2">
+        Add or remove admins and operators to your team.{" "}
+      </p>
+      <p className="text-sm text-gray-400 mb-2">
+        Make sure to have at least two admins at all times for security
+        purposes.
+      </p>
+      <p className="text-md mt-6 mb-2">View Members</p>
     </div>
   );
 }
