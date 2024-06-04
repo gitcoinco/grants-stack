9436f96f80af4baf136ad823f0799448c14777ea
eagle <aparokshambhargav@gmail.com>
2024-06-04 19:00:49 +0530

    chore: address feedback

--- packages/grant-explorer/src/features/projects/hooks/useProject.ts
+++ packages/grant-explorer/src/features/projects/hooks/useProject.ts
@@ -1,6 +1,5 @@
 import useSWR from "swr";
 import { DataLayer } from "data-layer";
-import { AlloVersion } from "data-layer/src/data-layer.types";
 
 type Params = {
   projectId: string;
@@ -10,7 +9,7 @@ export function useProject(params: Params, dataLayer: DataLayer) {
   return useSWR(["projects", params], async () => {
     const validatedParams = {
       projectId: params.projectId,
-      alloVersion: "allo-v2" as AlloVersion,
+      alloVersion: "allo-v2" as const,
     };
     return (await dataLayer.getProjectById(validatedParams)) ?? undefined;
   });
