e22c1fbcad24d259bab231f2d2603bb499ad37f1
eagle <aparokshambhargav@gmail.com>
2024-06-04 18:31:24 +0530

    chore: address engg feedback

--- packages/grant-explorer/src/features/projects/hooks/useProject.ts
+++ packages/grant-explorer/src/features/projects/hooks/useProject.ts
@@ -3,14 +3,13 @@ import { DataLayer } from "data-layer";
 import { AlloVersion } from "data-layer/src/data-layer.types";
 
 type Params = {
-  projectId?: string;
+  projectId: string;
 };
 
 export function useProject(params: Params, dataLayer: DataLayer) {
-  const shouldFetch = Object.values(params).every(Boolean);
-  return useSWR(shouldFetch ? ["applications", params] : null, async () => {
+  return useSWR(["projects", params], async () => {
     const validatedParams = {
-      projectId: params.projectId as string,
+      projectId: params.projectId,
       alloVersion: "allo-v2" as AlloVersion,
     };
     return (await dataLayer.getProjectById(validatedParams)) ?? undefined;
