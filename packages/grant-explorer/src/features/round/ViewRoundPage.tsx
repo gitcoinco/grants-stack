4677577544e2dbae18a88a50737a518deedb33b3
Hussein Martinez <husse.dev@gmail.com>
2024-06-07 19:10:26 +0200

    chore: removed legacy PreRoundPage, ApplyButton and Inactive Button component code

--- packages/grant-explorer/src/features/round/ViewRoundPage.tsx
+++ packages/grant-explorer/src/features/round/ViewRoundPage.tsx
@@ -18,11 +18,10 @@ import {
   useTokenPrice,
   TToken,
   getTokensByChainId,
-  getTokens,
   stringToBlobUrl,
   getChainById,
 } from "common";
-import { Button, Input } from "common/src/styles";
+import { Input } from "common/src/styles";
 import AlloV1 from "common/src/icons/AlloV1";
 import AlloV2 from "common/src/icons/AlloV2";
 
@@ -33,12 +32,10 @@ import { ReactComponent as WarpcastIcon } from "../../assets/warpcast-logo.svg";
 import { ReactComponent as TwitterBlueIcon } from "../../assets/x-logo.svg";
 
 import { useRoundById } from "../../context/RoundContext";
-import { CartProject, Project, Requirement, Round } from "../api/types";
+import { CartProject, Project, Round } from "../api/types";
 import { getDaysLeft, isDirectRound, isInfiniteDate } from "../api/utils";
 import { PassportWidget } from "../common/PassportWidget";
 
-import Footer from "common/src/components/Footer";
-import Navbar from "../common/Navbar";
 import NotFoundPage from "../common/NotFoundPage";
 import { ProjectBanner } from "../common/ProjectBanner";
 import RoundEndedBanner from "../common/RoundEndedBanner";
@@ -1171,215 +1168,5 @@ const ShareStatsButton = ({
       <LinkIcon className="w-4 h-4" />
       Share
     </button>
-  );
-};
-
-function PreRoundPage(props: {
-  round: Round;
-  chainId: string;
-  roundId: string;
-  element: (req: Requirement, index: number) => JSX.Element;
-}) {
-  const { round, chainId, roundId, element } = props;
-
-  const applicationURL = `${builderURL}/#/chains/${chainId}/rounds/${roundId}`;
-
-  const currentTime = new Date();
-  const isBeforeApplicationStartDate =
-    round && round.applicationsStartTime >= currentTime;
-  // covers infinite dates for applicationsEndTime
-  const isDuringApplicationPeriod =
-    (round && currentTime >= round.applicationsStartTime) ||
-    currentTime <=
-      (isInfiniteDate(round.applicationsEndTime) || round.applicationsEndTime);
-
-  const isAfterApplicationEndDateAndBeforeRoundStartDate =
-    round &&
-    round.roundStartTime >= currentTime &&
-    (isInfiniteDate(round.applicationsEndTime) ||
-      round.applicationsEndTime <= currentTime);
-
-  const { data } = useToken({
-    address: getAddress(props.round.token),
-    chainId: Number(chainId),
-  });
-
-  const tokenData = getTokensByChainId(Number(chainId)).find(
-    (t) => t.address.toLowerCase() === props.round.token.toLowerCase()
-  );
-
-  return (
-    <div className="mt-20 flex justify-center">
-      <div className="max-w-screen-lg md:w-full">
-        <div className="text-center">
-          <div className="lg:inline-block md:inline-block"></div>
-          <p className="mb-4 text-2xl text-black font-bold">
-            {round.roundMetadata?.name}
-          </p>
-          <p
-            className="text-lg my-2 font-normal text-grey-400"
-            data-testid="application-period"
-          >
-            Application Period:
-            <span className="mx-1">
-              <span className="mr-1">
-                {formatLocalDateAsISOString(round.applicationsStartTime)}
-              </span>
-
-              <span>( {getLocalTime(round.applicationsStartTime)} )</span>
-
-              <span className="mx-1">-</span>
-
-              {!isInfiniteDate(round.applicationsEndTime) && (
-                <>
-                  <span className="mr-1">
-                    {formatLocalDateAsISOString(round.applicationsEndTime)}
-                  </span>
-
-                  <span>{getLocalTime(round.applicationsEndTime)}</span>
-                </>
-              )}
-              {isInfiniteDate(round.applicationsEndTime) && (
-                <>
-                  <span>No End Date</span>
-                </>
-              )}
-            </span>
-          </p>
-          <p
-            className="text-lg my-2 font-normal text-grey-400"
-            data-testid="round-period"
-          >
-            Round Period:
-            <span>
-              <span className="mx-1">
-                {formatLocalDateAsISOString(round.roundStartTime)}
-              </span>
-
-              <span>( {getLocalTime(round.roundStartTime)} )</span>
-
-              <span className="mx-1">-</span>
-
-              {!isInfiniteDate(round.roundEndTime) && (
-                <>
-                  <span className="mr-1">
-                    {formatLocalDateAsISOString(round.roundEndTime)}
-                  </span>
-
-                  <span>{getLocalTime(round.roundEndTime)}</span>
-                </>
-              )}
-              {isInfiniteDate(round.roundEndTime) && (
-                <>
-                  <span>No End Date</span>
-                </>
-              )}
-            </span>
-          </p>
-          {!isDirectRound(round) && (
-            <div>
-              <p
-                className="text-lg my-2 text-grey-400 font-normal"
-                data-testid="matching-funds"
-              >
-                Matching Funds Available:
-                <span>
-                  {" "}
-                  &nbsp;
-                  {round.roundMetadata?.quadraticFundingConfig?.matchingFundsAvailable.toLocaleString()}
-                  &nbsp;
-                  {tokenData?.code ?? "..."}
-                </span>
-              </p>
-              <p
-                className="text-lg my-2 text-grey-400 font-normal"
-                data-testid="matching-cap"
-              >
-                Matching Cap:
-                {round.roundMetadata?.quadraticFundingConfig
-                  ?.matchingCapAmount ? (
-                  <span>
-                    {" "}
-                    &nbsp;
-                    {
-                      round.roundMetadata?.quadraticFundingConfig
-                        ?.matchingCapAmount
-                    }
-                    &nbsp;
-                    {"%"}
-                  </span>
-                ) : (
-                  <span>None</span>
-                )}
-              </p>
-            </div>
-          )}
-          <p className="text-lg my-5 text-grey-400 font-normal border-t py-5 border-b">
-            <span>{round.roundMetadata?.eligibility.description}</span>
-          </p>
-          <p
-            className="mb-4 text-2xl text-black font-bold"
-            data-testid="round-eligibility"
-          >
-            Round Eligibility
-          </p>
-          <div className="container justify-center max-w-fit mx-auto">
-            <ul className="list-disc list-inside text-lg text-grey-400 text-left font-normal">
-              {round.roundMetadata?.eligibility.requirements?.map(element)}
-            </ul>
-          </div>
-          <div className="container mx-auto flex mt-4 mb-8 lg:w-96">
-            {isBeforeApplicationStartDate && (
-              <InactiveButton
-                label="Apply to Grant Round"
-                testid="applications-open-button"
-              />
-            )}
-
-            {isDuringApplicationPeriod && (
-              <ApplyButton applicationURL={applicationURL} />
-            )}
-
-            {isAfterApplicationEndDateAndBeforeRoundStartDate && (
-              <InactiveButton
-                label="Application period ended"
-                testid="applications-closed-button"
-              />
-            )}
-          </div>
-        </div>
-        <div className="basis-1/2 right-0"></div>
-      </div>
-    </div>
-  );
-}
-
-const ApplyButton = (props: { applicationURL: string }) => {
-  const { applicationURL } = props;
-
-  return (
-    <Button
-      type="button"
-      onClick={() => window.open(applicationURL, "_blank")}
-      className="mt-2 basis-full items-center justify-center shadow-sm text-sm rounded md:h-12"
-      data-testid="apply-button"
-    >
-      Apply to Grant Round
-    </Button>
-  );
-};
-
-const InactiveButton = (props: { label: string; testid: string }) => {
-  const { label, testid } = props;
-
-  return (
-    <Button
-      type="button"
-      className="basis-full items-center justify-center shadow-sm text-sm bg-grey-300 rounded border-1 md:h-12"
-      data-testid={testid}
-      disabled={true}
-    >
-      {label}
-    </Button>
   );
 };
