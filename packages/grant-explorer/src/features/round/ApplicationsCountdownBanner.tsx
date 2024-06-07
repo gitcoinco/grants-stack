ac8bd5b734b0e9cace7a0b886c7c7e41007d22f8
Hussein Martinez <husse.dev@gmail.com>
2024-06-07 14:58:10 +0200

    feat: implemented ApplicationsCountdownBanner component

---
+++ packages/grant-explorer/src/features/round/ApplicationsCountdownBanner.tsx
@@ -0,0 +1,90 @@
+import { Button } from "common/src/styles";
+import { TimeLeft, getTimeLeft, parseTimeLeftString } from "../api/utils";
+
+type ApplicationPeriodStatus =
+  | "pre-application"
+  | "post-application"
+  | "during-application";
+
+const generateCountdownString = (
+  targetDate: Date | undefined
+): string | undefined => {
+  if (!targetDate) return undefined;
+
+  const targetDateString = Math.round(targetDate.getTime() / 1000).toString();
+  const timeLeft: TimeLeft = getTimeLeft(targetDateString);
+
+  const timeLeftString: string = parseTimeLeftString(timeLeft);
+
+  return timeLeftString;
+};
+
+const generateBannerString = (
+  status: ApplicationPeriodStatus,
+  targetDate: Date | undefined
+): string => {
+  switch (status) {
+    case "pre-application":
+      return `Applications open in ${generateCountdownString(targetDate)}!`;
+    case "during-application":
+      return `Applications close in ${generateCountdownString(targetDate)}!`;
+    case "post-application":
+      return `Applications are closed`;
+    default:
+      throw new Error("Unknown ApplicationPeriodStatus");
+  }
+};
+
+function ApplicationsCountdownBanner(props: {
+  startDate: Date;
+  endDate: Date;
+  applicationURL: string;
+}) {
+  const { startDate, endDate, applicationURL } = props;
+
+  let targetDate: Date | undefined = undefined;
+  let status: ApplicationPeriodStatus = "post-application";
+
+  const currentTime = new Date();
+
+  const isBeforeApplicationPeriod = currentTime < startDate;
+  const isDuringApplicationPeriod =
+    currentTime >= startDate && currentTime < endDate;
+
+  if (isDuringApplicationPeriod) {
+    targetDate = endDate;
+    status = "during-application";
+  } else if (isBeforeApplicationPeriod) {
+    targetDate = startDate;
+    status = "pre-application";
+  }
+
+  const bannerString = generateBannerString(status, targetDate);
+
+  return (
+    <div className="flex flex-col items-center bg-grey-50 w-fit py-6 px-36 rounded-2xl">
+      <p>{bannerString}</p>
+      <ApplyButton status={status} applicationURL={applicationURL} />
+    </div>
+  );
+}
+
+const ApplyButton = (props: {
+  status: ApplicationPeriodStatus;
+  applicationURL: string;
+}) => {
+  const { status, applicationURL } = props;
+
+  return (
+    <Button
+      type="button"
+      onClick={() => window.open(applicationURL, "_blank")}
+      className="mt-2 basis-full items-center justify-center shadow-sm text-sm rounded md:h-12"
+      data-testid="apply-button"
+    >
+      {status === "during-application" ? "Apply now!" : "View Round"}
+    </Button>
+  );
+};
+
+export default ApplicationsCountdownBanner;
