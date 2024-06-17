a09ad070418d9cf2e1c170ef3bea5548f200f7ba
Jaxcoder <codenamejason@outlook.com>
2024-06-17 13:26:39 -0400

    fix test

--- packages/round-manager/src/features/common/__tests__/CopyToClipboardButton.test.tsx
+++ packages/round-manager/src/features/common/__tests__/CopyToClipboardButton.test.tsx
@@ -14,17 +14,22 @@ Object.assign(navigator, {
 });
 
 describe("<CopyToClipboardButton />", () => {
+  const textToCopy = "foobar"; // Define the text to be copied
+
   beforeEach(() => {
     jest.clearAllMocks();
+    // Mock the clipboard API
+    const clipboard = { writeText: jest.fn() };
+    Object.assign(navigator, { clipboard });
   });
 
-  it("should display Copy to clipboard when not clicked ", () => {
+  it("should display 'Round application' when not clicked", () => {
     renderWrapped(<CopyToClipboardButton textToCopy={textToCopy} />);
 
-    expect(screen.getByText("Round Application")).toBeInTheDocument();
+    expect(screen.getByText("Round application")).toBeInTheDocument();
   });
 
-  it("should display Copied to clipboard when clicked", () => {
+  it("should display 'Link Copied' when clicked", () => {
     renderWrapped(<CopyToClipboardButton textToCopy={textToCopy} />);
     const copyButton = screen.getByRole("button");
     fireEvent.click(copyButton);
@@ -32,19 +37,11 @@ describe("<CopyToClipboardButton />", () => {
     expect(screen.getByText("Link Copied")).toBeInTheDocument();
   });
 
-  /*TODO: a better way to do this:
-  *    const clipboard = { writeText: vi.fn() }
-    Object.assign(navigator, { clipboard })
-    renderWithContext(<CopyToClipboardButton textToCopy={'foobar'} />);
-    const copyButton = screen.getByRole("button");
-    fireEvent.click(copyButton);
-    expect(navigator.clipboard.writeText()).toHaveBeenCalledWith('foobar');
-  * */
   it("should copy value to clipboard when clicked", () => {
     renderWrapped(<CopyToClipboardButton textToCopy={textToCopy} />);
     const copyButton = screen.getByRole("button");
     fireEvent.click(copyButton);
 
-    expect(navigator.clipboard.readText()).toEqual(textToCopy);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(textToCopy);
   });
 });
