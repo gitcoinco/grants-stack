6c1c2030059d171475f2602f32feedd36b33c0fa
Andrea Franz <andrea@gravityblast.com>
2024-06-26 15:40:03 +0200

    keep the finalize round button always enabled

--- packages/round-manager/src/features/round/ViewRoundResults/ViewRoundResults.tsx
+++ packages/round-manager/src/features/round/ViewRoundResults/ViewRoundResults.tsx
@@ -1269,7 +1269,6 @@ function FinalizeResultsButton(props: {
         onClick={() => {
           props.setWarningModalOpen(true);
         }}
-        disabled={!props.isRoundFullyFunded}
         className={`self-end w-fit ${
           props.isRoundFullyFunded ? "bg-violet-400" : "bg-violet-200"
         } text-white py-2 mt-2 px-3 rounded`}
