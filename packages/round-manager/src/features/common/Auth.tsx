bae6bdc5a004cb3e37a22036faa38961376637d2
Jaxcoder <codenamejason@outlook.com>
2024-06-27 19:54:46 -0400

    add updated bg image for manager

--- packages/round-manager/src/features/common/Auth.tsx
+++ packages/round-manager/src/features/common/Auth.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "./Spinner";
 import Footer from "common/src/components/Footer";
 import Navbar from "./Navbar";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ReactComponent as LandingBanner } from "../../assets/landing/banner.svg";
 
 /**
  * Component for protecting child routes that require web3 wallet instance.
@@ -24,12 +25,13 @@ export default function Auth() {
   // todo: add background image
   return !isConnected ? (
     <div className="flex flex-col min-h-screen">
+      <LandingBanner className="absolute inset-0 w-full h-full object-cover z-[-1]" />
       <Navbar programCta={false} />
       <main className="flex-grow pt-4 container mx-auto">
         {isConnecting ? (
           <Spinner text="Logging you in..." />
         ) : (
-          <div className="flex flex-col bg-white">
+          <div className="flex flex-col bg-transparent">
             <div className="my-[15rem] sm:ml-4 md:ml-8 lg:ml-20">
               <span className="mb-6 text-6xl">Fund the future.</span>
               <p className="text-2xl mt-2 mb-6 text-grey-400">
@@ -42,7 +44,7 @@ export default function Auth() {
           </div>
         )}
       </main>
-      <div className="w-full bg-white">
+      <div className="w-full bg-transparent">
         <div className="container mx-auto">
           <Footer />
         </div>
