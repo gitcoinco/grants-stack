a2624e480c92aefa2ebc3619f6e73e8aaef1b047
0xKurt <Kurt@Merbeth.io>
2024-08-02 10:14:24 +0200

    Fix direct allocation query

--- packages/data-layer/src/queries.ts
+++ packages/data-layer/src/queries.ts
@@ -840,11 +840,15 @@ export const getDirectDonationsByProjectId = gql`
   query getDirectDonationsByProjectId($projectId: String!, $chainIds: [Int!]!) {
     rounds(
       filter: {
-        strategyId: { equalTo: "0x4cd0051913234cdd7d165b208851240d334786d6e5afbb4d0eec203515a9c6f3" }
+        strategyId: {
+          equalTo: "0x4cd0051913234cdd7d165b208851240d334786d6e5afbb4d0eec203515a9c6f3"
+        }
         chainId: { in: $chainIds }
-        donations: { every: { projectId: { equalTo: $projectId } } }
       }
     ) {
+      donations(filter: { projectId: { equalTo: $projectId } }) {
+        id
+      }
       id
       uniqueDonorsCount
       totalAmountDonatedInUsd
