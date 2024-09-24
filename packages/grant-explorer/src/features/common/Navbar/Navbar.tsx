320c6c48a60af784db175f1787e06c7e34cf5049
Hussein Martinez <husse.dev@gmail.com>
2024-09-24 10:39:21 +0700

    refactor: split Navbar into components

--- packages/grant-explorer/src/features/common/Navbar/Navbar.tsx
+++ packages/grant-explorer/src/features/common/Navbar/Navbar.tsx
@@ -1,15 +1,14 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { ReactComponent as GitcoinLogo } from "../../../assets/gitcoinlogo-black.svg";
 import { ReactComponent as GrantsExplorerLogo } from "../../../assets/topbar-logos-black.svg";
 import { ReactComponent as GrantsExplorerLogoMobile } from "../../../assets/explorer-logo-mobile.svg";
 import NavbarCart from "./NavbarCart";
-import { UserCircleIcon } from "@heroicons/react/24/outline";
 import { useEffect } from "react";
-import { useAccount } from "wagmi";
 import { useCartStorage } from "../../../store";
 import { Link } from "react-router-dom";
-import { getAlloVersion } from "common/src/config";
-import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
+import ContributionHistoryLink from "./ContributionHistoryLink";
+import AlloVersionBanner from "./AlloVersionBanner";
+import { NavbarConnectButton } from "./NavbarConnectButton";
+import { NavbarSeparator } from "./NavbarSeparator";
 // Note: we use this during a GG round, disabling until then.
 // import ExploreRoundsDropdown, {
 //   ExploreRoundsDropdownProps,
@@ -21,7 +20,11 @@ export interface NavbarProps {
   showAlloVersionBanner?: boolean;
 }
 
-export default function Navbar(props: NavbarProps) {
+export default function Navbar({
+  showAlloVersionBanner = false,
+  showWalletInteraction = true,
+  customBackground = "",
+}: NavbarProps) {
   /** This part keeps the store in sync between tabs */
   const store = useCartStorage();
 
@@ -39,10 +42,6 @@ export default function Navbar(props: NavbarProps) {
   }, []);
   /** end of part that keeps the store in sync between tabs */
 
-  const showWalletInteraction = props.showWalletInteraction ?? true;
-  const { address: walletAddress } = useAccount();
-  const alloVersion = getAlloVersion();
-
   // Note: we use this during a GG round, disabling until then.
   // todo: update this list with GG21 featured rounds.
   // const rounds: ExploreRoundsDropdownProps[] = [
@@ -57,7 +56,7 @@ export default function Navbar(props: NavbarProps) {
 
   return (
     <nav
-      className={`blurred fixed w-full z-20 shadow-[0_4px_24px_0px_rgba(0,0,0,0.08)] ${props.customBackground}`}
+      className={`blurred fixed w-full z-20 shadow-[0_4px_24px_0px_rgba(0,0,0,0.08)] ${customBackground}`}
     >
       <div className="mx-auto px-4 sm:px-6 lg:px-20 max-w-screen-2xl">
         <div className="flex justify-between h-16">
@@ -69,7 +68,7 @@ export default function Navbar(props: NavbarProps) {
             >
               <div className="flex gap-1 lg:gap-3 items-center">
                 <GitcoinLogo className="" />
-                <div className="border-grey-400 border-2 h-4 border-r ml-[2px]" />
+                <NavbarSeparator />{" "}
                 <GrantsExplorerLogo className="hidden lg:block" />
                 <GrantsExplorerLogoMobile className="lg:hidden" />
               </div>
@@ -77,70 +76,15 @@ export default function Navbar(props: NavbarProps) {
           </div>
           <div className="flex flex-row items-center gap-6 font-mono font-medium">
             {/* {rounds && <ExploreRoundsDropdown rounds={rounds} />} */}
-            {showWalletInteraction && (
-              <div>
-                <div
-                  data-testid="connect-wallet-button"
-                  id="connect-wallet-button"
-                >
-                  <ConnectButton
-                    showBalance={false}
-                    accountStatus={{
-                      smallScreen: "avatar",
-                      largeScreen: "full",
-                    }}
-                    chainStatus={{ smallScreen: "icon" }}
-                  />
-                </div>
-              </div>
-            )}
-            {walletAddress && (
-              <div>
-                <Link
-                  to={`/contributors/${walletAddress}`}
-                  className="flex-shrink-0 flex items-center ph-no-capture"
-                  data-testid={"contributions-link"}
-                >
-                  <UserCircleIcon className="h-8 w-8 ph-no-capture" />
-                </Link>
-              </div>
-            )}
+            {showWalletInteraction && <NavbarConnectButton />}
+            <ContributionHistoryLink />
             <NavbarCart cart={store.projects} />
           </div>
         </div>
       </div>
-      {props.showAlloVersionBanner && (
+      {showAlloVersionBanner && (
         <div className="bg-white/40 backdrop-blur-sm p-4 text-center w-full font-medium flex flex-col items-center justify-center text-black">
-          <div>
-            <ExclamationCircleIcon className="h-5 w-5 inline-block mr-2" />
-            {alloVersion === "allo-v2" ? (
-              <>
-                Rounds launched before the 25th of March appear on Allo v1.
-                Check out those rounds{" "}
-                <a
-                  className="underline"
-                  target="_blank"
-                  href="https://explorer-v1.gitcoin.co"
-                >
-                  here
-                </a>
-                !
-              </>
-            ) : (
-              <>
-                Rounds launched after the 24th of March appear on Allo v2. Check
-                out those rounds{" "}
-                <a
-                  className="underline"
-                  target="_blank"
-                  href="https://explorer.gitcoin.co"
-                >
-                  here
-                </a>
-                !
-              </>
-            )}
-          </div>
+          <AlloVersionBanner />
         </div>
       )}
     </nav>
---
+++ packages/grant-explorer/src/features/common/Navbar/NavbarSeparator.tsx
@@ -0,0 +1,3 @@
+export const NavbarSeparator = () => {
+  return <div className="border-grey-400 border-2 h-4 border-r ml-[2px]" />;
+};
