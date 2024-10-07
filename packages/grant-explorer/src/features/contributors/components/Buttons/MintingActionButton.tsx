e66158ec6be5035a5b6fef639137ea2f4bcf13e7
Hussein Martinez <husse.dev@gmail.com>
2024-10-07 14:38:09 +0700

    chore: use query in MintingActionBurron

--- packages/grant-explorer/src/features/contributors/components/Buttons/MintingActionButton.tsx
+++ packages/grant-explorer/src/features/contributors/components/Buttons/MintingActionButton.tsx
@@ -1,3 +1,5 @@
+import { useDataLayer } from "data-layer";
+import { useQuery } from "@tanstack/react-query";
 import { MintDonationButton } from "./MintDonattionButton";
 import { ViewAttestationButton } from "./ViewAttestationButton";
 
@@ -9,8 +11,25 @@ export function MintingActionButton({
     chainId: number;
   };
 }) {
-  const isMinted = false;
-  const canMint = true;
+  const { hash: transactionHash, chainId } = transaction;
+  const dataLayer = useDataLayer();
+
+  const { data: attestations } = useQuery({
+    queryKey: [`attestationsIdsByDonationTransaction-${[transactionHash]}`],
+    enabled: !!transactionHash,
+    queryFn: () =>
+      dataLayer.getMintingAttestationIdsByTransactionHash({
+        transactionHashes: [transactionHash],
+      }),
+  });
+
+  console.log("DEBUG mintingActionButton", {
+    transactionHash,
+    attestations,
+  });
+  const nAttestations = attestations?.length ?? 0;
+  const isMinted = nAttestations > 0;
+  const canMint = true; // TODO
 
   return isMinted ? (
     <ViewAttestationButton
