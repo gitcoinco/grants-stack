a46ac71cb2a62bda788a67fe15de99a732e24650
Hussein Martinez <husse.dev@gmail.com>
2024-10-08 19:59:24 +0700

    chore: implemented useMintingAttestations

---
+++ packages/grant-explorer/src/features/contributors/hooks/useMintingAttestations.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from "react";
+
+import { MintingAttestationIdsData, useDataLayer } from "data-layer";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+type MintingAttestationsResponse = Omit<
+  UseQueryResult<MintingAttestationIdsData[], Error>,
+  "data"
+> & { data?: Record<string, MintingAttestationIdsData[]> };
+
+export const useMintingAttestations = (
+  transactionHashes: string[]
+): MintingAttestationsResponse => {
+  const dataLayer = useDataLayer();
+
+  const attestationsResponse = useQuery({
+    queryKey: ["attestations", transactionHashes],
+    queryFn: () =>
+      dataLayer.getMintingAttestationIdsByTransactionHash({
+        transactionHashes,
+      }),
+    enabled: !!transactionHashes && transactionHashes.length > 0,
+  });
+
+  const attestations = useMemo<
+    Record<string, MintingAttestationIdsData[]> | undefined
+  >(() => {
+    if (!attestationsResponse.data) return attestationsResponse.data;
+    const attestationsByTransactionHash: Record<
+      string,
+      MintingAttestationIdsData[]
+    > = {};
+    attestationsResponse.data.forEach((attestation) => {
+      const transactionHash = attestation.txnHash;
+      if (!attestationsByTransactionHash[transactionHash]) {
+        attestationsByTransactionHash[transactionHash] = [attestation];
+      } else {
+        attestationsByTransactionHash[transactionHash].push(attestation);
+      }
+    });
+    return attestationsByTransactionHash;
+  }, [attestationsResponse.data]);
+
+  return {
+    ...attestationsResponse,
+    data: attestations,
+  };
+};
