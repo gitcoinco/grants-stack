a9af0557409d035698bac69ffd616bb02082d430
Hussein Martinez <husse.dev@gmail.com>
2024-10-11 12:50:03 +0700

    refactor: MindDonationButton renders RainbowBorderButton

--- packages/grant-explorer/src/features/contributors/components/Buttons/MintDonationButton.tsx
+++ packages/grant-explorer/src/features/contributors/components/Buttons/MintDonationButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "common/src/styles";
+import { RainbowBorderButton } from "./RainbowBorderButton";
 
 export function MintDonationButton({
   disabled = false,
@@ -8,18 +8,12 @@ export function MintDonationButton({
   onClick?: () => void;
 }) {
   return (
-    <div
-      className={`flex align-center justify-center border-[1px] rounded-[8px] ${disabled ? "bg-grey-100 border-grey-100" : "bg-rainbow-gradient border-transparent"}`}
+    <RainbowBorderButton
+      disabled={disabled}
+      onClick={onClick}
+      data-testid="mint-donation-button"
     >
-      <Button
-        type="button"
-        disabled={disabled}
-        onClick={onClick}
-        className={`px-4 py-1 rounded-[8px] bg-white font-medium font-mono text-base text-black h-8 whitespace-nowrap border-[1px] border-transparent disabled:bg-grey-100 disabled:text-[#979998] disabled:border-grey-100`}
-        data-testid="mint-donation-button"
-      >
-        Mint donation
-      </Button>
-    </div>
+      Mint donation
+    </RainbowBorderButton>
   );
 }
