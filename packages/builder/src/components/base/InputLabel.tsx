9503a61537798d8e18e38d54c351a4e56f75c155
Henry <43460021+MasterHW@users.noreply.github.com>
2025-02-24 14:57:06 -0700

    Acknowledge "Hidden" application fields are still public (#3770)
    * tweaked verbiage
    
    * hidden to unlisted

--- packages/builder/src/components/base/InputLabel.tsx
+++ packages/builder/src/components/base/InputLabel.tsx
@@ -22,11 +22,11 @@ export default function InputLabel({
           className="shrink ml-2 px-2 py-1"
           bg="purple.900"
           hasArrow
-          label="This info will be hidden from your project’s Explorer page."
+          label="This info won't go on your project's Explorer page, but is still readable on-chain."
         >
           <span className="text-gray-400 text-xs mr-2 inline-block">
             <EyeSlashIcon className="w-3 h-3 inline-block mr-0.5 align-middle" />
-            <span className="align-middle">Hidden</span>
+            <span className="align-middle">Unlisted</span>
           </span>
         </Tooltip>
       )}
