0bd436937948fdd8739252deaa09619640fc9ecd
Huss Martinez <husse.dev@gmail.com>
2025-04-04 13:36:22 +0700

    fix: prose-pre style to fix width

--- packages/grant-explorer/src/features/round/ViewProjectDetails/components/Detail.tsx
+++ packages/grant-explorer/src/features/round/ViewProjectDetails/components/Detail.tsx
@@ -6,7 +6,7 @@ export function Detail(props: { text: string; testID: string }) {
       dangerouslySetInnerHTML={{
         __html: renderToHTML(props.text.replace(/\n/g, "\n\n")),
       }}
-      className="text-blue-800 text-md prose prose-h1:text-lg prose-h2:text-base prose-h3:text-base prose-a:text-blue-600 max-w-full"
+      className="prose-pre:whitespace-pre-wrap text-blue-800 text-md prose prose-h1:text-lg prose-h2:text-base prose-h3:text-base prose-a:text-blue-600 max-w-full"
       data-testid={props.testID}
     />
   );
